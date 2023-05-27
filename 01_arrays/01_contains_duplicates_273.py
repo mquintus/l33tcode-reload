c6076126a694aabfda7fb062d6d6e55ddab5045c
mquintus <marius.stuebs@riseup.net>
2023-05-27 13:58:17 -0700

    Update 01_contains_duplicates_273.py - Implement hashtable
    Using a hashtable is more efficient for larger lists with respect to Time Complexity, because it reduces it from O(n^2) to O(n) by storing all investigated numbers with a Space Complexity of O(n).

--- 01_arrays/01_contains_duplicates_273.py
+++ 01_arrays/01_contains_duplicates_273.py
@@ -30,4 +30,66 @@ class Solution:
             if first in nums:
                 return True
             return recursive(nums)
-        return recursive(nums)
+        # return recursive(nums)
+
+        '''
+        Since the list is not ordered, finding an element can't be quicker than O(n)
+        and since we've got to do this for each element, the time complexity is always quadratic.
+
+        Next idea: Using a hashmap of length n.
+
+        Time Complexity: O(n)
+        Space Complexity O(n)
+
+        We loop through the list once (O(n)) and add each element to the hashtable / hashmap 
+        Storing and checking in a hashtable requires only (O(1)) 
+
+        Note that the time complexity of the hashtable lookup can increase to O(n) in the worst case
+        if all items land in the same bucket. In our leetcode example that would happen if 
+        the list contains [0, n, 2n, 3n, 4n, ...] as values with a list of lenght n.
+        In this corner case, the hashtable time complexity would degenerate to O(n^2) as the previous solutions were,
+        but with far worse space complexity. 
+        In a real world environment, 
+        this could be improved by:
+        - instead of modulo based hashtable, using a proper hashing scheme 
+        - if many collisions occur, increase the key space / length of the hashtable
+          - also consider adding a second hasing scheme (as bloom filters do)
+        '''
+        class naive_hashtable:
+            def __init__(self, size):
+                self.size = size
+                self.entries = 0
+                self.table = [None for i in range(size)]
+            def put(self, item):
+                if self.entries == self.size:
+                    return False
+                pos = item % self.size
+                while self.table[pos] is not None and self.table[pos] != item:
+                    pos = (pos + 1) % self.size
+                if self.table[pos] is None:
+                    self.entries += 1
+                    self.table[pos] = item
+                return True
+            def has(self, item):
+                if self.entries == 0:
+                    return False
+                tries = self.size
+                pos = item % self.size
+                while self.table[pos] != item:
+                    if self.table[pos] is None or tries == 0:
+                        return False
+                    pos = (pos + 1) % self.size
+                    tries -= 1
+                return self.table[pos] == item
+
+        def use_hastable(nums: List[int]) -> bool:
+            size = len(nums)
+            hashtable = naive_hashtable(size)
+            for val in nums:
+                if hashtable.has(val):
+                    print("Match")
+                    return True
+                hashtable.put(val)
+            return False
+            
+        return use_hastable(nums)
