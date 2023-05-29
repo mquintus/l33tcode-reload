34e9536c974d1eb4acaf75b986b16d1f31b1b0ef
mquintus <marius.stuebs@riseup.net>
2023-05-28 18:39:21 -0700

    Update 03_Two_sum_001.py - Hashmap solution

--- 01_arrays/03_Two_sum_001.py
+++ 01_arrays/03_Two_sum_001.py
@@ -3,12 +3,35 @@ class Solution:
         '''
         Naive solution:
         Pop an element from the list and search for the "complement" in the remaining list.
-        Time complexity: O(n^2)
-        Space complexity: O(1) - we are only ever storing two counters.
+        Worst case time complexity: O(n^2)
         '''
         def naive_solution(nums: List[int], target: int) -> List[int]:
             for i in range(len(nums)):
                 for j in range(i + 1, len(nums)):
                     if target - nums[i] == nums[j]:
                         return i,j
-        return naive_solution(nums, target)
+        # return naive_solution(nums, target)
+
+        '''
+        Bucket sort solution: (hash map)
+        Since we are already looping once through the whole list,
+        we might use this loop to save the item to a hashmap.
+        Adding something to a hashmap or into a bucket has a time complexity of O(1)
+        
+        At the same time, for each num, we calculate the complement (O(1)) and
+        we check if the complement is already in the bucket (O(1))
+
+        At the time when the last num ends up in the bucket, checking the hashmap 
+        will definitely return the index of complement.
+
+        Time complexity: O(n)
+        Space complexity: O(n)
+        '''
+        def bucket_sort_solution(nums: List[int], target: int) -> List[int]:
+            hashmap = {}
+            for i in range(len(nums)):
+                complement = target - nums[i]
+                if complement in hashmap.keys():
+                    return hashmap[complement], i
+                hashmap[nums[i]] = i
+        return bucket_sort_solution(nums, target)
