e0423bf6f0193dcf96780a1393a0f342a0bcb66a
mquintus <marius.stuebs@riseup.net>
2023-05-30 12:05:15 -0700

    Update 05_Top_K_frequent_elemets_347.py We have a plan

--- 01_arrays/05_Top_K_frequent_elemets_347.py
+++ 01_arrays/05_Top_K_frequent_elemets_347.py
@@ -8,6 +8,19 @@ class Solution:
 
         When searching for the most frequent number, the naive approach is to keep a new counter
         for each distinct element.
-        This way, we only need to iterate once the original list
+        This way, we only need to iterate once the original list (O(n))
         and then iterate the counter list to select the element that occured most often.
+
+        However, looping over the counter list will take another O(n) of time complexity,
+        resulting in O(n^2)
+        and we want to stay below that.
+
+        If we use a heap to structure the counters, then
+        the list can stay sorted while we are updating it.
+
+        A max head keeps the largest element at the top.
+
+        At the end, we need to pop from the heap with O(log k)
+        but actually k times to get the k elements. Since k is a constant,
+        O(k * log n) can be reduced to O(log n).
         '''
