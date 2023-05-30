f77563f9a809d71fb748ece21c570d93ee141649
mquintus <marius.stuebs@riseup.net>
2023-05-30 12:38:09 -0700

    Update 05_Top_K_frequent_elemets_347.py

--- 01_arrays/05_Top_K_frequent_elemets_347.py
+++ 01_arrays/05_Top_K_frequent_elemets_347.py
@@ -32,3 +32,53 @@ class Solution:
 
         This sounds a lot like a priority queue.
         '''
+        def heap_solution(nums: List[int], k: int) -> List[int]:
+            from heapq import heappop, heappush, heapify
+
+            my_max_heap = []
+            my_counter = {}
+            tasks = {}
+
+            if len(nums) == 1:
+                '''Only one elemt: Is already sorted'''
+                return nums
+
+            while len(nums) > 0:
+                mynum = nums.pop()
+                ''' Get the next number '''
+                if mynum not in my_counter.keys():
+                    ''' 
+                    If it is a fresh number, 
+                    a) set the counter to -1
+                    Since we are using a minheap, we want the counter to be negative to always
+                    have the most occurences at the top. 
+                    b) Create a new "task" for the priority queue
+                    c) Add the task to the hashmap (to find it immediately if we need to change it)
+                    d) Add the task to the priority queue / minheap
+                    '''
+                    my_counter[mynum] = -1
+                    task = [my_counter[mynum], mynum]
+                    tasks[mynum] = task
+                    heappush(my_max_heap, task)
+                else:
+                    '''
+                    If the number is already known:
+                    a) Increase the hashmap counter
+                    b) Update the task within the heap, damaging the constraints
+                    c) Re-Heapify
+                       This action can be implemented more efficiently if we already
+                       know which element changed, but since we don't want to dive into
+                       this problem with our own heap implementation, we leave this as an
+                       exercise to the reader.
+                    '''
+                    my_counter[mynum] += -1
+                    tasks[mynum][0] = my_counter[mynum]
+                    heapq.heapify(my_max_heap)
+            ''' 
+            Finally let's pop the k "smallest" aka. largest tasks from the heap
+            and return the number of that task.
+            '''
+            return [x[1] for x in heapq.nsmallest(k, my_max_heap)]
+
+
+        return heap_solution(nums, k)
