8748401f2b75d031f775117883e32cb7b7bf3981
mquintus <marius.stuebs@riseup.net>
2023-05-29 22:10:41 -0700

    Update 04_Group_Anagrams_49.py

--- 01_arrays/04_Group_Anagrams_49.py
+++ 01_arrays/04_Group_Anagrams_49.py
@@ -1,4 +1,3 @@
-import collections
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -12,14 +11,33 @@ class Solution:
         the Counter could be a highly optimized hashmap
         - Breadth-first-search parsing all strings in parallel might yield performance advantages (just guessing)
         '''
-        myCounters = {}
 
-        for myString in strs:
-            # It is necessary to sort the myString because collections.Counter
-            # preserves order. I am still looking for a real hashmap (where the order is determined by the key)
-            hashed = str(collections.Counter(sorted(myString)))
-            if hashed not in myCounters.keys():
-                myCounters[hashed] = []
-            myCounters[hashed].append(myString)
+        def counters_approach(strs: List[str]) -> List[List[str]]:
+            import collections
+            myCounters = {}
 
-        return myCounters.values()
+            for myString in strs:
+                # It is necessary to sort the myString because collections.Counter
+                # preserves order. I am still looking for a real hashmap (where the order is determined by the key)
+                hashed = str(collections.Counter(sorted(myString)))
+                if hashed not in myCounters.keys():
+                    myCounters[hashed] = []
+                myCounters[hashed].append(myString)
+
+            return myCounters.values()
+        # return counters_approach(strs)
+
+        ''' 
+        Sorted string approach:
+        Looking at some other solutions, I realized that sorting and then counting
+        if not better than just sorting and using the sorted string directly as the hash.
+        '''
+        def sort_approach(strs: List[str]) -> List[List[str]]:
+            myCounters = {}
+            for myString in strs:
+                hashed = "".join(sorted(myString))
+                if hashed not in myCounters.keys():
+                    myCounters[hashed] = []
+                myCounters[hashed].append(myString)
+            return myCounters.values()
+        return sort_approach(strs)
