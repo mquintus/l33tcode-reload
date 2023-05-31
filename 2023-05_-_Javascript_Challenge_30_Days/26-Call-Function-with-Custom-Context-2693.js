53d97f50dbe6395d232f1a26debf9b05aae6b3b4
mquintus <marius.stuebs@riseup.net>
2023-05-31 13:27:03 -0700

    Update 26-Call-Function-with-Custom-Context-2693.js Simplified idea (failing a specific test case though)

--- 2023-05_-_Javascript_Challenge_30_Days/26-Call-Function-with-Custom-Context-2693.js
+++ 2023-05_-_Javascript_Challenge_30_Days/26-Call-Function-with-Custom-Context-2693.js
@@ -5,24 +5,20 @@
  */
 Function.prototype.callPolyfill = function(context, ...args) {
     /**
-     * Idea: 
+     * Naive Idea: 
      * 1. loop the dict "context", extract key and value,
      * and assign them onto a new object 
      *    proxyObj[key] = value;
      * 2. make "this" a function of my proxyObj
      * 3. call proxyObj."this"
+     *
+     * Simplified idea:
+     * 1. use the "context" object directly as my proxyObj
+     * 2. context.fn = this
+     * 3. return context.fn(...args);
      */
-    var proxyObj = {}
-
-    var contextkeys = Object.keys(context);
-    for (var i = 0; i < contextkeys.length; i++) {
-        var mykey = contextkeys[i];
-        proxyObj[mykey] = context[mykey];
-    }
-
-    var fn = this;
-    proxyObj.fn = fn;
-    return proxyObj.fn(...args);
+    context.fn = this;
+    return context.fn(...args);
 }
 
 /**
