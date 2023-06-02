42d8f1f7d3114c94465723e7c57888a7746eb641
mquintus <marius.stuebs@riseup.net>
2023-06-02 16:27:34 -0700

    Update 27-Event-Emitter-2694.js Submitted

--- 2023-05_-_Javascript_Challenge_30_Days/27-Event-Emitter-2694.js
+++ 2023-05_-_Javascript_Challenge_30_Days/27-Event-Emitter-2694.js
@@ -1,15 +1,47 @@
 class EventEmitter {
+  constructor() {
+      this.callback_store = {};
+      this.cb_counter = 0;
+  }
+
   subscribe(event, cb) {
-      
+    const cb_counter = this.cb_counter;
+    this.cb_counter = this.cb_counter + 1;
+
+    if (!(event in this.callback_store)) {
+        this.callback_store[event] = {};
+    }
+    
+    this.callback_store[event][cb_counter] = cb;
+    const that = this;
+
     return {
         unsubscribe: () => {
-
+            if (event in that.callback_store) {
+                 if (cb_counter in that.callback_store[event]) {
+                      delete that.callback_store[event][cb_counter];
+                 }
+            }
+            
         }
     };
   }
 
   emit(event, args = []) {
-
+    var ret = [];
+    if ((event in this.callback_store)) {
+        for (var [ctr, cb] of Object.entries(this.callback_store[event])) {
+            const ret_single = cb(...args);
+            if (typeof ret_single == 'undefined') {
+                continue;
+            }
+            if (ret_single === null) {
+                continue;
+            }
+            ret.push(ret_single);
+        }
+    }
+    return ret;
   }
 }
 
