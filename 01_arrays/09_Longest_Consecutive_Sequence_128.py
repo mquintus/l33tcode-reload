543fedfedbad37341865f83b6d81fb9d53bc9f09
mquintus <marius.stuebs@riseup.net>
2023-06-03 09:43:59 -0700

    Update 09_Longest_Consecutive_Sequence_128.py  Train of thought

--- 01_arrays/09_Longest_Consecutive_Sequence_128.py
+++ 01_arrays/09_Longest_Consecutive_Sequence_128.py
@@ -13,5 +13,15 @@ class Solution:
         Problem: Requirement is O(n) time complexity.
 
         Back to the naive approach:
-        - 
+        - Merging buckets maybe isn't too expensive. Even if the expense is n, we come out with two n of executing
+          which is still O(n)
+        - but... simpler. Forget about beginning/end. 
+
+        Algorithm outline:
+        - We iterate each element and hash that number:  n steps.
+        - We iterate each element a second time and check if it is a "begin" element by checking if i - 1 is in the hash map.
+          - This takes 2*n checks, one for each number, one for each "before number"
+        - If it is a begin element, iteratively check the after element until not found in the hashmap 
+          -> takes at most n*1 checks if not consequtive and 1*n checks if totally consequtive 
+        - Keep track of chain length -> return
         '''
