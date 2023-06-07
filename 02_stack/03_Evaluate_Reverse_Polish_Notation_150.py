03b31c74b3dba04833221bee2425f09193d926e5
mquintus <marius.stuebs@riseup.net>
2023-06-07 09:38:55 -0700

    Update 03_Evaluate_Reverse_Polish_Notation_150.py Second approach: Use a stack, reading from the left.

--- 02_stack/03_Evaluate_Reverse_Polish_Notation_150.py
+++ 02_stack/03_Evaluate_Reverse_Polish_Notation_150.py
@@ -1,5 +1,6 @@
 '''
 First approach: Build a tree, reading from the right.
+Second approach: Use a stack, reading from the left.
 '''
 class Node:
     left = None
@@ -30,9 +31,37 @@ def create_tree(tokens: List[str]) -> Node:
         node.right = create_tree(tokens)
         node.left = create_tree(tokens)
         node.value = int(node.solve())
-        return node
-      
+        return node    
+
+
+'''Second approach: Use a stack, reading from the left.'''
+def stack_solution(tokens: List[str]) -> int:
+    def solve(b,a,op):
+        print(b,a,op)
+        if op == '+':
+            return a + b
+        if op == '-':
+            return a - b
+        if op == '*':
+            return a * b
+        if op == '/':
+            return a / b
+
+
+    myStack = []
+    while len(tokens) > 0:
+        el = tokens.pop(0)
+        if el in operators:
+            value = solve(myStack.pop(), myStack.pop(), el)
+        else:
+            value = el
+        myStack.append(int(value))
+    return myStack.pop()
+
 class Solution:
-  def evalRPN(self, tokens: List[str]) -> int:
-      root_node = create_tree(tokens)
-      return root_node.value
+    def evalRPN(self, tokens: List[str]) -> int:
+        #root_node = create_tree(tokens)
+        #return root_node.value
+        return stack_solution(tokens)
+
+
