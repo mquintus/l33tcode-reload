7f38df86b25b2efe50ab3ad85cd4041449520450
mquintus <marius.stuebs@riseup.net>
2023-06-07 09:36:59 -0700

    Update 1318_Minimum_Flips_to_Make_a_OR_b_Equal_to_c.py

--- 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/1318_Minimum_Flips_to_Make_a_OR_b_Equal_to_c.py
+++ 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/1318_Minimum_Flips_to_Make_a_OR_b_Equal_to_c.py
@@ -23,5 +23,43 @@ class Solution:
 
 
         First, we write a function that counts set bits
-        Then, we do the operations as described
         '''
+        def countSetBits(number):
+            counter = 0
+            #print(number)
+            while number > 0:
+                if number & 1: 
+                    counter += 1
+                number >>= 1
+            return counter
+
+
+        #assert countSetBits(7) == 3
+        #assert countSetBits(6) == 2
+        #assert countSetBits(5) == 2
+        #assert countSetBits(4) == 1
+        #assert countSetBits(3) == 2
+        #assert countSetBits(2) == 1
+        #assert countSetBits(1) == 1
+        #assert countSetBits(0) == 0
+        
+        '''
+        Then, we do the operations as described:
+        '''
+        bits_ctr = 0
+        #print("bits_ctr", bits_ctr, "from",a ,b, "to", c)
+
+        bits_needed_to_be_set_to_1 = ((a | b) ^ c) & c
+        bits_ctr += countSetBits(bits_needed_to_be_set_to_1)
+        #print("bits_needed_to_be_set_to_1", countSetBits(bits_needed_to_be_set_to_1), bits_ctr)
+
+        bits_from_A_needed_to_be_set_to_0 = (a & ~c)
+        bits_ctr += countSetBits(bits_from_A_needed_to_be_set_to_0)
+        #print("bits_from_A_needed_to_be_set_to_0", countSetBits(bits_from_A_needed_to_be_set_to_0), bits_ctr)
+
+        bits_from_B_needed_to_be_set_to_0 = (b & ~c)
+        bits_ctr += countSetBits(bits_from_B_needed_to_be_set_to_0)
+        #print("bits_from_B_needed_to_be_set_to_0", countSetBits(bits_from_B_needed_to_be_set_to_0), bits_ctr)
+
+        return bits_ctr
+
