9fa5fc81ad3cb0cb5fbbe5068691ffdc42c3dd65
mquintus <marius.stuebs@riseup.net>
2023-06-21 10:41:41 -0700

    Create 05_Trapping_Rain_Water_42.py

---
+++ 03_Two_Pointers/05_Trapping_Rain_Water_42.py
@@ -0,0 +1,56 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        '''
+        This problem when approached as a two pointer problem
+        consists of two steps.
+        First, we acknowledge that any water between 
+        the two most outward poles of height 1 is captured.
+        Second, any pole in between reduces the amount of water.
+        '''
+
+        pointer1 = 0
+        pointer2 = len(height) - 1
+
+        previous_height = 0
+
+        def subtr(i, new_height):
+            subtr = min(height[i], new_height)
+            return subtr
+
+
+        full_amount = 0
+        new_height = 0
+        while pointer2 > pointer1:
+            '''Check if their height is 
+            higher that the already observed heights
+            '''
+            #print("pointer1", pointer1, "pointer2", pointer2)
+            if height[pointer1] > previous_height and height[pointer2] > previous_height:
+                new_height = min(height[pointer1], height[pointer2])
+                theoretical_amount = (new_height - previous_height) * (pointer2 - pointer1 - 1)
+                #print("theoretical_amount", theoretical_amount)
+
+                full_amount += theoretical_amount
+                #print("full_amount", full_amount)
+
+
+            if height[pointer1] < height[pointer2]:
+                pointer1 += 1
+                if pointer1 < pointer2:
+                    full_amount -= subtr(pointer1, new_height)
+
+            elif height[pointer2] < height[pointer1]:
+                pointer2 -= 1
+                if pointer1 < pointer2:
+                    full_amount -= subtr(pointer2, new_height)
+            else:
+                pointer1 += 1
+                pointer2 -= 1
+                if pointer1 <= pointer2:
+                    full_amount -= subtr(pointer1, new_height)
+                if pointer1 < pointer2:
+                    full_amount -= subtr(pointer2, new_height)
+
+            previous_height = new_height
+
+        return full_amount
