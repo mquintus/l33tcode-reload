29105c670ab8592e0a8bb3e99ea9680e96a926d4
mquintus <marius.stuebs@riseup.net>
2023-06-24 15:16:54 -0700

    Update 01_Binary_Search_704.py

--- 04_Binary_Search/01_Binary_Search_704.py
+++ 04_Binary_Search/01_Binary_Search_704.py
@@ -1,2 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)
+        
+        while start < end:
+            mid = start+(end-start)//2
+            if nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid 
+            else:
+                return mid
+        return -1
