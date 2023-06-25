504c0deaee5b25cffbc0072adbc589fb5e6658ce
mquintus <marius.stuebs@riseup.net>
2023-06-25 10:59:34 -0700

    Update 1575_Count_All_Possible_Routes.py

--- 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/1575_Count_All_Possible_Routes.py
+++ 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/1575_Count_All_Possible_Routes.py
@@ -1,5 +1,22 @@
+import functools
 class Solution:
     def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
         '''
         A dynamic programming challenge
         '''
+        @cache
+        def solve(start: int, finish: int, fuel: int) -> int:
+            paths = 0
+            if start == finish:
+                paths = 1
+
+            for i, location in enumerate(locations):
+                if i == start:
+                    continue
+                distance = abs(location - locations[start])
+                if distance > fuel:
+                    continue
+                paths += solve(i, finish, fuel - distance)
+
+            return paths
+        return solve(start, finish, fuel) % (10**9 + 7)
