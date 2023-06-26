9f56d808340e5897f5858dda767e6d008d0d622d
mquintus <marius.stuebs@riseup.net>
2023-06-26 09:52:02 -0400

    Update 2462_Total_Cost_to_Hire_K_Workers.py

--- 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/2462_Total_Cost_to_Hire_K_Workers.py
+++ 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/2462_Total_Cost_to_Hire_K_Workers.py
@@ -6,6 +6,8 @@ class Solution:
         with two twists:
             - We can select from two subsets and must choose the better
             - Subsets and indexes might change after each selection
+            - When pulling a number from the left of the subsets, it might
+              disappear in the right as well
         
         The intuitive approach is to loop both the cost subsets 
         and put them into a minheap.
@@ -13,7 +15,7 @@ class Solution:
         The annoying thing is when they overlap, removing a worker 
         from the left list also removes them from the right list
         '''
-        if candidates * 2 > len(costs):
+        if candidates * 2 >= len(costs) or k >= len(costs):
             return sum(sorted(costs)[:k])
 
         number_hired = 0
@@ -34,60 +36,57 @@ class Solution:
             idx = len(costs_with_index) - 1 - i
             costs_with_index[idx] = None
 
-        def refill(myList, sign='1', skip=0):
+        def next_worker(sign='1', skip=0):
             if sign == -1:
                 skip += 1
-            idx = sign * (candidates + skip - 1)
+            idx = sign * (candidates + skip)
             if sign == -1:
                 idx = len(costs_with_index) + idx
             new_el = None
             if idx < len( costs_with_index) and idx >= 0:
                 new_el = costs_with_index[idx]
-                #print(new_el)
-            else:
-                pass
-                #print(len(costs_with_index), "Index out of bounds", idx, costs_with_index)
-            killindex = 0
-            if new_el is not None:
-                heapq.heappush(myList, new_el)
-                killindex = new_el[1]
-            return killindex
+            return new_el
         
-        skip_left = 0
-        skip_right = 0
+        hired_left = 0
+        hired_right = 0
+        overlap = False
         for number_hired in range(k):
-            #print(number_hired, k)
-            if left == []:
-                #print("left is empty")
-                left = [i for i in right]
-            if right == []:
-                #print("right is empty")
-                right = [i for i in left]
-            left_smallest = heapq.nsmallest(1, left)[0]
-            right_smallest = heapq.nsmallest(1, right)[0]
+            next_left = next_worker(1, hired_left)
+            next_right = next_worker(-1, hired_right)
+            
+            if len(right) == 0 or left[0][0] <= right[0][0]:
+                left_smallest = left[0]
+                hired_left += 1
 
-            if left_smallest[0] <= right_smallest[0]:
-                skip_left += 1
-                heapq.heappop(left)
+                if next_left is not None:
+                    heapq.heapreplace(left, next_left)
+                else:
+                    heapq.heappop(left)
+
                 that_index = left_smallest[1]
-                listindex = refill(left, 1, skip_left)
-                if that_index == right_smallest[1]:
-                    skip_right += 1
-                    heapq.heappop(right)
-                    listindex2 = refill(right, -1, skip_right)
-                    costs_with_index[listindex2] = None
+                
+                ## If the item is also in the right list,
+                ## Remove it there also.
+                ## No need to refill on the right: We know there is an overlap,
+                ## therefore we can always find any item in the left list.
+                if len(right) > 0:
+                    right_smallest = right[0]
+                    if that_index == right_smallest[1]:
+                        popped_right = heapq.heappop(right)
+                        assert popped_right[1] == that_index
+                        overlap = True
+                    
                 curr_cost = left_smallest[0]
-
-                costs_with_index[listindex] = None
+                costs_with_index[left_smallest[1]] = None
             else:
-                skip_right += 1
-                heapq.heappop(right)
+                right_smallest = right[0]
+                hired_right += 1
+                if not overlap and next_right is not None:
+                    heapq.heapreplace(right, next_right)
+                else:
+                    heapq.heappop(right)
                 curr_cost = right_smallest[0]
-
-
-                #refill heap
-                listindex2 = refill(right, -1, skip_right)
-                costs_with_index[listindex2] = None
+                costs_with_index[right_smallest[1]] = None
             total_cost += curr_cost
         
         return total_cost
