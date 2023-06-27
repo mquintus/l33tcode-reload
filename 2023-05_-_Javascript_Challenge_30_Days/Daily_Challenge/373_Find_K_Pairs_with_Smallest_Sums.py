b5e022a23603c81c437d2f66378977f14c67649e
mquintus <marius.stuebs@riseup.net>
2023-06-27 01:41:45 -0400

    Update 373_Find_K_Pairs_with_Smallest_Sums.py

--- 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/373_Find_K_Pairs_with_Smallest_Sums.py
+++ 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/373_Find_K_Pairs_with_Smallest_Sums.py
@@ -1,2 +1,6 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        '''
+        This sounds like a two-pointer problem.
+        Each round, we iterate on of the pointers (the one whose follower is smaller) and add the pair that both pointers are pointing to.
+        '''
