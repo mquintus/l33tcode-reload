c2372e4329fecd4b626ba2f591ea995709eb2ba5
mquintus <marius.stuebs@riseup.net>
2023-06-27 01:50:35 -0400

    Update 373_Find_K_Pairs_with_Smallest_Sums.py

--- 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/373_Find_K_Pairs_with_Smallest_Sums.py
+++ 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/373_Find_K_Pairs_with_Smallest_Sums.py
@@ -4,3 +4,19 @@ class Solution:
         This sounds like a two-pointer problem.
         Each round, we iterate on of the pointers (the one whose follower is smaller) and add the pair that both pointers are pointing to.
         '''
+        pairs = []
+        p = [0, 0]
+        for i in range(k):
+            print(p, len(nums1), len(nums2), k)
+            pairs.append([nums1[p[0]], nums2[p[1]]])
+
+            if len(nums1) > p[0] + 1 and (len(nums2) == p[1] + 1 or nums1[p[0] + 1] < nums2[p[1] + 1]):
+                p[0] += 1
+                p[1] = 0
+            elif len(nums2) > p[1] + 1:
+                p[0] = 0
+                p[1] += 1
+            else:
+                break
+        return pairs
+        
