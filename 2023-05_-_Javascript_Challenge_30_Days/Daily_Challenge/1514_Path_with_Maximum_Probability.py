2aaf5229a9d19c575f6abf69102e95b8dac44136
mquintus <marius.stuebs@riseup.net>
2023-06-28 06:13:18 -0400

    Update 1514_Path_with_Maximum_Probability.py

--- 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/1514_Path_with_Maximum_Probability.py
+++ 2023-05_-_Javascript_Challenge_30_Days/Daily_Challenge/1514_Path_with_Maximum_Probability.py
@@ -1,2 +1,38 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        def approach_with_maxheap(n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+            new_edges = {}
+            for i, e in enumerate(edges):
+                if not e[0] in new_edges.keys():
+                    new_edges[e[0]] = []    
+                if not e[1] in new_edges.keys():
+                    new_edges[e[1]] = []    
+                new_edges[e[0]].append((e[1], succProb[i], i,))
+                new_edges[e[1]].append((e[0], succProb[i], i,))
+
+
+            probabilities = [0 for i in range(n)]
+            probabilities[start] = 1
+            nodes_to_visit_bfs = [(-1, start,)]
+            
+            while len(nodes_to_visit_bfs) > 0:
+                oldprob, curr_node = heapq.heappop(nodes_to_visit_bfs)
+                oldprob = abs(oldprob)
+
+                if curr_node == end:
+                    return probabilities[end]
+
+                if curr_node not in new_edges.keys():
+                    continue
+
+                for next_node, edge_prob, i in new_edges[curr_node]:
+                    next_prob = edge_prob * oldprob
+                    if next_prob <= probabilities[next_node]:
+                        continue
+                    probabilities[next_node] = max(next_prob, probabilities[next_node])
+                    
+                    heapq.heappush(nodes_to_visit_bfs, (-next_prob, next_node,) )
+
+            return probabilities[end]
+            
+        return approach_with_maxheap(n, edges, succProb, start, end)
