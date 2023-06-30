9937c6ac86d520fc8c69990841a67372b84c36c4
mquintus <marius.stuebs@riseup.net>
2023-06-29 20:04:36 -0400

    Update 864_Shortest_Path_to_Get_All_Keys.py

--- Daily_Challenge/864_Shortest_Path_to_Get_All_Keys.py
+++ Daily_Challenge/864_Shortest_Path_to_Get_All_Keys.py
@@ -1,107 +1,55 @@
-import heapq
+import collections
+
 class Solution:
-    def shortestPathAllKeys(self, grid: List[str]) -> int:
-        keys = {} # name, coordinates
-        locks = {} # name, coordinates       
-        start = None
-
-
-        def populate_variables():
-            for y, row in enumerate(grid):
-                for x, cell in enumerate(row):
-                    if cell == '@':
-                        start = (x,y,)
-                    elif cell in 'abcdefghijklmnopqrstuvwxyz':
-                        keys[cell] = (x,y,)
-                    elif cell in 'abcdefghijklmnopqrstuvwxyz'.upper():
-                        locks[cell] = (x,y,)
-            return start, keys, locks
+    def shortestPathAllKeys(self, grid):
+        n = len(grid)
+        m = len(grid[0])
+        abs_keycount = 0
+        for y, row in enumerate(grid):
+            for x, cell in enumerate(row):
+                if cell == '@':
+                    start = (x,y,)
+                elif cell in 'abcdefghijklmnopqrstuvwxyz':
+                    abs_keycount += 1
                 
-        start, keys, locks = populate_variables()
+        
+        visited = set()
+        walk_dirs = [[0,1],[1,0],[-1,0],[0,-1]]
 
-        def replace_grid_field(grid, x, y, char):
-            grid[y] = grid[y][:x] + char + grid[y][x + 1:]
-
-        replace_grid_field(grid, start[0], start[1], '.')
-
-        def get_heuristic(position, to_key):
-            next_key_coords = keys[to_key]
-            
-            heuristic = abs(position[0] - next_key_coords[0]) + abs(position[1] - next_key_coords[1])
-            return heuristic
-
-
-        def get_possible_directions(grid, position, k, has_keys):
-            #print("position", position)
-            positions = []
-            for p in [
-                (position[0] - 1, position[1]),
-                (position[0] + 1, position[1]),
-                (position[0], position[1] - 1),
-                (position[0], position[1] + 1),
-                ]:
-                if p[0] >= 0 and p[1] >= 0 and p[0] < len(grid[0]) and p[1] < len(grid):
-                    cell_value = grid[p[1]][p[0]]
-#                    print(p, cell_value)
-                    if cell_value == '#':
-                        continue
-                    if cell_value == cell_value.upper() and cell_value.lower() in has_keys:
-                        positions.append(p)
-                    if cell_value == k:
-                        return [p]
-                    if cell_value == '.':
-                        positions.append(p)
-
-            return positions
-
-
-        def getShortestPathFromKeyAToKeyB(position: tuple, to_key: int, has_keys: list) -> int:
-            grid_copy = [cell for cell in [row for row in grid]]
-            # heuristic, steps, coordinates
-            waypoints = [( 10000, 0, position)]
-            steps = 0
-            while len(waypoints) > 0:
-                next_step = heapq.heappop(waypoints)
-#                print("Substate:", next_step)
-                p = next_step[2]
-                steps = next_step[1]
-#               print("if grid[p[1]][p[0]] == keys[to_key]:", grid[p[1]][p[0]], keys[to_key])
-                if grid[p[1]][p[0]] == to_key:
+        keyCount = 0
+        #grid_copy = [cell for cell in [row for row in grid]]
+        # steps, coordinates
+        waypoints = collections.deque()
+        waypoints.append( (0, start, "".join(sorted(".@abcdef")), 0,))
+        steps = 0
+        state_counter = 0
+        while waypoints:
+            state_counter += 1
+            next_step = waypoints.popleft()
+            steps = next_step[0]
+            p = next_step[1]
+            has_keys = next_step[2]
+            keyCount = next_step[3]
+            steps = next_step[0]
+            cell = grid[p[1]][p[0]]
+            if cell in 'abcdef' and cell.upper() not in has_keys:
+                has_keys += cell.upper()
+                has_keys = "".join(sorted(has_keys))
+                #print(has_keys, keyCount)
+                keyCount += 1
+                if keyCount == abs_keycount:
+                    print(state_counter)
                     return steps
-                steps = steps + 1
-                replace_grid_field(grid_copy, p[0], p[1], '@')
-                for next_field in get_possible_directions(grid_copy, p, k, has_keys):
-#                    print("next_field", next_field)
-                    heuristic = get_heuristic(next_field, to_key)
-                    heap_el = (heuristic, steps, next_field,)
-                    heapq.heappush(waypoints, heap_el)
-            return -1
 
-        prev_key = -1
-        level_stepcount = 0
-        has_keys = []
-        states = [ (len(has_keys), level_stepcount, start, has_keys,) ]
-        while len(states) > 0:
-            state = heapq.heappop(states)
-#            print("State:", state)
-            has_keys = state[3]
-            level_stepcount = state[1]
-            curr_pos = state[2]
-            if len(has_keys) == len(keys):
-                return level_stepcount
-            for k in keys.keys():
-                if k not in has_keys:
-                    distance = getShortestPathFromKeyAToKeyB(curr_pos, k, has_keys)
-#                    print("Key:",k, "Distance:", distance)
-                    if distance == -1:
-                        continue
-                    else:                        
-                        heapq.heappush(states, 
-                                        ( len(has_keys) + 1, 
-                                        distance + level_stepcount,
-                                        keys[k],
-                                        [*has_keys, k], ))
-
+            #replace_grid_field(grid_copy, p[0], p[1], '@')
+            for direction in walk_dirs:
+                x = p[0] + direction[0]
+                y = p[1] + direction[1]
+                if x >= 0 and y >= 0 and x < m and y < n:
+                    if grid[y][x] in has_keys:
+                        if (x, y, has_keys,) not in visited:
+                            heap_el = [steps + 1, (x, y,) , has_keys, keyCount]
+                            waypoints.append(heap_el)
+                            visited.add((x, y, has_keys,))
+        print(state_counter)
         return -1
-
-
