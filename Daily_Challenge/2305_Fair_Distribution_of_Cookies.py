85ccb4437b98b540f8bd75ec833defd81298f01c
mquintus <marius.stuebs@riseup.net>
2023-07-01 15:53:33 -0700

    Create 2305_Fair_Distribution_of_Cookies.py

---
+++ Daily_Challenge/2305_Fair_Distribution_of_Cookies.py
@@ -0,0 +1,5 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        '''
+        Max. 8 bags of cookies means that we can do backtracking through all of them.
+        '''
