f401eaddbf32d51e924ae97a2a9d1725a080ebbf
mquintus <mqs@stuebs.tech>
2023-07-27 21:48:36 -0700

    Update 486_Predict_the_Winner.py

---
+++ Daily_Challenge/486_Predict_the_Winner.py
@@ -0,0 +1,29 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        '''
+        The current player will win, if the next player won't.
+
+        This only 20 numbers are in the array, this should be manageable with brute force recursion.
+
+        isWinner(n) = ~isWinner(n + 1)
+
+        
+
+        '''
+        dp = []
+
+
+        def isWinner(nums, prev_points):
+            if len(nums) == 0:
+                return prev_points
+
+            if len(nums) == 1:
+                return isWinner([], -prev_points + nums[0])
+
+            if len(nums) > 1:
+                take_first = -1 * isWinner(nums[1:], -prev_points + nums[0])
+                take_last = -1 * isWinner(nums[:-1], -prev_points + nums[-1])
+                wins = max(take_first, take_last)
+                return wins
+
+        return 0 <= isWinner(nums, 0)
