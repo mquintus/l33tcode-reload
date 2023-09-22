a008270af68df56a1ab61b26dab89528cac87cab
mquintus <mqs@stuebs.tech>
2023-09-21 23:04:46 -0700

    Update 392_Is_Subsequence.py

--- Daily_Challenge/392_Is_Subsequence.py
+++ Daily_Challenge/392_Is_Subsequence.py
@@ -1,1 +1,13 @@
 # 392 - Is Subsequence
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+            
+        pointer1 = 0
+        for pointer2 in range(len(t)):
+            if s[pointer1] == t[pointer2]:
+                pointer1 += 1
+            if pointer1 == len(s):
+                return True
+        return False
