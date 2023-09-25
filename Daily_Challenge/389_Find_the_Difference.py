b3496e53e74c03f0e0363f48ef68dcfc2240d028
mquintus <mqs@stuebs.tech>
2023-09-24 18:18:01 -0700

    Update 389_Find_the_Difference.py

--- Daily_Challenge/389_Find_the_Difference.py
+++ Daily_Challenge/389_Find_the_Difference.py
@@ -1,1 +1,16 @@
 # 389 - Find the Difference
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        '''
+        Beautiful constant size, linear time complexity solution.
+        '''
+        letters = 0
+        i = -1
+        for i in range(len(s)):
+            letters -= ord(s[i])
+            letters += ord(t[i])
+        letters += ord(t[i + 1])
+        
+        return chr(letters)
+        
+
