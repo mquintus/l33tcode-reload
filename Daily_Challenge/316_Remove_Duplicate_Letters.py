0b1b22821ce48026a5cd84a72907407522e0068b
mquintus <mqs@stuebs.tech>
2023-09-25 22:02:38 -0700

    Update 316_Remove_Duplicate_Letters.py

--- Daily_Challenge/316_Remove_Duplicate_Letters.py
+++ Daily_Challenge/316_Remove_Duplicate_Letters.py
@@ -1,1 +1,24 @@
 # 316 - Remove Duplicate Letters
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        '''
+        This is a tough one.
+        Let's say we count how often each letter occurs.
+        '''
+        latest_occurence = [0] * 123
+        for i, char in enumerate(s):
+            latest_occurence[ord(char)] = i
+
+        seen = [False] * 123
+        stack = []
+        for i, char in enumerate(s):
+            while (len(stack) > 0
+                and (not seen[ord(char)])
+                and char < stack[-1] 
+                and latest_occurence[ord(stack[-1])] > i): 
+                    seen[ord(stack[-1])] = False
+                    stack.pop()
+            if not seen[ord(char)]:
+                stack.append(char)
+                seen[ord(char)] = True
+        return "".join(stack)
