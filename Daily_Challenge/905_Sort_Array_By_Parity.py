b6e16d6fa384d6cab4b1ca1ac732d38f1514eef5
mquintus <mqs@stuebs.tech>
2023-09-27 17:49:47 -0700

    Update 905_Sort_Array_By_Parity.py

--- Daily_Challenge/905_Sort_Array_By_Parity.py
+++ Daily_Challenge/905_Sort_Array_By_Parity.py
@@ -1,1 +1,15 @@
 # 905 - Sort Array By Parity
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        p1 = 0
+        p2 = 1
+
+        while p2 < len(nums):
+            if nums[p1] % 2 == 0:
+                p1 += 1
+
+            if nums[p2] % 2 == 0:
+                nums[p1], nums[p2] = nums[p2], nums[p1]
+            p2 += 1
+
+        return nums
