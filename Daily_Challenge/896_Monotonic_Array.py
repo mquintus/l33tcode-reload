d66dc49acf5e26dac51f5489a30aa884c9498059
mquintus <mqs@stuebs.tech>
2023-09-28 19:49:02 -0700

    Update 896_Monotonic_Array.py

--- Daily_Challenge/896_Monotonic_Array.py
+++ Daily_Challenge/896_Monotonic_Array.py
@@ -1,1 +1,11 @@
 # 896 - Monotonic Array
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        alwaysIncreasing = True
+        alwaysDecreasing = True
+        old = nums[0]
+        for num in nums[1:]:
+            alwaysIncreasing &= (num >= old)
+            alwaysDecreasing &= (num <= old)
+            old = num
+        return alwaysIncreasing or alwaysDecreasing
