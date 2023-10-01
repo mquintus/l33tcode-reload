1c7df3f1d73fe12c7dc5e0a1fb0b0aeccbdb7b7e
mquintus <mqs@stuebs.tech>
2023-09-30 19:26:29 -0700

    Update 557_Reverse_Words_in_a_String_III.py

--- Daily_Challenge/557_Reverse_Words_in_a_String_III.py
+++ Daily_Challenge/557_Reverse_Words_in_a_String_III.py
@@ -1,1 +1,6 @@
 # 557 - Reverse Words in a String III
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split(' ')
+        words = map(lambda x: x[::-1], words)
+        return " ".join(words)
