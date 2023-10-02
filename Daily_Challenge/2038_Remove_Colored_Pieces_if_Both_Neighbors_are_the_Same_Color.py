4d3f2382d2586ef08771aa5b5f47b4d32ddd3513
mquintus <mqs@stuebs.tech>
2023-10-01 18:54:22 -0700

    Update 2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.py

--- Daily_Challenge/2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.py
+++ Daily_Challenge/2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.py
@@ -1,1 +1,19 @@
 # 2038 - Remove Colored Pieces if Both Neighbors are the Same Color
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        alice_wins = 0
+        i = 0
+        while i < len(colors) - 2:
+            while colors[i:i+3] == 'AAA':
+                alice_wins += 1
+                i += 1
+            while colors[i:i+3] == 'BBB':
+                alice_wins -= 1
+                i += 1
+            i += 1
+
+        return alice_wins > 0
+
+
+
+        
