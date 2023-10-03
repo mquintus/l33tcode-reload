164ee5108f02e7cc76134e46e8afc0178ba21c23
mquintus <mqs@stuebs.tech>
2023-10-03 14:11:47 -0700

    Update 1512_Number_of_Good_Pairs.py

--- Daily_Challenge/1512_Number_of_Good_Pairs.py
+++ Daily_Challenge/1512_Number_of_Good_Pairs.py
@@ -1,1 +1,15 @@
 # 1512 - Number of Good Pairs
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        hashTable = {}
+        for i in nums:
+            if i in hashTable:
+                hashTable[i] += 1
+            else:
+                hashTable[i] = 1
+
+        counter = 0
+        for key, val in hashTable.items():
+            if val > 1:
+                counter += ((val - 1) * (val)) // 2
+        return counter
