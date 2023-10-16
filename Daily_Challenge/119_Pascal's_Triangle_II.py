39a9387d098f6527b125a91ef43a03414145bb12
mquintus <mqs@stuebs.tech>
2023-10-15 18:23:46 -0700

    Update 119_Pascal's_Triangle_II.py

--- Daily_Challenge/119_Pascal's_Triangle_II.py
+++ Daily_Challenge/119_Pascal's_Triangle_II.py
@@ -1,1 +1,12 @@
 # 119 - Pascal's Triangle II
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        currentRow = [1]
+        
+        for currentRowIndex in range(rowIndex):
+            nextRow = [1]
+            for position in range(1, len(currentRow)):
+                nextRow.append(currentRow[position] + currentRow[position - 1])
+            nextRow.append(1)
+            currentRow = nextRow
+        return currentRow
