5a820eb5929f724a974616b409957be0199f2fc9
mquintus <mqs@stuebs.tech>
2023-10-26 11:53:48 -0700

    Update 823_Binary_Trees_With_Factors.py

--- Daily_Challenge/823_Binary_Trees_With_Factors.py
+++ Daily_Challenge/823_Binary_Trees_With_Factors.py
@@ -1,1 +1,33 @@
 # 823 - Binary Trees With Factors
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        MOD = 10**9+7
+        arr = sorted(arr) 
+        n = len(arr)
+        dp = {}
+        for i in arr:
+            dp[i] = 1
+
+        for parent in arr:
+            #print(parent, "_", "_", 1)
+            for leftChild in arr:
+                if leftChild > parent // 2:
+                    continue
+                if parent % leftChild != 0:
+                    continue
+                rightChild = parent // leftChild
+                if rightChild not in dp.keys():
+                    continue
+                if rightChild < leftChild:
+                    break
+
+                parentSum = dp[leftChild]*dp[rightChild]
+                if leftChild != rightChild:
+                    parentSum *= 2
+                dp[parent]+=(parentSum) % MOD
+                #print(parent, leftChild, rightChild, parentSum)
+
+        mySum = 0
+        for i in arr:
+            mySum += dp[i] % MOD
+        return mySum % MOD
