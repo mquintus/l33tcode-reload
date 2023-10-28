9ea3da8289e6c27d517b3d42f864cdd5479dc7bc
mquintus <mqs@stuebs.tech>
2023-10-27 21:59:23 -0700

    Update 1220_Count_Vowels_Permutation.py

--- Daily_Challenge/1220_Count_Vowels_Permutation.py
+++ Daily_Challenge/1220_Count_Vowels_Permutation.py
@@ -1,1 +1,23 @@
 # 1220 - Count Vowels Permutation
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        MOD = 10**9+7
+        rules = {
+            'a': ['e'],
+            'e': ['a', 'i'],
+            'i': ['a', 'e', 'o', 'u'],
+            'o': ['i', 'u'],
+            'u': ['a']
+        }
+
+        last_letter = {'a':1, 'e':1, 'i':1, 'o':1, 'u':1}
+
+        for i in range(n-1):
+            added_letters = {'a':0, 'e':0, 'i':0, 'o':0, 'u':0}
+            for l, c in last_letter.items():
+                for follow in rules[l]:
+                    added_letters[follow] += c
+                    added_letters[follow] %= MOD
+            last_letter = added_letters
+        
+        return sum(last_letter.values()) % MOD
