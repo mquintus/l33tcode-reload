a9cc3e64b3dc1503eb062826f75e34553f3decfe
mquintus <mqs@stuebs.tech>
2023-10-29 09:47:35 -0700

    Update 458_Poor_Pigs.py

--- Daily_Challenge/458_Poor_Pigs.py
+++ Daily_Challenge/458_Poor_Pigs.py
@@ -1,1 +1,8 @@
 # 458 - Poor Pigs
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        rounds = minutesToTest//minutesToDie + 1
+        pigs = 0
+        while rounds ** pigs < buckets:
+            pigs += 1
+        return pigs
