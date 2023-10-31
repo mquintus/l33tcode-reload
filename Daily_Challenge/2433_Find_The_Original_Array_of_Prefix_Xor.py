0bcac823e2dcd5eec70440a6c8479e26907c4cae
mquintus <mqs@stuebs.tech>
2023-10-30 18:05:16 -0700

    Update 2433_Find_The_Original_Array_of_Prefix_Xor.py

--- Daily_Challenge/2433_Find_The_Original_Array_of_Prefix_Xor.py
+++ Daily_Challenge/2433_Find_The_Original_Array_of_Prefix_Xor.py
@@ -1,1 +1,11 @@
 # 2433 - Find The Original Array of Prefix Xor
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        # We can replace in-place, not requiring any additional space
+        previous = 0
+        for i, n in enumerate(pref):
+            x = previous ^ n
+            pref[i] = x
+            previous = n
+            
+        return pref
