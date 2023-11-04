6cd9f969ab9bbf9f804e0e85a3d40ddef54b2d03
mquintus <mqs@stuebs.tech>
2023-11-03 20:15:30 -0700

    Update 1503_Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank.py

--- Daily_Challenge/1503_Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank.py
+++ Daily_Challenge/1503_Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank.py
@@ -1,1 +1,5 @@
 # 1503 - Last Moment Before All Ants Fall Out of a Plank
+class Solution:
+    def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
+        times = [*left, *[n - r for r in right]]
+        return max(times)
