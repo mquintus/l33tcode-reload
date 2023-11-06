db98fdf4db6f96a5c9fcfb59b6f5657b3b032ab7
mquintus <mqs@stuebs.tech>
2023-11-05 17:12:42 -0800

    Update 1845_Seat_Reservation_Manager.py

--- Daily_Challenge/1845_Seat_Reservation_Manager.py
+++ Daily_Challenge/1845_Seat_Reservation_Manager.py
@@ -1,1 +1,13 @@
 # 1845 - Seat Reservation Manager
+import heapq
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.free_seats = list(range(1, n+1))
+
+    def reserve(self) -> int:
+        return heapq.heappop(self.free_seats)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.free_seats, seatNumber)
+        
