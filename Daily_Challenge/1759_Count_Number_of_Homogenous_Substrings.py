5413d17aaba6ab13ed91664bb75d48a2f5eac1de
mquintus <mqs@stuebs.tech>
2023-11-09 13:12:30 -0800

    Update 1759_Count_Number_of_Homogenous_Substrings.py

--- Daily_Challenge/1759_Count_Number_of_Homogenous_Substrings.py
+++ Daily_Challenge/1759_Count_Number_of_Homogenous_Substrings.py
@@ -1,1 +1,19 @@
 # 1759 - Count Number of Homogenous Substrings
+class Solution:
+    def countHomogenous(self, s: str) -> int:
+        MOD = 10**9 + 7
+        dp = 0
+
+        prev = "!"
+        length = 0
+        for char in s:
+            if char != prev:
+                dp += ((length + 1) * length) // 2
+                length = 0
+                prev = char
+            length += 1
+        dp += ((length + 1) * length) // 2
+        dp %= MOD
+
+        return dp
+            
