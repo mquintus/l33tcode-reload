22de6c7ce58c1db6dc887a302236c5b6b6d5c08f
mquintus <mqs@stuebs.tech>
2023-11-11 17:41:35 -0800

    Update 815_Bus_Routes.py

--- Daily_Challenge/815_Bus_Routes.py
+++ Daily_Challenge/815_Bus_Routes.py
@@ -1,1 +1,47 @@
 # 815 - Bus Routes
+import heapq
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:
+            return 0
+
+        edges = {}
+        for routenr, route in enumerate(routes):
+            for orig in route:
+                if orig not in edges:
+                    edges[orig] = set()
+                edges[orig].add(routenr)
+
+        if target not in edges or source not in edges:
+            return -1
+
+        print(edges[target], edges[source])
+
+        connectors = {}
+        for stop, connections in edges.items():
+            for orig in connections:
+                if orig not in connectors:
+                    connectors[orig] = set()
+                for dest in connections:
+                    connectors[orig].add(dest)
+
+        visited = {}
+        states = []
+        for conn in edges[source]:
+            states.append((1, conn))
+
+        while len(states) > 0:
+            cost, orig = heapq.heappop(states)
+            if orig in edges[target]:
+                return cost
+                
+            visited[orig] = True
+
+            for dest in connectors[orig]:
+                if dest in visited.keys():
+                    continue
+
+                new_cost = cost + 1
+                heapq.heappush(states, (new_cost, dest,))
+        return -1
+        
