e1eb148eb034b2d475d182f2f10e4977a6312f67
mquintus <mqs@stuebs.tech>
2023-11-14 21:49:29 -0800

    Update 1846_Maximum_Element_After_Decreasing_and_Rearranging.py

--- Daily_Challenge/1846_Maximum_Element_After_Decreasing_and_Rearranging.py
+++ Daily_Challenge/1846_Maximum_Element_After_Decreasing_and_Rearranging.py
@@ -1,1 +1,13 @@
 # 1846 - Maximum Element After Decreasing and Rearranging
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        mymax = 1 
+        prev = 1
+        arr[0] = 1
+        for a in arr:
+            if a > prev+1:
+                a = prev+1
+            prev = a
+            mymax = max(mymax, a)
+        return mymax
