5d2998924752cae75e842196bdaa7f8f06cd2622
mquintus <mqs@stuebs.tech>
2023-11-21 10:19:59 -0800

    Update 1814_Count_Nice_Pairs_in_an_Array.py

--- Daily_Challenge/1814_Count_Nice_Pairs_in_an_Array.py
+++ Daily_Challenge/1814_Count_Nice_Pairs_in_an_Array.py
@@ -1,1 +1,24 @@
 # 1814 - Count Nice Pairs in an Array
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+        hashmap = {}
+
+        MOD = 10**9 + 7
+
+        def reverse_num(num):
+            return int(f"{num}"[::-1])
+
+        for i,a in enumerate(nums):
+            identifier = a - reverse_num(a)
+            if identifier not in hashmap:
+                hashmap[identifier] = 0
+            hashmap[identifier] += 1
+
+        global_count = 0
+        for count in hashmap.values():
+            if count == 1:
+                continue
+            combinations = (count * (count-1)) // 2
+            global_count += combinations  
+
+        return global_count % MOD
