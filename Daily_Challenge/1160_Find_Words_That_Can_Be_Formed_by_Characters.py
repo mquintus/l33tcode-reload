3eb566e9cffdf814369f8f45fcffa6f1f65ba078
mquintus <mqs@stuebs.tech>
2023-12-01 22:16:04 -0800

    Update 1160_Find_Words_That_Can_Be_Formed_by_Characters.py

--- Daily_Challenge/1160_Find_Words_That_Can_Be_Formed_by_Characters.py
+++ Daily_Challenge/1160_Find_Words_That_Can_Be_Formed_by_Characters.py
@@ -1,1 +1,24 @@
 # 1160 - Find Words That Can Be Formed by Characters
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        myHashtable = {}
+        for c in chars:
+            if c not in myHashtable:
+                myHashtable[c] = 0
+            myHashtable[c] += 1
+
+        matching_count = 0
+
+        for word in words:
+            local_count = {}
+            local_length = 0
+            for c in word:
+                if c not in local_count:
+                    local_count[c] = 0
+                local_count[c] += 1
+                if c not in myHashtable or local_count[c] > myHashtable[c]:
+                    break
+                local_length += 1
+            else:
+                matching_count += local_length
+        return matching_count
