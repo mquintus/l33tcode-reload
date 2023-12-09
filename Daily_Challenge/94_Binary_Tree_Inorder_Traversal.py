eb21e404283337d3f26b7045cdcba181225e4f81
mquintus <mqs@stuebs.tech>
2023-12-09 13:51:13 -0800

    Update 94_Binary_Tree_Inorder_Traversal.py

--- Daily_Challenge/94_Binary_Tree_Inorder_Traversal.py
+++ Daily_Challenge/94_Binary_Tree_Inorder_Traversal.py
@@ -1,1 +1,17 @@
 # 94 - Binary Tree Inorder Traversal
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if root is None:
+            return result
+
+        result.extend(self.inorderTraversal(root.left))
+        result.append(root.val)
+        result.extend(self.inorderTraversal(root.right))
+        return result
