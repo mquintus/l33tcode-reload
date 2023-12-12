20292a80b9a97ed39ef8807cb684f265f20c289f
mquintus <mqs@stuebs.tech>
2023-12-12 11:08:34 -0800

    Update 1464_Maximum_Product_of_Two_Elements_in_an_Array.py

--- Daily_Challenge/1464_Maximum_Product_of_Two_Elements_in_an_Array.py
+++ Daily_Challenge/1464_Maximum_Product_of_Two_Elements_in_an_Array.py
@@ -1,1 +1,18 @@
 # 1464 - Maximum Product of Two Elements in an Array
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        '''
+        Intuition: Select the two largest items
+        '''
+
+        max1 = -1
+        max2 = -1
+
+        for el in nums:
+            if el >= max1:
+                max2 = max1
+                max1 = el
+            elif el >= max2:
+                max2 = el
+
+        return (max1 - 1) * (max2 - 1)
