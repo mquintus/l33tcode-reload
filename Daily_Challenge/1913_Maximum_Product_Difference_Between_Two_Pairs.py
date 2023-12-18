26049c12d648104e9b33081d677a87290552a9b8
mquintus <mqs@stuebs.tech>
2023-12-17 21:09:15 -0800

    Update 1913_Maximum_Product_Difference_Between_Two_Pairs.py

--- Daily_Challenge/1913_Maximum_Product_Difference_Between_Two_Pairs.py
+++ Daily_Challenge/1913_Maximum_Product_Difference_Between_Two_Pairs.py
@@ -1,1 +1,26 @@
 # 1913 - Maximum Product Difference Between Two Pairs
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        '''
+        Intuition:
+        Find the smallest two and the largest two.
+        '''
+        min1 = 100000
+        min2 = 100000
+        max1 = -1
+        max2 = -1
+
+        for num in nums:
+            if num <= min1:
+                min2 = min1
+                min1 = num
+            elif num <= min2:
+                min2 = num
+            
+            if num >= max1:
+                max2 = max1
+                max1 = num
+            elif num >= max2:
+                max2 = num
+        
+        return max1*max2 - min1*min2
