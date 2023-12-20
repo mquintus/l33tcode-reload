a29946709a33d6d8c761d449cf093f47bae78d3c
mquintus <mqs@stuebs.tech>
2023-12-19 17:20:08 -0800

    Update 2706_Buy_Two_Chocolates.py

--- Daily_Challenge/2706_Buy_Two_Chocolates.py
+++ Daily_Challenge/2706_Buy_Two_Chocolates.py
@@ -1,1 +1,22 @@
 # 2706 - Buy Two Chocolates
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        '''
+        Intuition: Find the two smallest numbers.
+        If their sum is smaller or equal the money,
+        return the difference.
+        Otherwise return the money.
+        '''
+        small1 = 101
+        small2 = 101
+        for p in prices:
+            if p <= small1:
+                small2 = small1
+                small1 = p
+            elif p <= small2:
+                small2 = p
+
+        difference = money - small1 - small2
+        if difference >= 0:
+            return difference
+        return money
