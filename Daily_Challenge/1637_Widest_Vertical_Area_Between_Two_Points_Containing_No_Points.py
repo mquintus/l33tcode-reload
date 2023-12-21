d8696052517d522c04028abbdcb68a017b59c21a
mquintus <mqs@stuebs.tech>
2023-12-20 17:28:37 -0800

    Update 1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.py

--- Daily_Challenge/1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.py
+++ Daily_Challenge/1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.py
@@ -1,1 +1,16 @@
 # 1637 - Widest Vertical Area Between Two Points Containing No Points
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        # Intuition:
+        # The y values are not relevant.
+        # The question is, given two points where no other point is in between,
+        # what is the max distance between any points i!=j
+        x_values = sorted([x[0] for x in points])
+
+        distance = -1
+        prev = x_values[0]       
+        for x in x_values[1:]:
+            distance = max(distance, x - prev)
+            prev = x
+        
+        return distance
