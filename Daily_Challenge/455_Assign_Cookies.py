9556dd94cded098a698e0e2f7f5e30680e44a0e7
mquintus <mqs@stuebs.tech>
2024-01-01 13:26:08 -0800

    Update 455_Assign_Cookies.py

--- Daily_Challenge/455_Assign_Cookies.py
+++ Daily_Challenge/455_Assign_Cookies.py
@@ -1,1 +1,23 @@
 # 455 - Assign Cookies
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        
+        happy = 0
+
+        gp = len(g) - 1
+        sp = len(s) - 1
+
+        while gp >= 0 and sp >= 0:
+            size = s[sp]
+            greed = g[gp]
+            if size >= greed:
+                happy += 1
+                sp -= 1
+
+            gp -= 1
+        
+        return happy
+
+        
