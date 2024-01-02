fe66574b49c0c605459c79cb67013530f7dd3ca1
mquintus <mqs@stuebs.tech>
2024-01-01 16:50:55 -0800

    Update 2610_Convert_an_Array_Into_a_2D_Array_With_Conditions.py

--- Daily_Challenge/2610_Convert_an_Array_Into_a_2D_Array_With_Conditions.py
+++ Daily_Challenge/2610_Convert_an_Array_Into_a_2D_Array_With_Conditions.py
@@ -1,1 +1,16 @@
 # 2610 - Convert an Array Into a 2D Array With Conditions
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        hashmap = {}
+        result = []
+        for n in nums:
+            pos = 0
+            if n not in hashmap:
+                hashmap[n] = pos
+            else:
+                pos = hashmap[n]
+            if len(result) <= pos:
+                result.append([])
+            result[pos].append(n)
+            hashmap[n] += 1
+        return result
