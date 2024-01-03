9e3928c2cd251470a87887e5d2547e83b3d042b6
mquintus <mqs@stuebs.tech>
2024-01-02 18:13:06 -0800

    Update 2125_Number_of_Laser_Beams_in_a_Bank.py

--- Daily_Challenge/2125_Number_of_Laser_Beams_in_a_Bank.py
+++ Daily_Challenge/2125_Number_of_Laser_Beams_in_a_Bank.py
@@ -1,1 +1,18 @@
 # 2125 - Number of Laser Beams in a Bank
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        '''
+        Intuition:
+        Count 1s
+        Drop each row with only zeros
+        Pairwise multiply x and y (row i and row j)
+        '''
+        all_lasers = 0
+        current_lasers = 0
+        for row in bank:
+            next_lasers = row.count('1')
+            if next_lasers == 0:
+                continue
+            all_lasers += current_lasers * next_lasers
+            current_lasers = next_lasers
+        return all_lasers
