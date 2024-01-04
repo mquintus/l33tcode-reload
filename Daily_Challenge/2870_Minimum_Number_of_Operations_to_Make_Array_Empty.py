3a3f2906b7c5cc01c0d5c0930ab92706215c460a
mquintus <mqs@stuebs.tech>
2024-01-03 16:27:30 -0800

    Update 2870_Minimum_Number_of_Operations_to_Make_Array_Empty.py

--- Daily_Challenge/2870_Minimum_Number_of_Operations_to_Make_Array_Empty.py
+++ Daily_Challenge/2870_Minimum_Number_of_Operations_to_Make_Array_Empty.py
@@ -1,1 +1,18 @@
 # 2870 - Minimum Number of Operations to Make Array Empty
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        operations = 0
+        for v in counts.values():
+            if v == 1:
+                return -1
+            if v % 3 == 0:
+                operations += v // 3
+                continue
+            if v % 3 == 1:
+                operations += (v - 4) // 3 + 2
+                continue
+            if v % 3 == 2:
+                operations += (v - 1) // 3 + 1
+                continue
+        return operations
