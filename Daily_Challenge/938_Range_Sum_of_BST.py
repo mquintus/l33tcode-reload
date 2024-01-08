22048b817918558862a4b5efbf1567056c53aafa
mquintus <mqs@stuebs.tech>
2024-01-07 16:35:07 -0800

    Update 938_Range_Sum_of_BST.py

--- Daily_Challenge/938_Range_Sum_of_BST.py
+++ Daily_Challenge/938_Range_Sum_of_BST.py
@@ -1,1 +1,15 @@
 # 938 - Range Sum of BST
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if root is None:
+            return 0
+        ret = 0
+        if low <= root.val <= high:
+            ret = root.val
+        return ret + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
