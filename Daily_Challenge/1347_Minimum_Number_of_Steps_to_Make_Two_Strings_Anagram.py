d9b4a35b1621675dfb9ba49b0c39bb4172e22b9c
mquintus <mqs@stuebs.tech>
2024-01-12 17:24:49 -0800

    Update 1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.py

--- Daily_Challenge/1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.py
+++ Daily_Challenge/1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.py
@@ -1,1 +1,15 @@
 # 1347 - Minimum Number of Steps to Make Two Strings Anagram
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+
+        p = Counter(s)
+        q = Counter(t)
+
+        equals = 0
+        for key, value in p.items():
+            if key in q.keys():
+                equals += min(value, q[key]) 
+
+        unequals = len(s) - equals
+
+        return unequals
