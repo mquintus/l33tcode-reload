d1175c43d32b1826e374f1e97edd9ea1062c2b27
mquintus <mqs@stuebs.tech>
2024-01-15 17:45:56 -0800

    Update 380_Insert_Delete_GetRandom_O(1).py

--- Daily_Challenge/380_Insert_Delete_GetRandom_O(1).py
+++ Daily_Challenge/380_Insert_Delete_GetRandom_O(1).py
@@ -1,1 +1,39 @@
 # 380 - Insert Delete GetRandom O(1)
+class RandomizedSet:
+
+    def __init__(self):
+        self.order = []
+        self.items = {}
+
+    def insert(self, val: int) -> bool:
+        vacant = val not in self.items
+        if vacant:
+            self.items[val] = len(self.order)
+            self.order.append(val)
+        return vacant
+
+    def remove(self, val: int) -> bool:
+        present = val in self.items.keys()
+        if not present:
+            return False
+
+        position = self.items[val]
+        if position == len(self.order) - 1:
+            self.order.pop()
+            del self.items[val]
+        else:
+            lastEl = self.order.pop()
+            self.items[lastEl] = position
+            self.order[position] = lastEl
+            del self.items[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.order)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
