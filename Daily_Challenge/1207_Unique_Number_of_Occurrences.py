5e62cd48e664cc29548164aef17afd34a9858338
mquintus <mqs@stuebs.tech>
2024-01-16 16:37:12 -0800

    Update 1207_Unique_Number_of_Occurrences.py

--- Daily_Challenge/1207_Unique_Number_of_Occurrences.py
+++ Daily_Challenge/1207_Unique_Number_of_Occurrences.py
@@ -1,1 +1,9 @@
 # 1207 - Unique Number of Occurrences
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        frequencies = Counter(arr)
+        frequencies_of_frequencies = Counter(frequencies.values())
+        for f in frequencies_of_frequencies.values():
+            if f != 1:
+                return False
+        return True
