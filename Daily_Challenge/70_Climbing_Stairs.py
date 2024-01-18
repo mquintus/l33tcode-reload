43e284598e1889ecced0a2e6058e4e8a517d7336
mquintus <mqs@stuebs.tech>
2024-01-17 16:20:48 -0800

    Update 70_Climbing_Stairs.py

--- Daily_Challenge/70_Climbing_Stairs.py
+++ Daily_Challenge/70_Climbing_Stairs.py
@@ -1,1 +1,12 @@
 # 70 - Climbing Stairs
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1, 2]
+        i = 2
+        if n < 3:
+            return dp[n-1]
+
+        for _ in range(2, n):
+            dp.append(dp[i-2] + dp[i-1])
+            dp.pop(0)
+        return dp[-1]
