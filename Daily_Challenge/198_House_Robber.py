7e1da8810f0821e9bfeb8a8cc923d00c48de076a
mquintus <mqs@stuebs.tech>
2024-01-20 16:53:16 -0800

    Update 198_House_Robber.py

--- Daily_Challenge/198_House_Robber.py
+++ Daily_Challenge/198_House_Robber.py
@@ -1,1 +1,13 @@
 # 198 - House Robber
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        @cache
+        def tos(i):
+            if i >= n:
+                return 0
+            take = nums[i] + tos(i+2)
+            skip = tos(i+1)
+            return max(take, skip)
+
+        return tos(0)
