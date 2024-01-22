524ec9658e6c8dc5c0502596308690ae8cf50931
mquintus <mqs@stuebs.tech>
2024-01-21 16:53:32 -0800

    Update 645_Set_Mismatch.py

--- Daily_Challenge/645_Set_Mismatch.py
+++ Daily_Challenge/645_Set_Mismatch.py
@@ -1,1 +1,16 @@
 # 645 - Set Mismatch
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        allNumbers = set(nums)
+        missing = -1
+        for el in range(1, len(nums) + 1):
+            if el not in allNumbers:
+                missing = el
+                break
+
+        sortedNumbers = sorted(nums)
+        prev = 0
+        for i in sortedNumbers:
+            if i == prev:
+                return [i, missing]
+            prev = i
