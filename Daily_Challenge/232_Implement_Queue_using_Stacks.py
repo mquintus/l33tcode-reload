886a69535b89d7ac4113ea3266e2560786ed114c
mquintus <mqs@stuebs.tech>
2024-01-28 18:30:14 -0800

    Update 232_Implement_Queue_using_Stacks.py

--- Daily_Challenge/232_Implement_Queue_using_Stacks.py
+++ Daily_Challenge/232_Implement_Queue_using_Stacks.py
@@ -1,1 +1,35 @@
 # 232 - Implement Queue using Stacks
+class MyQueue:
+
+    def __init__(self):
+        self.a = []
+        self.b = []
+
+    def push(self, x: int) -> None:
+        self.a.append(x)
+
+    def revert(self):
+        n = len(self.a)
+        for _ in range(n):
+            self.b.append(self.a.pop())
+
+    def pop(self) -> int:
+        if len(self.b) == 0:
+            self.revert()
+        return self.b.pop()
+
+    def peek(self) -> int:
+        if len(self.b) == 0:
+            self.revert()
+        return self.b[-1]
+
+    def empty(self) -> bool:
+        return len(self.b) == len(self.a) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
