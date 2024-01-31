750fa6151c3b042d704bbc62b53b4b890a88b095
mquintus <mqs@stuebs.tech>
2024-01-31 11:26:27 -0800

    Update 739_Daily_Temperatures.py

--- Daily_Challenge/739_Daily_Temperatures.py
+++ Daily_Challenge/739_Daily_Temperatures.py
@@ -1,1 +1,29 @@
 # 739 - Daily Temperatures
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        '''
+        Let's iterate from the end
+        '''
+
+        result = []
+        monotonicStack = []
+        for i in range(len(temperatures)-1, -1, -1 ):
+            el = temperatures[i]
+
+            while len(monotonicStack) > 0 and el >= monotonicStack[0][0]:
+                monotonicStack.pop(0)
+
+            if len(monotonicStack) == 0:
+                distance = 0
+                result.insert(0, distance)
+                monotonicStack.insert(0, (el, i))
+                continue
+            
+            if el < monotonicStack[0][0]:
+                distance = monotonicStack[0][1] - i
+                result.insert(0, distance)
+                monotonicStack.insert(0, (el, i))
+            
+        return result
+
+
