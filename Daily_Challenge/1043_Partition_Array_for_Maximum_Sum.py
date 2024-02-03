3bced2096522b1162b9ec4c2003cfb0153643b1d
mquintus <mqs@stuebs.tech>
2024-02-02 19:53:13 -0800

    Update 1043_Partition_Array_for_Maximum_Sum.py

--- Daily_Challenge/1043_Partition_Array_for_Maximum_Sum.py
+++ Daily_Challenge/1043_Partition_Array_for_Maximum_Sum.py
@@ -1,1 +1,21 @@
 # 1043 - Partition Array for Maximum Sum
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        dp = [-1] * n
+
+        # take or skip
+        @cache
+        def solve(pos):
+            if pos >= n:
+                return 0
+            bestoption = -1
+            for length in range(1, k+1):
+                if pos+length > n:
+                    break
+                take = solve(pos + length) + length*max(arr[pos:pos+length])
+                #print(pos, length, take)
+                bestoption = max(bestoption, take)
+            return bestoption
+        
+        return solve(0)
