14881ad837af5275807136313c1ff840a559c044
mquintus <mqs@stuebs.tech>
2024-02-05 23:33:35 -0800

    Update 49_Group_Anagrams.py

--- Daily_Challenge/49_Group_Anagrams.py
+++ Daily_Challenge/49_Group_Anagrams.py
@@ -1,1 +1,11 @@
 # 49 - Group Anagrams
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for s in strs:
+            counter = frozenset(Counter(s).items())
+            if counter not in anagrams.keys():
+                anagrams[counter] = []
+            anagrams[counter].append(s)
+        return anagrams.values()
+
