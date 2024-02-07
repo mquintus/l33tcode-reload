6bc7901bf92836ec3d011435d77077b98280399f
mquintus <mqs@stuebs.tech>
2024-02-06 16:38:23 -0800

    Update 451_Sort_Characters_By_Frequency.py

--- Daily_Challenge/451_Sort_Characters_By_Frequency.py
+++ Daily_Challenge/451_Sort_Characters_By_Frequency.py
@@ -1,1 +1,5 @@
 # 451 - Sort Characters By Frequency
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        c = Counter(s)
+        return "".join([k*v for k, v in sorted(list(c.items()), reverse=True, key=lambda x: x[1])])
