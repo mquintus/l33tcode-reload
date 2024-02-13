9920e3d906f502d9641d19026ccf0cb9e664ddc9
mquintus <mqs@stuebs.tech>
2024-02-12 16:36:37 -0800

    Update 2108_Find_First_Palindromic_String_in_the_Array.py

--- Daily_Challenge/2108_Find_First_Palindromic_String_in_the_Array.py
+++ Daily_Challenge/2108_Find_First_Palindromic_String_in_the_Array.py
@@ -1,1 +1,17 @@
 # 2108 - Find First Palindromic String in the Array
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        def is_p(s):
+            if len(s) == 1:
+                return True
+            p1 = 0
+            p2 = len(s) - 1
+            while p1 <= p2 and p2 > 0 and p1 < len(s) and s[p1] == s[p2]:
+                p1 += 1
+                p2 -= 1
+            return p1 > p2
+        
+        for s in words:
+            if is_p(s):
+                return s
+        return ""
