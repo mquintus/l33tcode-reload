49fc32d415dedc2b3aa98d6a2a3e29bf503cc681
mquintus <mqs@stuebs.tech>
2024-02-14 19:45:10 -0800

    Update 2971_Find_Polygon_With_the_Largest_Perimeter.py

--- Daily_Challenge/2971_Find_Polygon_With_the_Largest_Perimeter.py
+++ Daily_Challenge/2971_Find_Polygon_With_the_Largest_Perimeter.py
@@ -1,1 +1,23 @@
 # 2971 - Find Polygon With the Largest Perimeter
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        # Condition: The sum of the other sides has to be larger than the largest part.
+        # 1. Sort 
+        nums.sort()
+        # 2. Iterate all edges, keep running sum, identify largest possible option
+        running = 0
+        count = 0
+        max_index = -1
+        running_max = -1
+        for i, edge in enumerate(nums):
+            if i >= 2:
+                if running > edge:
+                    max_index = i
+                    running_max = running + edge
+            running += edge
+            
+        return running_max
+
+
+
+
