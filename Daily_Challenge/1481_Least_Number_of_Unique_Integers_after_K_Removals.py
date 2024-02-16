b248557d2f522c3d4f9e65240a2e3c3b8d1eb748
mquintus <mqs@stuebs.tech>
2024-02-16 11:16:15 -0800

    Update 1481_Least_Number_of_Unique_Integers_after_K_Removals.py

--- Daily_Challenge/1481_Least_Number_of_Unique_Integers_after_K_Removals.py
+++ Daily_Challenge/1481_Least_Number_of_Unique_Integers_after_K_Removals.py
@@ -1,1 +1,11 @@
 # 1481 - Least Number of Unique Integers after K Removals
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        c = Counter(arr)
+        c = sorted(c.values())
+        different_numbers = len(c)
+        for el in c:
+            if k >= el:
+                different_numbers -= 1
+                k -= el
+        return different_numbers
