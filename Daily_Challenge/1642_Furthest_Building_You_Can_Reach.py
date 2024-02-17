a185fa58e45380a0092844ff3cad5162a375b0e9
mquintus <mqs@stuebs.tech>
2024-02-16 17:42:15 -0800

    Update 1642_Furthest_Building_You_Can_Reach.py

--- Daily_Challenge/1642_Furthest_Building_You_Can_Reach.py
+++ Daily_Challenge/1642_Furthest_Building_You_Can_Reach.py
@@ -1,1 +1,33 @@
 # 1642 - Furthest Building You Can Reach
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        ladder_heap = []
+        i = 0
+        while i < len(heights) - 1:
+            height = heights[i + 1] - heights[i]
+            #print(i, bricks, ladders, ladder_heap)
+
+            if height <= 0:
+                i += 1
+                continue
+            
+            if ladders > 0:
+                ladders -= 1
+                i += 1
+                heapq.heappush(ladder_heap, height)
+                continue
+            
+            if bricks >= 0:
+                new_height = height
+                if ladder_heap:
+                    new_height = ladder_heap[0]
+                if height > new_height and bricks >= new_height:
+                    bricks -= heapq.heappop(ladder_heap)
+                    ladders += 1
+                    continue
+                if bricks >= height:
+                    bricks -= height
+                    i += 1
+                    continue
+            break
+        return i
