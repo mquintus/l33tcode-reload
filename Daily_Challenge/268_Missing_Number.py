0ea71947d1e9db7cb954964eab22bb826e17dd87
mquintus <mqs@stuebs.tech>
2024-02-20 09:37:05 -0800

    Update 268_Missing_Number.py

--- Daily_Challenge/268_Missing_Number.py
+++ Daily_Challenge/268_Missing_Number.py
@@ -1,1 +1,8 @@
 # 268 - Missing Number
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = ((n) * (n+1)) // 2
+        actual_sum = sum(nums)
+        missing_number = expected_sum - actual_sum
+        return missing_number
