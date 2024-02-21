08082dc7514c5497f887a7fe17b1c66d63a80d53
mquintus <mqs@stuebs.tech>
2024-02-20 21:16:44 -0800

    Update 201_Bitwise_AND_of_Numbers_Range.py

--- Daily_Challenge/201_Bitwise_AND_of_Numbers_Range.py
+++ Daily_Challenge/201_Bitwise_AND_of_Numbers_Range.py
@@ -1,1 +1,10 @@
 # 201 - Bitwise AND of Numbers Range
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        steps = 0
+        while left != right:
+            left >>= 1
+            right >>= 1
+            steps += 1
+        left <<= steps
+        return left
