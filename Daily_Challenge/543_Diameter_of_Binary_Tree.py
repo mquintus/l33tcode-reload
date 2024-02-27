ab17980d8196868ff0f9b08d372bd88a8d66126b
mquintus <mqs@stuebs.tech>
2024-02-26 22:37:30 -0800

    Update 543_Diameter_of_Binary_Tree.py

--- Daily_Challenge/543_Diameter_of_Binary_Tree.py
+++ Daily_Challenge/543_Diameter_of_Binary_Tree.py
@@ -1,1 +1,32 @@
 # 543 - Diameter of Binary Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        def solve(root):
+            if root is None:
+                return 0
+            return max(
+                solve(root.left),
+                solve(root.right),
+                depth(root.left) + depth(root.right)
+            )
+
+        def depth(root):
+            if root is None:
+                return 0
+            return 1 + max(depth(root.left), depth(root.right))
+
+        #print(
+        #        solve(root.left),
+        #        solve(root.right),
+        #        depth(root.left), 
+        #        depth(root.right)
+        #    )
+
+        return solve(root)
