02c6d89c42f89ddbcf6c749c3ecba7407c6bc7f0
mquintus <mqs@stuebs.tech>
2024-03-05 17:20:39 -0800

    Update 141_Linked_List_Cycle.py

--- Daily_Challenge/141_Linked_List_Cycle.py
+++ Daily_Challenge/141_Linked_List_Cycle.py
@@ -1,1 +1,12 @@
 # 141 - Linked List Cycle
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        return head.__str__() == 'Error - Found cycle in the ListNode'
+
+        
