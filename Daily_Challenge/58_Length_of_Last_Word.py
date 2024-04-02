0e207d75d9a6efc76368218c086d7841631d356d
mquintus <mqs@stuebs.tech>
2024-04-01 20:29:03 -0700

    Update 58_Length_of_Last_Word.py

--- Daily_Challenge/58_Length_of_Last_Word.py
+++ Daily_Challenge/58_Length_of_Last_Word.py
@@ -1,1 +1,24 @@
 # 58 - Length of Last Word
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        if len(s) < 2:
+            return True
+
+        translations = {}
+        used = set()
+        for i, el in enumerate(s):
+            el2 = t[i]
+            if el not in translations:
+                translations[el] = el2
+                if el2 in used:
+                    return False
+                used.add(el2)
+            else:
+                if translations[el] != el2:
+                    return False
+
+        return True
+        
+        
