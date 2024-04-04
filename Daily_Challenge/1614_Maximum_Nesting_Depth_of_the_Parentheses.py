79ec0886117dc70e07544c77b74f42f6e8a1f90d
mquintus <mqs@stuebs.tech>
2024-04-03 17:59:02 -0700

    Update 1614_Maximum_Nesting_Depth_of_the_Parentheses.py

--- Daily_Challenge/1614_Maximum_Nesting_Depth_of_the_Parentheses.py
+++ Daily_Challenge/1614_Maximum_Nesting_Depth_of_the_Parentheses.py
@@ -1,1 +1,12 @@
 # 1614 - Maximum Nesting Depth of the Parentheses
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        depth = 0
+        mdepth = 0
+        for el in s:
+            if el == '(':
+                depth += 1
+                mdepth = max(mdepth, depth)
+            elif el == ')':
+                depth -= 1
+        return mdepth
