dae2266b457719ead1a85286f8985aef7406e1e3
mquintus <mqs@stuebs.tech>
2024-04-05 21:18:05 -0700

    Update 1249_Minimum_Remove_to_Make_Valid_Parentheses.py

--- Daily_Challenge/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Daily_Challenge/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -1,1 +1,24 @@
 # 1249 - Minimum Remove to Make Valid Parentheses
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        counter = 0
+        p = -1
+        stack = []
+        while p < len(s)-1:
+            p += 1
+            if s[p] == ')' and counter <= 0:
+                continue
+            elif s[p] == ')':
+                counter -= 1
+            elif s[p] == '(':
+                counter += 1
+            stack.append(s[p])
+        
+        p = len(stack) 
+        while counter > 0:
+            if stack[p-1] == '(':
+                counter -= 1
+                del stack[p-1]
+            p -= 1
+
+        return "".join(stack)        
