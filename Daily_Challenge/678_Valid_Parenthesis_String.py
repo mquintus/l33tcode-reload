e2f0f3a105a32a713dbf69f1d6f93ec14416fab6
mquintus <mqs@stuebs.tech>
2024-04-06 18:13:26 -0700

    Update 678_Valid_Parenthesis_String.py

--- Daily_Challenge/678_Valid_Parenthesis_String.py
+++ Daily_Challenge/678_Valid_Parenthesis_String.py
@@ -1,1 +1,30 @@
 # 678 - Valid Parenthesis String
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        star_count = s.count("*")
+        left_count = s.count("(")
+        right_count = s.count(")")
+        if abs(left_count - right_count) > star_count:
+            return False
+        
+        opened = 0
+        for el in s:
+            if el in '(*':
+                opened += 1
+            elif el == ')':
+                if opened > 0:
+                    opened -= 1
+                else:
+                    return False
+        
+        opened = 0
+        for el in s[::-1]:
+            if el in '*)':
+                opened += 1
+            elif el == '(':
+                if opened > 0:
+                    opened -= 1
+                else:
+                    return False
+
+        return True
