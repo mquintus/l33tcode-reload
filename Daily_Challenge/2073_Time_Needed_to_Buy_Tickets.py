887c18c74377a1145b00dafbdcb09fb3cbdab785
mquintus <mqs@stuebs.tech>
2024-04-09 09:23:54 -0700

    Update 2073_Time_Needed_to_Buy_Tickets.py

--- Daily_Challenge/2073_Time_Needed_to_Buy_Tickets.py
+++ Daily_Challenge/2073_Time_Needed_to_Buy_Tickets.py
@@ -1,1 +1,17 @@
 # 2073 - Time Needed to Buy Tickets
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        seconds = 0
+        newlist = []
+        n = len(tickets)
+        i = -1
+        while True:
+            i = (i + 1) % n
+            if tickets[i] == 0:
+                continue
+
+            tickets[i] -= 1
+            seconds += 1
+
+            if i == k and tickets[i] == 0:
+                return seconds
