53a91c0a2a7ef75fa7a818393910bb5412c06d1b
mquintus <mqs@stuebs.tech>
2024-04-09 21:03:29 -0700

    Update 950_Reveal_Cards_In_Increasing_Order.py

--- Daily_Challenge/950_Reveal_Cards_In_Increasing_Order.py
+++ Daily_Challenge/950_Reveal_Cards_In_Increasing_Order.py
@@ -1,1 +1,17 @@
 # 950 - Reveal Cards In Increasing Order
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort()
+        n = len(deck)
+        newdeck = [i for i in range(n)]
+        newsort = []
+        while newdeck:
+            newsort.append(newdeck.pop(0))
+            if newdeck:
+                newdeck.append(newdeck.pop(0))
+
+        newdeck = [0 for i in range(n)]
+        for i in range(n):
+            newdeck[newsort[i]] = deck[i]
+
+        return newdeck
