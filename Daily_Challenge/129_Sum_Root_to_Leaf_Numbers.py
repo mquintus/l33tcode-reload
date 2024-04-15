3bfa2da2208bf3be1fe291907cd9c021ea377245
mquintus <mqs@stuebs.tech>
2024-04-15 10:32:28 -0700

    Update 129_Sum_Root_to_Leaf_Numbers.py

--- Daily_Challenge/129_Sum_Root_to_Leaf_Numbers.py
+++ Daily_Challenge/129_Sum_Root_to_Leaf_Numbers.py
@@ -1,1 +1,32 @@
 # 129 - Sum Root to Leaf Numbers
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        result = 0
+        currSum = 0
+
+        def recursion(root, currSum):
+            if root.left is None and root.right is None:
+                currSum *= 10
+                currSum += root.val
+                #print(root.val, currSum)
+                return currSum
+            
+            bufSum = 0
+            if root.left is not None:
+                bufSum += recursion(root.left, currSum * 10 + root.val)
+            if root.right is not None:
+                bufSum += recursion(root.right, currSum * 10 + root.val)
+
+            return bufSum 
+                
+        
+        return recursion(root, 0)
+
+
+
