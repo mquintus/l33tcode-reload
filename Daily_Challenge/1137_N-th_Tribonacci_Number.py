a1082b02673329cb743acd0fb868a18eab7dd5b3
mquintus <mqs@stuebs.tech>
2024-04-23 17:24:51 -0700

    Update 1137_N-th_Tribonacci_Number.py

--- Daily_Challenge/1137_N-th_Tribonacci_Number.py
+++ Daily_Challenge/1137_N-th_Tribonacci_Number.py
@@ -1,1 +1,16 @@
 # 1137 - N-th Tribonacci Number
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a = 0
+        b = 1
+        c = 1
+        if n == 0:
+            return a
+        if n == 1:
+            return b
+        if n == 2:
+            return c
+        for i in range(2,n):
+            a,b,c=b,c,a+b+c
+
+        return c
