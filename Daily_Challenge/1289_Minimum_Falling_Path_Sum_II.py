7827dcfcf018aa9d72c40cab7f407a5d2ca19f1c
mquintus <mqs@stuebs.tech>
2024-04-26 11:02:02 -0700

    Update 1289_Minimum_Falling_Path_Sum_II.py

--- Daily_Challenge/1289_Minimum_Falling_Path_Sum_II.py
+++ Daily_Challenge/1289_Minimum_Falling_Path_Sum_II.py
@@ -1,1 +1,21 @@
 # 1289 - Minimum Falling Path Sum II
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if n == 1:
+            return grid[0][0]
+        m = len(grid[0])
+        prev_row = [0] * m
+        curr_row = [0] * m
+        
+        for row in grid:
+            for i, cell in enumerate(row):
+                if i == 0:
+                    min_val = min(prev_row[i+1:])
+                elif i == m-1:
+                    min_val = min(prev_row[:i])
+                else:
+                    min_val = min([min(prev_row[:i]),min(prev_row[i+1:])])
+                curr_row[i] = cell + min_val
+            prev_row = [*curr_row]
+        return min(curr_row)
