443073abb60fb60ed42a59c50f553224942204fc
mquintus <mqs@stuebs.tech>
2024-05-07 22:48:58 -0700

    Update 506_Relative_Ranks.py

--- Daily_Challenge/506_Relative_Ranks.py
+++ Daily_Challenge/506_Relative_Ranks.py
@@ -1,1 +1,14 @@
 # 506 - Relative Ranks
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        scores = sorted([[b,a] for a,b in enumerate(score)])[::-1]
+        places = [[idx, rank] for rank, (points, idx) in enumerate(scores)]
+        places[0][1] = "Gold Medal"
+        if len(places) >= 2:
+            places[1][1] = "Silver Medal"
+        if len(places) >= 3:
+            places[2][1] = "Bronze Medal"
+        for i in range(3, len(places)):
+            places[i][1] = str(places[i][1]+1)
+        ranks = [rank for idx, rank in sorted(places)]
+        return ranks            
