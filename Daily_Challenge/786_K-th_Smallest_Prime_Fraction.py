8bdda3ff8ca9105403b9993e12492881a9b9d19d
mquintus <mqs@stuebs.tech>
2024-05-09 20:35:18 -0700

    Update 786_K-th_Smallest_Prime_Fraction.py

--- Daily_Challenge/786_K-th_Smallest_Prime_Fraction.py
+++ Daily_Challenge/786_K-th_Smallest_Prime_Fraction.py
@@ -1,1 +1,11 @@
 # 786 - K-th Smallest Prime Fraction
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        pairs = []
+        for i, a in enumerate(arr):
+            for b in arr[i+1:]:
+                pairs.append([a/b, a, b])
+        pairs.sort()
+        #print(pairs)
+        solution = pairs[k-1]
+        return [solution[1], solution[2]]
