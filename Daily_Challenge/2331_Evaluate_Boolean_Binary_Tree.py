098075ae4cca834fe3be2ce39363e99a02caf7aa
mquintus <mqs@stuebs.tech>
2024-05-16 11:18:29 -0700

    Update 2331_Evaluate_Boolean_Binary_Tree.py

--- Daily_Challenge/2331_Evaluate_Boolean_Binary_Tree.py
+++ Daily_Challenge/2331_Evaluate_Boolean_Binary_Tree.py
@@ -1,1 +1,19 @@
 # 2331 - Evaluate Boolean Binary Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        def evaluate(node):
+            if node.val == 0:
+                return False
+            if node.val == 1:
+                return True
+
+            my_operator = operator.__or__ if node.val == 2 else operator.__and__
+            return my_operator(evaluate(node.left), evaluate(node.right))
+
+        return evaluate(root)
