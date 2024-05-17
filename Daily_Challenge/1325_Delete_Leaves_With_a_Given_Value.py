e64deb5db6d8c15cd49eb148299689d5ee00c0df
mquintus <mqs@stuebs.tech>
2024-05-17 10:25:11 -0700

    Update 1325_Delete_Leaves_With_a_Given_Value.py

--- Daily_Challenge/1325_Delete_Leaves_With_a_Given_Value.py
+++ Daily_Challenge/1325_Delete_Leaves_With_a_Given_Value.py
@@ -1,1 +1,24 @@
 # 1325 - Delete Leaves With a Given Value
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        def recursiveFunction(node):
+            if node is None:
+                return None
+
+            node.left = recursiveFunction(node.left)
+            node.right = recursiveFunction(node.right)
+            if node.left or node.right:
+                return node
+
+            # This is a leaf node
+            if node.val == target:
+                return None
+            return node
+           
+        return recursiveFunction(root)
