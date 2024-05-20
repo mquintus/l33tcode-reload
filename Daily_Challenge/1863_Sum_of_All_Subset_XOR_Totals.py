bdb084541594eddb47ee6c6ca0dd805cc80605f5
mquintus <mqs@stuebs.tech>
2024-05-19 18:28:25 -0700

    Update 1863_Sum_of_All_Subset_XOR_Totals.py

--- Daily_Challenge/1863_Sum_of_All_Subset_XOR_Totals.py
+++ Daily_Challenge/1863_Sum_of_All_Subset_XOR_Totals.py
@@ -1,1 +1,9 @@
 # 1863 - Sum of All Subset XOR Totals
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        xortotal = 0
+        for length in range(1, len(nums)+1):
+            for subset in itertools.combinations(nums, length):
+                xor = reduce(operator.__xor__, subset)
+                xortotal += xor
+        return xortotal
