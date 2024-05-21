91651f73f3a7940fdd9f230859db720e4b6d32f3
mquintus <mqs@stuebs.tech>
2024-05-20 23:33:40 -0700

    Update 78_Subsets.py

--- Daily_Challenge/78_Subsets.py
+++ Daily_Challenge/78_Subsets.py
@@ -1,1 +1,9 @@
 # 78 - Subsets
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        all_subsets = set([()])
+        for length in range(len(nums)+1):
+            subsets = itertools.combinations(nums, length)
+            for s in subsets:
+                all_subsets.add(tuple(s))
+        return [list(s) for s in all_subsets]
