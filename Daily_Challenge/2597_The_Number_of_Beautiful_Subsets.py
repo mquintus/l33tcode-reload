f40e9752da5ed65c2dd07f0195fa217e24602fc1
mquintus <mqs@stuebs.tech>
2024-05-22 21:13:51 -0700

    Update 2597_The_Number_of_Beautiful_Subsets.py

--- Daily_Challenge/2597_The_Number_of_Beautiful_Subsets.py
+++ Daily_Challenge/2597_The_Number_of_Beautiful_Subsets.py
@@ -1,1 +1,19 @@
 # 2597 - The Number of Beautiful Subsets
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        cnt = [0 for _ in range(1002)]
+        n = len(nums)
+
+        def dfs(i):
+            if i == n:
+                return 1
+            take = 0
+            skip = dfs(i + 1)
+            if cnt[nums[i] - k] == 0:
+                 cnt[nums[i]] += 1
+                 take = dfs(i + 1)
+                 cnt[nums[i]] -= 1
+            return take + skip
+
+        return dfs(0) - 1
