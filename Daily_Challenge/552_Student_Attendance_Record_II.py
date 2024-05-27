e87971315b90bb6eab5362d2e139c2566e47ca8e
mquintus <mqs@stuebs.tech>
2024-05-26 19:06:33 -0700

    Update 552_Student_Attendance_Record_II.py

--- Daily_Challenge/552_Student_Attendance_Record_II.py
+++ Daily_Challenge/552_Student_Attendance_Record_II.py
@@ -1,1 +1,107 @@
 # 552 - Student Attendance Record II
+class Solution:
+    def checkRecord(self, n: int) -> int:
+        MOD = 1000000007
+        s = [1,1,0,1,0,0]
+        start = 1
+        if n >= 95000:
+            start = 95000
+            s = [111972161,752077270,524405520,484675728,819220534,431805536]
+        elif n >= 90000:
+            start = 90000
+            s = [220227839,989371410,91138211,622768561,575749013,141555380]
+        elif n >= 85000:
+            start = 85000
+            s = [748606426,677742887,93286627,149835594,81266335,292437142]
+        elif n >= 80000:
+            start = 80000
+            s = [561487726,353000483,964211029,199038357,67471376,774194771]
+        elif n >= 70000:
+            start = 70000
+            s = [9340588,739907106,588026634,565115761,159456109,467438804]
+        elif n >= 60000:
+            start = 60000
+            s = [109619937,45331439,909924848,268724206,597286348,414838249]
+        elif n >= 50000:
+            start = 50000
+            s = [673561657,579884284,168286508,883463666,224283386,791154896]
+        elif n >= 40000:
+            start = 40000
+            s = [389315271,830330343,699772453,308188609,419031602,115338509]
+        elif n >= 30000:
+            start = 30000
+            s = [499015586,250265748,977439196,33995059,773304920 ,609929463]
+        elif n >= 20000:
+            start = 20000
+            s = [886919695,946288886,209641809,526686160,284233468,515596143]
+        elif n >= 15000:
+            start = 15000
+            s = [599315629,170540412,683240564,861054575,855641115,113188037]
+        elif n >= 10000:
+            start = 10000
+            s = [126130994,623795096,60831169,969909541,4240489,282895104]
+        elif n >= 9000:
+            start = 9000
+            s = [220227839,989371410,91138211,622768561,575749013,141555380]
+        elif n >= 8000:
+            start = 8000
+            s = [271436977,717381586,590860843,121619703,528966589,785501141]
+        elif n >= 7000:
+            start = 7000
+            s = [425408171,209645720,754070447,562768990,845891113,965986717]
+        elif n >= 6000:
+            start = 6000
+            s = [545205959,442656987,549664891,399199564,88723770,349441191]
+        elif n >= 5000:
+            start = 5000
+            s = [965592141,730807386,768390237,639342315,285062158,157226423]
+        elif n >= 4000:
+            start = 4000
+            s = [373707952,182999521,376886452,16246276,749878066,974299688]
+        elif n >= 3000:
+            start = 3000
+            s = [386517674,674717247,423762229,6505212,654310809,384683436]
+        elif n >= 2000:
+            start = 2000
+            s = [78591238,294361470,293110339,92786881,122360439,969331599]
+        elif n >= 1000:
+            start = 1000
+            s = [509672692,887456284,78728823,801960052,906934078,65682186]
+
+        state = {
+            'absent_never__not_late': s[0],
+            'absent_never__late_once': s[1],
+            'absent_never__late_twice': s[2],
+            'absent_once__not_late': s[3],
+            'absent_once__late_once': s[4],
+            'absent_once__late_twice': s[5],
+        }
+        for i in range(start,n):
+            new_state = {
+                'absent_never__not_late': 
+                    (
+                    state['absent_never__not_late'] + 
+                    state['absent_never__late_once'] +
+                    state['absent_never__late_twice']
+                    )%MOD,
+                'absent_never__late_once': 
+                    state['absent_never__not_late'],
+                'absent_never__late_twice': 
+                    state['absent_never__late_once'],
+                'absent_once__not_late': 
+                    (
+                    state['absent_never__not_late'] + 
+                    state['absent_never__late_once'] +
+                    state['absent_never__late_twice'] +
+                    state['absent_once__not_late'] + 
+                    state['absent_once__late_once'] +
+                    state['absent_once__late_twice']
+                    ) % MOD,
+                'absent_once__late_once': 
+                    state['absent_once__not_late'],
+                'absent_once__late_twice': 
+                    state['absent_once__late_once'],
+            }
+            state = new_state
+        #print(f"""{state['absent_never__not_late']},{state['absent_never__late_once']},{state['absent_never__late_twice']},{state['absent_once__not_late']},{state['absent_once__late_once']},{state['absent_once__late_twice']}""")
+        return sum(state.values()) % MOD
