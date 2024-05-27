c8ca092334c950437ecd94bd70e95919704ec30d
mquintus <mqs@stuebs.tech>
2024-05-26 19:05:38 -0700

    Update 1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py

--- Daily_Challenge/1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py
+++ Daily_Challenge/1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py
@@ -1,1 +1,16 @@
 # 1608 - Special Array With X Elements Greater Than or Equal X
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        l = 0
+        counter = len(nums)+1
+
+        while counter >= l:
+            #print(l)
+            counter = 0
+            for i in nums:
+                if i >= l:
+                    counter += 1
+            if counter == l:
+                return counter
+            l += 1
+        return -1
