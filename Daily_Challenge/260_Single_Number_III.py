627cb522faef5f71f7de139939b6d41b448afc21
mquintus <mqs@stuebs.tech>
2024-05-31 23:59:39 +0200

    Update 260_Single_Number_III.py

--- Daily_Challenge/260_Single_Number_III.py
+++ Daily_Challenge/260_Single_Number_III.py
@@ -1,1 +1,18 @@
 # 260 - Single Number III
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        globalXOR = reduce(operator.__xor__, nums)
+        p = 1
+        smallestBit = p & globalXOR
+        while smallestBit == 0:
+            p <<= 1
+            smallestBit = p & globalXOR
+
+        groupAxor = 0
+        groupBxor = 0
+        for el in nums:
+            if el & p:
+                groupAxor ^= el
+            else:
+                groupBxor ^= el
+        return [groupAxor, groupBxor]
