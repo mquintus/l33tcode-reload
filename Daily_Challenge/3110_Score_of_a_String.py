4711b962d3e1725b85a8a4dddc3fc837955c0c3d
mquintus <mqs@stuebs.tech>
2024-06-01 08:55:27 +0200

    Update 3110_Score_of_a_String.py

--- Daily_Challenge/3110_Score_of_a_String.py
+++ Daily_Challenge/3110_Score_of_a_String.py
@@ -1,1 +1,9 @@
 # 3110 - Score of a String
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        a = ord(s[0])
+        for b in s[1:]:
+            score += abs(a-ord(b))
+            a = ord(b)
+        return score
