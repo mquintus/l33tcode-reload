460332ff0c01cbe87078e99909902f48a6be1e80
mquintus <mqs@stuebs.tech>
2024-06-04 06:56:53 +0200

    Update 409_Longest_Palindrome.py

--- Daily_Challenge/409_Longest_Palindrome.py
+++ Daily_Challenge/409_Longest_Palindrome.py
@@ -1,1 +1,12 @@
 # 409 - Longest Palindrome
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counts = Counter(s)
+        has_indiv = False
+        result = 0
+        for value in counts.values():
+            result += 2 * (value // 2)
+            if not has_indiv and value % 2 == 1:
+                result += 1
+                has_indiv = True
+        return result
