0016cf8c5909c83c7aa48caee6d83b2777d8988a
mquintus <mqs@stuebs.tech>
2024-06-06 11:42:30 +0200

    Update 846_Hand_of_Straights.py

--- Daily_Challenge/846_Hand_of_Straights.py
+++ Daily_Challenge/846_Hand_of_Straights.py
@@ -1,1 +1,14 @@
 # 846 - Hand of Straights
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        from sortedcontainers import SortedDict 
+        #hand.sort()
+        handCounts = SortedDict(Counter(hand))
+        for card, count in handCounts.items():
+            if count == 0:
+                continue
+            for i in range(card, card+groupSize):
+                if i not in handCounts.keys() or handCounts[i] < count:
+                    return False
+                handCounts[i] -= count
+        return True
