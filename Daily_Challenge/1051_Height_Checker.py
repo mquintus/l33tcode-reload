b998036b299c32bea57d24682216e54d2c86b0d8
mquintus <mqs@stuebs.tech>
2024-06-10 13:35:37 +0200

    Update 1051_Height_Checker.py

--- Daily_Challenge/1051_Height_Checker.py
+++ Daily_Challenge/1051_Height_Checker.py
@@ -1,1 +1,9 @@
 # 1051 - Height Checker
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        hsorted = sorted(heights)
+        c = 0
+        for i in range(len(heights)):
+            if hsorted[i] != heights[i]:
+                c+=1
+        return c
