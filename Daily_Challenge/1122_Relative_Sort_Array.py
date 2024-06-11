6ed1863bc0729f6dc5bb996d44c2044da307b95c
mquintus <mqs@stuebs.tech>
2024-06-11 22:46:51 +0200

    Update 1122_Relative_Sort_Array.py

--- Daily_Challenge/1122_Relative_Sort_Array.py
+++ Daily_Challenge/1122_Relative_Sort_Array.py
@@ -1,1 +1,12 @@
 # 1122 - Relative Sort Array
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        set2 = set(arr2)
+        pos2 = {val: pos for pos, val in enumerate(arr2)}
+        arr3 = sorted([val for val in arr1 if val not in set2])
+
+        result = [val for val in arr1 if val in set2]
+        result.sort(key=lambda val: pos2[val])
+        result.extend(arr3)
+
+        return result
