804f0dab1f67e563f6dbbd78b7ab4b2990d8a559
mquintus <mqs@stuebs.tech>
2024-06-13 13:35:02 +0200

    Update 2037_Minimum_Number_of_Moves_to_Seat_Everyone.py

--- Daily_Challenge/2037_Minimum_Number_of_Moves_to_Seat_Everyone.py
+++ Daily_Challenge/2037_Minimum_Number_of_Moves_to_Seat_Everyone.py
@@ -1,1 +1,6 @@
 # 2037 - Minimum Number of Moves to Seat Everyone
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        return sum([abs(seats[i] - students[i]) for i in range(len(seats))])
