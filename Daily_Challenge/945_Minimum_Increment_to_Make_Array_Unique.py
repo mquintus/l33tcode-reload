846ec90770c2a056726aeccebe3a8bca7b27d1f9
mquintus <mqs@stuebs.tech>
2024-06-14 14:13:15 +0200

    Update 945_Minimum_Increment_to_Make_Array_Unique.py

--- Daily_Challenge/945_Minimum_Increment_to_Make_Array_Unique.py
+++ Daily_Challenge/945_Minimum_Increment_to_Make_Array_Unique.py
@@ -1,1 +1,15 @@
 # 945 - Minimum Increment to Make Array Unique
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        steps = 0
+        nums.sort()
+        prev = nums[0]
+        for i in range(1, len(nums)):
+            req = prev + 1
+            missing = (req - nums[i])
+            if missing > 0:
+                steps += missing
+                prev = req
+            else:
+                prev = nums[i]
+        return steps
