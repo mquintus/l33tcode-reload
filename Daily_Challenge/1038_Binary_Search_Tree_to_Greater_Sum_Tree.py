1152452492b1bb94a330bda8f57c6680c55cb5c7
mquintus <mqs@stuebs.tech>
2024-06-25 10:20:33 +0200

    Update 1038_Binary_Search_Tree_to_Greater_Sum_Tree.py

--- Daily_Challenge/1038_Binary_Search_Tree_to_Greater_Sum_Tree.py
+++ Daily_Challenge/1038_Binary_Search_Tree_to_Greater_Sum_Tree.py
@@ -1,1 +1,24 @@
 # 1038 - Binary Search Tree to Greater Sum Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+
+        def getVal(root, val=0):
+            if root.right:
+                val = getVal(root.right, val)
+            
+            root.val += val
+            val = root.val
+            
+            if root.left:
+                val = getVal(root.left, val)
+            
+            return val
+        
+        getVal(root)
+        return root
