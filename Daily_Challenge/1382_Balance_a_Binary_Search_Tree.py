597d40056c6b7fd3b9eab3c4a51f869d0a32f1e1
mquintus <mqs@stuebs.tech>
2024-06-26 11:41:04 +0200

    Update 1382_Balance_a_Binary_Search_Tree.py

--- Daily_Challenge/1382_Balance_a_Binary_Search_Tree.py
+++ Daily_Challenge/1382_Balance_a_Binary_Search_Tree.py
@@ -1,1 +1,32 @@
 # 1382 - Balance a Binary Search Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        allnodes = []
+        def rec(node):
+            if node.left:
+                rec(node.left)
+            if node.right:
+                rec(node.right)
+            nonlocal allnodes
+            allnodes.append(node.val)
+
+        rec(root)
+        allnodes.sort()
+        def generate(left, right):
+            if left >= right:
+                return None
+            
+            mid = (left + right) // 2
+            newroot = TreeNode(
+                allnodes[mid],
+                generate(left, mid),
+                generate(mid+1, right)
+            )
+            return newroot
+        return generate(0, len(allnodes))
