609e3c32fe6e002800b3c7c2c3ff5fb6679b5996
mquintus <mqs@stuebs.tech>
2024-06-28 12:21:24 +0200

    Update 2285_Maximum_Total_Importance_of_Roads.py

--- Daily_Challenge/2285_Maximum_Total_Importance_of_Roads.py
+++ Daily_Challenge/2285_Maximum_Total_Importance_of_Roads.py
@@ -1,1 +1,12 @@
 # 2285 - Maximum Total Importance of Roads
+class Solution:
+    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        degree = [0] * n
+        for f, t in roads:
+            degree[f] += 1
+            degree[t] += 1
+        degree.sort()
+        fullscore = 0
+        for points, multiplier in enumerate(degree):
+            fullscore += (points+1) * multiplier
+        return fullscore
