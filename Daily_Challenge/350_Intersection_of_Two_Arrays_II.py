2e9dd75c114f8a8bc730e678f9600aaa81efc74b
mquintus <mqs@stuebs.tech>
2024-07-02 20:00:32 +0200

    Update 350_Intersection_of_Two_Arrays_II.py

--- Daily_Challenge/350_Intersection_of_Two_Arrays_II.py
+++ Daily_Challenge/350_Intersection_of_Two_Arrays_II.py
@@ -1,1 +1,12 @@
 # 350 - Intersection of Two Arrays II
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        C2 = Counter(nums2)
+        for k,v in Counter(nums1).items():
+            if k not in C2:
+                continue
+            v = min(v, C2[k])
+            if v > 0:
+                result += [k]*v
+        return result
