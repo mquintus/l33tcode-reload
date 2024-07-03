2e474c2745f680e5db563b2239255b2e988bee02
mquintus <mqs@stuebs.tech>
2024-07-03 17:46:42 +0200

    Update 1509_Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.py

--- Daily_Challenge/1509_Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.py
+++ Daily_Challenge/1509_Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.py
@@ -1,1 +1,21 @@
 # 1509 - Minimum Difference Between Largest and Smallest Value in Three Moves
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n < 5:
+            return 0
+
+        bestDistance = float('inf')
+        nums.sort()
+        for k in range(0,4):
+            smallest = nums[k]
+            biggest =  nums[n-4+k]
+            #print(k, smallest)
+            #print(n-4+k, biggest)
+            distance = biggest - smallest
+            bestDistance = min([distance, bestDistance])
+
+        return bestDistance
+        
+        
