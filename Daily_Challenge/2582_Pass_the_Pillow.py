148123e2dff2940a33988d8e4ad616e28114ff1b
mquintus <mqs@stuebs.tech>
2024-07-06 21:45:50 +0200

    Update 2582_Pass_the_Pillow.py

--- Daily_Challenge/2582_Pass_the_Pillow.py
+++ Daily_Challenge/2582_Pass_the_Pillow.py
@@ -1,1 +1,8 @@
 # 2582 - Pass the Pillow
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        time = time % (2 * (n-1))
+        solution = 1 + time
+        if time > n-1:
+            solution = 2 * n - time - 1
+        return solution
