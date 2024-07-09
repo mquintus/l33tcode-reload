22690b0b79c602787917e42a2f15e62e3cac1d37
mquintus <mqs@stuebs.tech>
2024-07-09 10:52:32 +0200

    Update 1701_Average_Waiting_Time.py

--- Daily_Challenge/1701_Average_Waiting_Time.py
+++ Daily_Challenge/1701_Average_Waiting_Time.py
@@ -1,1 +1,13 @@
 # 1701 - Average Waiting Time
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        busyUntil = 0
+        overallWaitingTime = 0
+        for arrival, difficulty in customers:
+            waitingTime = difficulty
+            if busyUntil > arrival:
+                waitingTime += busyUntil - arrival
+            busyUntil = max(arrival, busyUntil) + difficulty
+            print(waitingTime)
+            overallWaitingTime += waitingTime
+        return overallWaitingTime / len(customers)
