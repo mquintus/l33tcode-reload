7d3bd255491a644f169774cc1f179eb863ed1234
mquintus <mqs@stuebs.tech>
2024-07-10 13:12:51 +0200

    Update 1598_Crawler_Log_Folder.py

--- Daily_Challenge/1598_Crawler_Log_Folder.py
+++ Daily_Challenge/1598_Crawler_Log_Folder.py
@@ -1,1 +1,12 @@
 # 1598 - Crawler Log Folder
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        depth = 0
+        for operation in logs:
+            if operation == '../':
+                depth = max(0, depth-1)
+            elif operation == './':
+                pass
+            else:
+                depth += 1
+        return depth
