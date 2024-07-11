48325196b621f7c4482295e546c452b6cc9df82d
mquintus <mqs@stuebs.tech>
2024-07-11 22:38:59 +0200

    Update 1190_Reverse_Substrings_Between_Each_Pair_of_Parentheses.py

--- Daily_Challenge/1190_Reverse_Substrings_Between_Each_Pair_of_Parentheses.py
+++ Daily_Challenge/1190_Reverse_Substrings_Between_Each_Pair_of_Parentheses.py
@@ -1,1 +1,21 @@
 # 1190 - Reverse Substrings Between Each Pair of Parentheses
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        def rev(start):
+            nonlocal s
+            result = ""
+            i = start
+            while i < len(s):
+                if s[i] == '(':
+                    localresult = '(' + rev(i+1)
+                    i += len(localresult)
+                    result += localresult[::-1]
+                elif s[i] == ')':
+                    return result + ')'
+                else:
+                    result += s[i]
+                    i += 1
+            return result
+                
+        returnValue = rev(0).replace("(","").replace(")","")
+        return returnValue
