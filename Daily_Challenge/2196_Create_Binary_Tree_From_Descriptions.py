df4a1a2ecff1e4a68d141a96c8a70bb1cdb3b51a
mquintus <mqs@stuebs.tech>
2024-07-15 12:13:24 +0200

    Update 2196_Create_Binary_Tree_From_Descriptions.py

--- Daily_Challenge/2196_Create_Binary_Tree_From_Descriptions.py
+++ Daily_Challenge/2196_Create_Binary_Tree_From_Descriptions.py
@@ -1,1 +1,30 @@
 # 2196 - Create Binary Tree From Descriptions
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        hashmap = {}
+        hasparent = set()
+        isparent = set()
+        for parent, child, isLeft in descriptions:
+            if child not in hashmap:
+                hashmap[child] = TreeNode(child)
+            hasparent.add(child)
+
+            if parent not in hashmap:
+                hashmap[parent] = TreeNode(parent)
+            isparent.add(parent)
+            if isLeft:
+                hashmap[parent].left = hashmap[child]
+            else:
+                hashmap[parent].right = hashmap[child]
+        
+        for node in isparent:
+            if node not in hasparent: 
+                break        
+                
+        return hashmap[node]
