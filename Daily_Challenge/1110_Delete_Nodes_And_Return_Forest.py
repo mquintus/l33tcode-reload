138e9d2ce476fd03a54ff8794a05532cb3b38aea
mquintus <mqs@stuebs.tech>
2024-07-17 13:32:12 +0200

    Update 1110_Delete_Nodes_And_Return_Forest.py

--- Daily_Challenge/1110_Delete_Nodes_And_Return_Forest.py
+++ Daily_Challenge/1110_Delete_Nodes_And_Return_Forest.py
@@ -1,1 +1,38 @@
 # 1110 - Delete Nodes And Return Forest
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        def populateForest(root, isroot, forest):
+            # Returns if this root is deleted.
+            nonlocal to_delete
+            if root is None:
+                return True
+            
+            deletethis = root.val in to_delete
+            if isroot and not deletethis:
+                forest.append(root)
+
+            childIsRoot = False
+            if deletethis:
+                childIsRoot = True
+
+            deleteLeft = populateForest(root.left, childIsRoot, forest)
+            if deleteLeft:
+                root.left = None
+            
+            deleteRight = populateForest(root.right, childIsRoot, forest)        
+            if deleteRight:
+                root.right = None
+
+            return deletethis
+            
+        forest = []
+        populateForest(root, True, forest)
+        return forest
+
+            
