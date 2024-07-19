5ab5560fdac694e633bb0680fb6d84eac6339f17
mquintus <mqs@stuebs.tech>
2024-07-19 12:25:06 +0200

    Update 1380_Lucky_Numbers_in_a_Matrix.py

--- Daily_Challenge/1380_Lucky_Numbers_in_a_Matrix.py
+++ Daily_Challenge/1380_Lucky_Numbers_in_a_Matrix.py
@@ -1,1 +1,13 @@
 # 1380 - Lucky Numbers in a Matrix
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        lucky = []
+        minPerRow = set()
+        for row in matrix:
+            minPerRow.add(min(row))
+        for i in range(len(matrix[0])):
+            col = [row[i] for row in matrix]
+            maxPerCol = max(col)
+            if maxPerCol in minPerRow:
+                lucky.append(maxPerCol)
+        return lucky
