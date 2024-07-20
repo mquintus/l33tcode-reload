9110c3ff676b53c5ae5d81cbc3d4d5e81e4fbbaf
mquintus <mqs@stuebs.tech>
2024-07-20 19:37:37 +0200

    Update 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.py

--- Daily_Challenge/1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.py
+++ Daily_Challenge/1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.py
@@ -1,1 +1,19 @@
 # 1605 - Find Valid Matrix Given Row and Column Sums
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        n = len(rowSum)
+        m = len(colSum)
+        matrix = [[0] * m for _ in range(n)]
+
+        rowId = 0
+        colId = 0
+        while rowId < n and colId < m:
+            val = min([rowSum[rowId], colSum[colId]]) 
+            rowSum[rowId] -= val
+            colSum[colId] -= val
+            matrix[rowId][colId] = val
+            if rowSum[rowId] == 0:
+                rowId += 1
+            if colSum[colId] == 0:
+                colId += 1
+        return matrix
