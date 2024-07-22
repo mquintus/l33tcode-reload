a8e44d8b4b8e2c9ea9a2429769a4646175900bf2
mquintus <mqs@stuebs.tech>
2024-07-22 07:22:34 +0200

    Update 2418_Sort_the_People.py

--- Daily_Challenge/2418_Sort_the_People.py
+++ Daily_Challenge/2418_Sort_the_People.py
@@ -1,1 +1,7 @@
 # 2418 - Sort the People
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        name_height = list(zip(heights, names))
+        name_height.sort()
+        name_height = name_height[::-1]
+        return [name for _, name in name_height]
