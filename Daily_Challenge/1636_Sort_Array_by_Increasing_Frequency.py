c03c6a58ce3ba044ae0908033340681bac0ce0f5
mquintus <mqs@stuebs.tech>
2024-07-23 09:19:50 +0200

    Update 1636_Sort_Array_by_Increasing_Frequency.py

--- Daily_Challenge/1636_Sort_Array_by_Increasing_Frequency.py
+++ Daily_Challenge/1636_Sort_Array_by_Increasing_Frequency.py
@@ -1,1 +1,8 @@
 # 1636 - Sort Array by Increasing Frequency
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        counts = sorted([(x,-y) for y,x in Counter(nums).items()])
+        full = []
+        for count, val in counts:
+            full += [-val] * count
+        return full
