523294426f93a0c8062625165df29cbdf77ba325
mquintus <mqs@stuebs.tech>
2024-07-24 10:14:57 +0200

    Update 2191_Sort_the_Jumbled_Numbers.py

--- Daily_Challenge/2191_Sort_the_Jumbled_Numbers.py
+++ Daily_Challenge/2191_Sort_the_Jumbled_Numbers.py
@@ -1,1 +1,7 @@
 # 2191 - Sort the Jumbled Numbers
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        mapping = {str(i):str(v) for i,v in enumerate(mapping)}
+        trans = str.maketrans(mapping)
+        nums.sort(key=lambda i:int(i.__str__().translate(trans)))
+        return nums
