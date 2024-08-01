a6249e25cec0ae8771d03481e051d24b7f308ae1
mquintus <mqs@stuebs.tech>
2024-08-01 11:43:14 +0200

    Update 2678_Number_of_Senior_Citizens.py

--- Daily_Challenge/2678_Number_of_Senior_Citizens.py
+++ Daily_Challenge/2678_Number_of_Senior_Citizens.py
@@ -1,1 +1,4 @@
 # 2678 - Number of Senior Citizens
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        return sum([1 for citizen in details if int(citizen[11:13]) > 60])
