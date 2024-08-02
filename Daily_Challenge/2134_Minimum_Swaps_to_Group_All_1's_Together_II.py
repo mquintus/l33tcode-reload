29ba93991f2262930b8f6401370a671e4f545525
mquintus <mqs@stuebs.tech>
2024-08-02 12:50:30 +0200

    Update 2134_Minimum_Swaps_to_Group_All_1's_Together_II.py

--- Daily_Challenge/2134_Minimum_Swaps_to_Group_All_1's_Together_II.py
+++ Daily_Challenge/2134_Minimum_Swaps_to_Group_All_1's_Together_II.py
@@ -1,1 +1,16 @@
 # 2134 - Minimum Swaps to Group All 1's Together II
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        groupSize = sum(nums)
+        n = len(nums)
+
+        curr_sum = sum(nums[-groupSize:])
+        max_sum = curr_sum
+        prev_pos = -groupSize
+        for i in range(n):
+            curr_sum += nums[i]
+            curr_sum -= nums[prev_pos]
+            prev_pos += 1
+            max_sum = max(max_sum, curr_sum)
+        
+        return groupSize - max_sum
