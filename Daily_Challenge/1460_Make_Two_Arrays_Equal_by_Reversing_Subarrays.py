1c5e5313be0b9c1b6548f4a4229816b6385ac03c
mquintus <mqs@stuebs.tech>
2024-08-03 10:53:53 +0200

    Update 1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays.py

--- Daily_Challenge/1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays.py
+++ Daily_Challenge/1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays.py
@@ -1,1 +1,4 @@
 # 1460 - Make Two Arrays Equal by Reversing Subarrays
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return sorted(target) == sorted(arr)
