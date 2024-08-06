c7f1a0351854ad1daea01f714ac1149a1695066a
mquintus <mqs@stuebs.tech>
2024-08-06 08:45:08 +0200

    Update 3016_Minimum_Number_of_Pushes_to_Type_Word_II.py

--- Daily_Challenge/3016_Minimum_Number_of_Pushes_to_Type_Word_II.py
+++ Daily_Challenge/3016_Minimum_Number_of_Pushes_to_Type_Word_II.py
@@ -1,1 +1,14 @@
 # 3016 - Minimum Number of Pushes to Type Word II
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        counter = Counter(word)
+        curr_space = 8
+        curr_length = 1
+        result = 0
+        for occ, val in sorted([[o,v] for v,o in counter.items()])[::-1]:
+            result += occ * curr_length
+            curr_space -= 1
+            if curr_space == 0:
+                curr_space = 8
+                curr_length += 1
+        return result
