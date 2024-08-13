d8e4a6d5a9ce8d8eb1338044b3503fd92ac4a3c2
mquintus <mqs@stuebs.tech>
2024-08-13 14:21:57 +0200

    Update 40_Combination_Sum_II.py

--- Daily_Challenge/40_Combination_Sum_II.py
+++ Daily_Challenge/40_Combination_Sum_II.py
@@ -1,1 +1,31 @@
 # 40 - Combination Sum II
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        n = len(candidates)
+        solutions = set()
+
+        def take_or_skip(i, currsum, values):
+            if currsum == target:
+                solutions.add(tuple(sorted(values)))
+                return
+
+            if currsum > target:
+                return
+            
+            if i >= n:
+                return
+
+            # take 
+            if currsum+candidates[i] <= target:
+                values.append(candidates[i])
+                take_or_skip(i+1, currsum+candidates[i], values)
+                values.pop()
+
+            # skip
+            while i < n - 1 and candidates[i] == candidates[i+1]:
+                i += 1
+            take_or_skip(i+1, currsum, values)
+
+        take_or_skip(0, 0, [])
+        return list(solutions)
