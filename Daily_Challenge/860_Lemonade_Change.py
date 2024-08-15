bbf9bc0d3ecb8d1a06ee795132cf5c627a09da58
mquintus <mqs@stuebs.tech>
2024-08-15 10:40:21 +0200

    Update 860_Lemonade_Change.py

--- Daily_Challenge/860_Lemonade_Change.py
+++ Daily_Challenge/860_Lemonade_Change.py
@@ -1,1 +1,21 @@
 # 860 - Lemonade Change
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        counts = [0,0]
+        for customer in bills:
+            if customer == 5:
+                counts[0] += 1
+            elif customer == 10:
+                if counts[0] == 0:
+                    return False
+                counts[0] -= 1
+                counts[1] += 1
+            else: #paying with 20
+                if counts[0] == 0: return False
+                if counts[1] == 0 and counts[0] < 3: return False
+                if counts[1] == 0:
+                    counts[0] -= 3
+                else:
+                    counts[1] -= 1
+                    counts[0] -= 1
+        return True
