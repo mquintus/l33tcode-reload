2a9b6b1a68f3659a56d8ecbbc36b30a7252684f4
mquintus <mqs@stuebs.tech>
2024-08-18 12:28:56 +0200

    Update 264_Ugly_Number_II.py

--- Daily_Challenge/264_Ugly_Number_II.py
+++ Daily_Challenge/264_Ugly_Number_II.py
@@ -1,1 +1,16 @@
 # 264 - Ugly Number II
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        visited = set()
+        pipe = [1]
+        for i in range(n):
+            curr = heapq.heappop(pipe)
+            if len(pipe) < n:
+                for m in [2,3,5]:
+                    nexxt = curr*m
+                    if nexxt not in visited:
+                        heapq.heappush(pipe, nexxt)
+                        visited.add(nexxt)
+            #pipe.sort()
+            #print(pipe)
+        return curr
