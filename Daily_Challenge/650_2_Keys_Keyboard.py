f999ad97a81cf38e9e9dad383b55ccac4410b3c5
mquintus <mqs@stuebs.tech>
2024-08-19 11:53:41 +0200

    Update 650_2_Keys_Keyboard.py

--- Daily_Challenge/650_2_Keys_Keyboard.py
+++ Daily_Challenge/650_2_Keys_Keyboard.py
@@ -1,1 +1,31 @@
 # 650 - 2 Keys Keyboard
+class Solution:
+    def minSteps(self, n: int) -> int:
+        @cache
+        def recursive(length, clipboard):
+            if length == n:
+                return 0
+            if length > n:
+                return float('inf')
+            
+            if length == clipboard:
+                # don't copy! paste
+                return 1 + recursive(length + clipboard, clipboard)
+
+            # copy
+            copy = 1 + recursive(length, length)
+            # if clipboard is empty, only option is to copy           
+            if clipboard == 0:
+                return copy
+
+            # paste
+            paste = 1 + recursive(length + clipboard, clipboard)
+
+            return min(copy, paste)
+
+        return recursive(1,0)
+
+            
+
+
+            
