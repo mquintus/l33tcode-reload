ded26df332f279d91317210310e21ab669ac6178
mquintus <mqs@stuebs.tech>
2024-08-22 10:00:48 +0200

    Update 476_Number_Complement.py

--- Daily_Challenge/476_Number_Complement.py
+++ Daily_Challenge/476_Number_Complement.py
@@ -1,1 +1,7 @@
 # 476 - Number Complement
+class Solution:
+    def findComplement(self, num: int) -> int:
+        base = 2
+        while base <= num:
+            base *= 2
+        return (base-1)-num
