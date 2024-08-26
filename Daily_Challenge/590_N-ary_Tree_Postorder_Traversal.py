ca3dbda2d2cce87a2aaaeefb57ff7228f7c52a24
mquintus <mqs@stuebs.tech>
2024-08-26 09:50:58 +0200

    Update 590_N-ary_Tree_Postorder_Traversal.py

--- Daily_Challenge/590_N-ary_Tree_Postorder_Traversal.py
+++ Daily_Challenge/590_N-ary_Tree_Postorder_Traversal.py
@@ -6,23 +6,20 @@ class Node:
         self.val = val
         self.children = children
 """
-
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         stack = []
-        stack.append(root)
+        stack.append((root,False))
         solution = []
         while stack:
-            node = stack.pop()
+            node,visited = stack.pop()
             if node is None:
                 continue
-            if len(node.children) == 0:
+            if visited:
                 solution.append(node.val)
                 continue
-            stack.append(node)
+            stack.append((node, True))
             for child in node.children[::-1]:
-                stack.append(child)
-            node.children = []
+                stack.append((child,False))
             
         return solution
-
