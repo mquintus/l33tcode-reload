a83e565f69c7379dc02d5c0ee5ab92ad3d6ccc33
mquintus <mqs@stuebs.tech>
2024-09-02 09:46:58 +0200

    Update 1894_Find_the_Student_that_Will_Replace_the_Chalk.py

--- Daily_Challenge/1894_Find_the_Student_that_Will_Replace_the_Chalk.py
+++ Daily_Challenge/1894_Find_the_Student_that_Will_Replace_the_Chalk.py
@@ -1,1 +1,12 @@
 # 1894 - Find the Student that Will Replace the Chalk
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        #First calculate as many *full* rounds as possible
+        full_round = sum(chalk)
+        k %= full_round
+
+        # Now let's simulate the last round.
+        for student, amount in enumerate(chalk):
+            k -= amount
+            if k < 0:
+                return student
