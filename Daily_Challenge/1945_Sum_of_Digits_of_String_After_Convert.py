2ea74c760d24e2812f7e16c359db787e46154343
mquintus <mqs@stuebs.tech>
2024-09-04 09:26:06 +0200

    Update 1945_Sum_of_Digits_of_String_After_Convert.py

--- Daily_Challenge/1945_Sum_of_Digits_of_String_After_Convert.py
+++ Daily_Challenge/1945_Sum_of_Digits_of_String_After_Convert.py
@@ -1,1 +1,18 @@
 # 1945 - Sum of Digits of String After Convert
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        dsum = ""
+        a = ord('a')
+        for c in s:
+            dsum += str(ord(c) - a + 1)
+        
+        def digitsum(s):
+            dsum = 0
+            for c in s:
+                dsum += int(c)
+            return dsum
+
+        for _ in range(k):
+            dsum = str(digitsum(dsum))
+
+        return int(dsum)
