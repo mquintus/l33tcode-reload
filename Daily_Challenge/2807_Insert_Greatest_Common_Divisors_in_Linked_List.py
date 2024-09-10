b9672694cf3580358148e7c3a4373229df732baa
mquintus <mqs@stuebs.tech>
2024-09-10 10:07:59 +0200

    Update 2807_Insert_Greatest_Common_Divisors_in_Linked_List.py

--- Daily_Challenge/2807_Insert_Greatest_Common_Divisors_in_Linked_List.py
+++ Daily_Challenge/2807_Insert_Greatest_Common_Divisors_in_Linked_List.py
@@ -1,1 +1,20 @@
 # 2807 - Insert Greatest Common Divisors in Linked List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        prev = node
+        node = node.next
+        while node:
+            newNode = ListNode(
+                 math.gcd(node.val, prev.val),
+                 node
+            )
+            prev.next = newNode
+            prev = node
+            node = node.next
+        return head
