4a26b0951db42cf9f784f38d3a91f8f864bf8bf7
mquintus <mqs@stuebs.tech>
2024-09-12 09:09:29 +0200

    Update 1684_Count_the_Number_of_Consistent_Strings.py

--- Daily_Challenge/1684_Count_the_Number_of_Consistent_Strings.py
+++ Daily_Challenge/1684_Count_the_Number_of_Consistent_Strings.py
@@ -1,1 +1,15 @@
 # 1684 - Count the Number of Consistent Strings
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        allow = [False] * 200
+        for c in allowed:
+            allow[ord(c)] = True
+        del c
+        counter = 0
+        for word in words:
+            for char in word:
+                if not allow[ord(char)]:
+                    break
+            else:
+                counter += 1
+        return counter
