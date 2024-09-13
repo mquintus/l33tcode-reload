10cc7daf98cf2867380733f41f00802d011d6203
mquintus <mqs@stuebs.tech>
2024-09-13 11:14:02 +0200

    Update 1310_XOR_Queries_of_a_Subarray.py

--- Daily_Challenge/1310_XOR_Queries_of_a_Subarray.py
+++ Daily_Challenge/1310_XOR_Queries_of_a_Subarray.py
@@ -1,1 +1,11 @@
 # 1310 - XOR Queries of a Subarray
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        left = [0]
+        for el in arr:
+            left.append(left[-1]^el)
+      
+        result = []
+        for fr, to in queries:
+            result.append(left[fr]^left[to+1])
+        return result
