e5b99a80c4b55f7779744863d2d1981789aaa94a
mquintus <mqs@stuebs.tech>
2024-09-14 11:09:14 +0200

    Update 2419_Longest_Subarray_With_Maximum_Bitwise_AND.py

--- Daily_Challenge/2419_Longest_Subarray_With_Maximum_Bitwise_AND.py
+++ Daily_Challenge/2419_Longest_Subarray_With_Maximum_Bitwise_AND.py
@@ -1,1 +1,13 @@
 # 2419 - Longest Subarray With Maximum Bitwise AND
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        target = max(nums)
+        l = 0
+        res = 0
+        for el in nums:
+            if el == target:
+                l += 1
+                res = max(res,l)
+            else:
+                l = 0
+        return res
