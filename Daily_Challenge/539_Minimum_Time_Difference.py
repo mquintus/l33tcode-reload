9030fc0f09268eec3841051a469f299e6a59d7ca
mquintus <mqs@stuebs.tech>
2024-09-16 09:05:29 +0200

    Update 539_Minimum_Time_Difference.py

--- Daily_Challenge/539_Minimum_Time_Difference.py
+++ Daily_Challenge/539_Minimum_Time_Difference.py
@@ -1,1 +1,15 @@
 # 539 - Minimum Time Difference
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        if len(timePoints) > 1440:
+            return 0
+        def convert(s):
+            return int(s[:2])*60 + int(s[-2:])
+        timePoints = sorted(list(map(convert, timePoints)))
+        smallest_difference = (timePoints[0] - timePoints[-1])%(1440)
+        for i,tp in enumerate(timePoints[1:]):
+            d = tp - timePoints[i]
+            smallest_difference = min(smallest_difference, d)
+            if smallest_difference == 0:
+                return 0
+        return smallest_difference
