e4ba47079d998b164f51d7417d0f756b855ba16a
mquintus <mqs@stuebs.tech>
2024-09-21 11:35:06 +0200

    Update 386_Lexicographical_Numbers.py

--- Daily_Challenge/386_Lexicographical_Numbers.py
+++ Daily_Challenge/386_Lexicographical_Numbers.py
@@ -1,1 +1,13 @@
 # 386 - Lexicographical Numbers
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        result = []
+        def rec(prev):
+            for d in range(0,10):
+                actual = prev + d
+                if actual > 0 and actual <= n:
+                    result.append(actual)
+                    rec(actual * 10)
+        rec(0)
+        return result
+                
