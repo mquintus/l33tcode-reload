df06d242f49c01e9d18fa470e649acbbd6f06bb0
mquintus <mqs@stuebs.tech>
2024-09-25 14:36:24 +0200

    Update 2416_Sum_of_Prefix_Scores_of_Strings.py

--- Daily_Challenge/2416_Sum_of_Prefix_Scores_of_Strings.py
+++ Daily_Challenge/2416_Sum_of_Prefix_Scores_of_Strings.py
@@ -1,1 +1,19 @@
 # 2416 - Sum of Prefix Scores of Strings
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        hashings = {}
+        for word in words:
+            for end in range(1,len(word)+1):
+                token = word[:end]
+                if token not in hashings:
+                    hashings[token] = 0
+                hashings[token] += 1
+        #print(hashings)
+        result = []
+        for word in words:
+            result.append(0)
+            for end in range(1,len(word)+1):
+                token = word[:end]
+                result[-1] += hashings[token]
+        return result
+        
