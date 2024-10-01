39bfc4c8ba8f9fedbd0061b21d29dc8e666efaa8
mquintus <mqs@stuebs.tech>
2024-10-01 08:50:41 +0200

    Update 1497_Check_If_Array_Pairs_Are_Divisible_by_k.py

--- Daily_Challenge/1497_Check_If_Array_Pairs_Are_Divisible_by_k.py
+++ Daily_Challenge/1497_Check_If_Array_Pairs_Are_Divisible_by_k.py
@@ -1,1 +1,13 @@
 # 1497 - Check If Array Pairs Are Divisible by k
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        if sum(arr) % k != 0:
+            return False
+        arr = [el % k for el in arr]
+        rests = Counter(arr)
+        #print(rests)
+        for rest, count in rests.items():
+            req_rest = (k - rest) % k
+            if count != rests[req_rest]:
+                return False
+        return True
