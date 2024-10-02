89a23000015a1ae68cda25fd4d965566be3a7541
mquintus <mqs@stuebs.tech>
2024-10-02 18:43:20 +0200

    Update 1331_Rank_Transform_of_an_Array.py

--- Daily_Challenge/1331_Rank_Transform_of_an_Array.py
+++ Daily_Challenge/1331_Rank_Transform_of_an_Array.py
@@ -1,1 +1,18 @@
 # 1331 - Rank Transform of an Array
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        if len(arr) == 0:
+            return []
+        arr_tuple = [[arr[i], i] for i in range(len(arr))]
+        arr_tuple.sort()
+        arr_tuple[0][0] = 1
+        arr.sort()
+        for i in range(1, len(arr)):
+            if arr_tuple[i][0] == arr[i-1]:
+                arr_tuple[i][0] = arr_tuple[i-1][0]
+            else:
+                arr_tuple[i][0] = arr_tuple[i-1][0] + 1
+        
+        res_tuple = [[pos, rank] for rank, pos in arr_tuple]
+        res_tuple.sort()
+        return [rank for pos, rank in res_tuple]
