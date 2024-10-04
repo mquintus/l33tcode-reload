96060a548b77c0fbac9b3766c7ff886d5c6ffd72
mquintus <mqs@stuebs.tech>
2024-10-04 23:52:46 +0200

    Update 2491_Divide_Players_Into_Teams_of_Equal_Skill.py

--- Daily_Challenge/2491_Divide_Players_Into_Teams_of_Equal_Skill.py
+++ Daily_Challenge/2491_Divide_Players_Into_Teams_of_Equal_Skill.py
@@ -1,1 +1,13 @@
 # 2491 - Divide Players Into Teams of Equal Skill
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        n = len(skill)
+        skill.sort()
+        chemsum = 0
+        checksum = skill[0] + skill[-1]
+        for i in range(n//2):
+            chemsum += skill[i] * skill[n-i-1]
+            if skill[i] + skill[n-i-1] != checksum:
+                return -1
+        return chemsum
+        
