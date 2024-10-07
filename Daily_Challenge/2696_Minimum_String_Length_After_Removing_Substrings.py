6a700e6eab8db3a4afec530eb845116d28ea9c1a
mquintus <mqs@stuebs.tech>
2024-10-07 10:11:14 +0200

    Update 2696_Minimum_String_Length_After_Removing_Substrings.py

--- Daily_Challenge/2696_Minimum_String_Length_After_Removing_Substrings.py
+++ Daily_Challenge/2696_Minimum_String_Length_After_Removing_Substrings.py
@@ -1,1 +1,10 @@
 # 2696 - Minimum String Length After Removing Substrings
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+        for char in s:
+            if len(stack) > 0 and (char == 'B' and stack[-1] == 'A' or char == 'D' and stack[-1] == 'C'):
+                stack.pop()
+            else:
+                stack.append(char)
+        return len(stack)
