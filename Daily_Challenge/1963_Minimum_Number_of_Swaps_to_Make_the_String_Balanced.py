53a52be727b9ca51b76dc90ccd03d084e6eed436
mquintus <mqs@stuebs.tech>
2024-10-08 15:07:35 +0200

    Update 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.py

--- Daily_Challenge/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.py
+++ Daily_Challenge/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.py
@@ -1,1 +1,21 @@
 # 1963 - Minimum Number of Swaps to Make the String Balanced
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        left = 0
+        right = len(s)-1
+        r = 0
+        c = 0
+        while left < right:
+            if s[left] == "]":
+                if c == 0: 
+                    r += 1
+                    c = 1
+                    while s[right] != '[':
+                        right -= 1
+                    right -= 1
+                else:
+                    c -= 1
+            else:
+                c += 1
+            left += 1
+        return r
