e9cba6c23fde0f09387d13d82a2cb21de83b9306
mquintus <mqs@stuebs.tech>
2024-10-09 15:11:12 +0200

    Update 921_Minimum_Add_to_Make_Parentheses_Valid.py

--- Daily_Challenge/921_Minimum_Add_to_Make_Parentheses_Valid.py
+++ Daily_Challenge/921_Minimum_Add_to_Make_Parentheses_Valid.py
@@ -1,1 +1,14 @@
 # 921 - Minimum Add to Make Parentheses Valid
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        left = 0
+        broken = 0
+        for char in s:
+            if char == '(':
+                left += 1
+            else:
+                if left == 0:
+                    broken += 1
+                else:
+                    left -= 1
+        return broken + left
