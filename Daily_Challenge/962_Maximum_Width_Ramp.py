9f2cd2437c83ae24be569c7619b702939bdf82b3
mquintus <mqs@stuebs.tech>
2024-10-10 09:25:17 +0200

    Update 962_Maximum_Width_Ramp.py

--- Daily_Challenge/962_Maximum_Width_Ramp.py
+++ Daily_Challenge/962_Maximum_Width_Ramp.py
@@ -1,1 +1,24 @@
 # 962 - Maximum Width Ramp
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        monoStack = []
+        maxl = 0
+        for i, el in enumerate(nums):
+            #while len(monoStack) > 0 and monoStack[-1][0] > el:
+            #    monoStack.pop()
+            #    
+            if len(monoStack) == 0 or el < monoStack[-1][0]:
+                monoStack.append((el, i))
+
+        print(monoStack)
+        for i in range(len(nums)-1, -1, -1):
+            if len(monoStack) == 0:
+                break
+            el = nums[i]
+            while len(monoStack) > 0 and monoStack[-1][0] <= el:
+                comp = monoStack.pop()
+                #print(comp)
+                maxl = max(i - comp[1], maxl)
+
+
+        return maxl
