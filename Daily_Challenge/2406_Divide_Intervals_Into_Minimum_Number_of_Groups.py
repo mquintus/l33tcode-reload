fbbbee05b947e01770ecf176370f2e546dbcd43a
mquintus <mqs@stuebs.tech>
2024-10-12 10:56:05 +0200

    Update 2406_Divide_Intervals_Into_Minimum_Number_of_Groups.py

--- Daily_Challenge/2406_Divide_Intervals_Into_Minimum_Number_of_Groups.py
+++ Daily_Challenge/2406_Divide_Intervals_Into_Minimum_Number_of_Groups.py
@@ -1,1 +1,19 @@
 # 2406 - Divide Intervals Into Minimum Number of Groups
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        starts = sorted([start for start,_ in intervals])
+        ends = sorted([end for _,end in intervals])
+        pointer_s = 0
+        pointer_e = 0
+
+        concurrent = 0
+        res = 1
+        while pointer_e < len(ends):
+            if pointer_s < len(starts) and starts[pointer_s] <= ends[pointer_e]:
+                concurrent += 1
+                pointer_s += 1
+                res = max(res,concurrent)
+            else:
+                concurrent -= 1
+                pointer_e += 1
+        return res
