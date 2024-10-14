3f80417ac27763952638af066ad06c39c136ea5c
mquintus <mqs@stuebs.tech>
2024-10-14 09:17:20 +0200

    Update 2530_Maximal_Score_After_Applying_K_Operations.py

--- Daily_Challenge/2530_Maximal_Score_After_Applying_K_Operations.py
+++ Daily_Challenge/2530_Maximal_Score_After_Applying_K_Operations.py
@@ -1,1 +1,9 @@
 # 2530 - Maximal Score After Applying K Operations
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        nums = [-el for el in nums]
+        heapq.heapify(nums)
+        score = 0
+        for _ in range(k):
+            score -= heapq.heapreplace(nums, nums[0]//3)
+        return score
