19d484b4498670efacd65e6087d985e0be3673b5
mquintus <mqs@stuebs.tech>
2024-10-16 09:22:41 +0200

    Update 2938_Separate_Black_and_White_Balls.py

--- Daily_Challenge/2938_Separate_Black_and_White_Balls.py
+++ Daily_Challenge/2938_Separate_Black_and_White_Balls.py
@@ -1,1 +1,15 @@
 # 2938 - Separate Black and White Balls
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        n = len(s)
+        distance = 0
+        count = 0
+
+        for char in s:
+            if char == "0":
+                distance += count
+            else:
+                count += 1
+        
+        return distance
+
