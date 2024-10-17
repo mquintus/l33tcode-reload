96f8e6cb788f91bc6eb759b93f7a7b196ed14695
mquintus <mqs@stuebs.tech>
2024-10-17 09:39:02 +0200

    Update 670_Maximum_Swap.py

--- Daily_Challenge/670_Maximum_Swap.py
+++ Daily_Challenge/670_Maximum_Swap.py
@@ -1,1 +1,30 @@
 # 670 - Maximum Swap
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        nums = [int(el) for el in str(num)]
+
+        upperlimit = 10
+
+        while True:
+            maxvalue = -1
+            found_better_max = False
+            for pointer, val in enumerate(nums):
+                if val >= maxvalue and val < upperlimit:
+                    maxvalue = val
+                    maxpointer = pointer
+                    found_better_max = True
+            if not found_better_max:
+                break
+            #print(maxpointer, maxvalue)
+
+            leftpointer = 0
+            while leftpointer < maxpointer and nums[leftpointer] >= maxvalue:
+                leftpointer += 1
+            if leftpointer == maxpointer:
+                upperlimit = maxvalue
+            else:
+                #print(maxpointer, maxvalue, leftpointer, nums[leftpointer])
+                nums[leftpointer], nums[maxpointer] = nums[maxpointer], nums[leftpointer]
+                break
+        return int("".join([str(el) for el in nums]))
+
