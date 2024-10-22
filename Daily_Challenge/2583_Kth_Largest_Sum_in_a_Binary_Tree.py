7c1b9b7d0a803b2105a19f5d6b26bd52b5ece18e
mquintus <mqs@stuebs.tech>
2024-10-22 21:36:06 +0200

    Update 2583_Kth_Largest_Sum_in_a_Binary_Tree.py

--- Daily_Challenge/2583_Kth_Largest_Sum_in_a_Binary_Tree.py
+++ Daily_Challenge/2583_Kth_Largest_Sum_in_a_Binary_Tree.py
@@ -1,1 +1,22 @@
 # 2583 - Kth Largest Sum in a Binary Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        levels = []
+        def rec(node, depth):
+            if len(levels) < depth + 1:
+                levels.append(0)
+            levels[depth] += node.val
+            if node.left is not None:
+                rec(node.left, depth+1)
+            if node.right is not None:
+                rec(node.right, depth+1)
+        rec(root, 0)
+        if len(levels) < k: return -1
+        levels.sort()
+        return levels[-k]
