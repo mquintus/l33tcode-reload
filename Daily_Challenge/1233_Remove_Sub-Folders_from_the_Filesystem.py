37ed34c6dc1ec75de9c5bfdcff3e1b626b6088b3
mquintus <mqs@stuebs.tech>
2024-10-25 10:11:52 +0200

    Update 1233_Remove_Sub-Folders_from_the_Filesystem.py

--- Daily_Challenge/1233_Remove_Sub-Folders_from_the_Filesystem.py
+++ Daily_Challenge/1233_Remove_Sub-Folders_from_the_Filesystem.py
@@ -1,1 +1,18 @@
 # 1233 - Remove Sub-Folders from the Filesystem
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        shortestPrefixes = set()
+        folder.sort()
+        #print(folder)
+        for path in folder:
+            prefixes = path.split('/')[1:]
+            for length in range(1, len(prefixes)):
+                #print("Looking for",tuple(prefixes[:length]))
+                if tuple(prefixes[:length]) in shortestPrefixes:
+                    #print("Found",tuple(prefixes[:length]))
+                    break
+            else:
+                #print("Adding", tuple(prefixes))
+                shortestPrefixes.add(tuple(prefixes))
+        return ["/" + "/".join(prefix) for prefix in shortestPrefixes]
+
