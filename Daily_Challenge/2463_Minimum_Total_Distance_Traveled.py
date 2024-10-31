3ada85f99d2c0a451fed02c973c3db9b6b2b60cc
mquintus <mqs@stuebs.tech>
2024-10-31 12:50:46 +0100

    Update 2463_Minimum_Total_Distance_Traveled.py

--- Daily_Challenge/2463_Minimum_Total_Distance_Traveled.py
+++ Daily_Challenge/2463_Minimum_Total_Distance_Traveled.py
@@ -1,1 +1,26 @@
 # 2463 - Minimum Total Distance Traveled
+class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+        robot.sort()
+        factory.sort()
+
+        @cache
+        def take_or_skip(r, f, fc):
+            if r == len(robot): return 0
+            if f == len(factory): return float('inf')
+
+            # skip
+            skip_steps = take_or_skip(r, f+1, 0)
+
+            capacity = factory[f][1] - fc
+            if capacity == 0: 
+                return skip_steps
+
+            # take
+            steps = abs(robot[r] - factory[f][0])
+            take_steps = steps + take_or_skip(r+1, f, fc+1)
+
+            result = min(skip_steps, take_steps)
+            #print(r,f,fc, result)
+            return result
+        return take_or_skip(0, 0, 0)
