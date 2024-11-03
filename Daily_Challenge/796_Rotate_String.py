0b1914a8571c93c34d2e4719d2067347641c9fc0
mquintus <mqs@stuebs.tech>
2024-11-03 20:19:05 +0100

    Update 796_Rotate_String.py

--- Daily_Challenge/796_Rotate_String.py
+++ Daily_Challenge/796_Rotate_String.py
@@ -1,1 +1,11 @@
 # 796 - Rotate String
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        n = len(s)
+        if n != len(goal): return False
+        if Counter(s) != Counter(goal): return False
+        for pivot in range(n):
+            #print(pivot, s[pivot:], goal[:n-pivot], ".", s[:pivot],goal[n-pivot:])
+            if s[pivot:] == goal[:n-pivot] and s[:pivot] == goal[n-pivot:]:
+                return True
+        return False
