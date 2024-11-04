58d700dcedb6f3667a834c2a9ebe04e552f15f2c
mquintus <mqs@stuebs.tech>
2024-11-04 08:15:05 +0100

    Update 3163_String_Compression_III.py

--- Daily_Challenge/3163_String_Compression_III.py
+++ Daily_Challenge/3163_String_Compression_III.py
@@ -1,1 +1,14 @@
 # 3163 - String Compression III
+class Solution:
+    def compressedString(self, word: str) -> str:
+        c = 1
+        prev = word[0]
+        newword = []
+        for el in word[1:]:
+            if c == 9 or el != prev:
+                newword.append(f"{c}{prev}")
+                c = 0
+            c += 1
+            prev = el
+        newword.append(f"{c}{prev}")
+        return "".join(newword)
