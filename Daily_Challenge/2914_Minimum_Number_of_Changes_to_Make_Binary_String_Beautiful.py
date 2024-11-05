5cea09a27f0486334cae43c89ad4ceb7f8360b12
mquintus <mqs@stuebs.tech>
2024-11-05 16:04:36 +0100

    Update 2914_Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.py

--- Daily_Challenge/2914_Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.py
+++ Daily_Challenge/2914_Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.py
@@ -1,1 +1,9 @@
 # 2914 - Minimum Number of Changes to Make Binary String Beautiful
+def pairwise(iterable):
+    "s -> (s0, s1), (s2, s3), (s4, s5), ..."
+    a = iter(iterable)
+    return zip(a, a)
+
+class Solution:
+    def minChanges(self, s: str) -> int:
+        return sum([1 for el,le in pairwise(s) if el!=le])
