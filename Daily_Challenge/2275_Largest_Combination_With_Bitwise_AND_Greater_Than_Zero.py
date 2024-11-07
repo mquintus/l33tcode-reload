cd239787a7bc6d6444fbd2694720bc53dfe5e694
mquintus <mqs@stuebs.tech>
2024-11-07 15:27:27 +0100

    Update 2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.py

--- Daily_Challenge/2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.py
+++ Daily_Challenge/2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.py
@@ -1,1 +1,20 @@
 # 2275 - Largest Combination With Bitwise AND Greater Than Zero
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        all_set = 0
+        curr_set = 0
+        curr_bit = 1
+        relevant_bits = []
+
+        for bit in range(32):
+            for c in candidates:
+                if curr_bit & c > 0:
+                   #print("Match", curr_bit, c,curr_bit & c)
+                   curr_set += 1 
+            #print(curr_bit, curr_set)
+            all_set = max(all_set, curr_set)
+            curr_set = 0
+            curr_bit <<= 1
+
+        return all_set
+
