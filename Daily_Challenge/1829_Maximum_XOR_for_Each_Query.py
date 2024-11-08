de8e353f5e107efa567f92df6c547d6eb8402606
mquintus <mqs@stuebs.tech>
2024-11-08 09:34:15 +0100

    Update 1829_Maximum_XOR_for_Each_Query.py

--- Daily_Challenge/1829_Maximum_XOR_for_Each_Query.py
+++ Daily_Challenge/1829_Maximum_XOR_for_Each_Query.py
@@ -1,1 +1,16 @@
 # 1829 - Maximum XOR for Each Query
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        something = 2**maximumBit-1
+        xorall = 0
+        for el in nums:
+            xorall = xorall ^ el
+        xorall ^= something
+
+        solution = []
+        solution.append(xorall)
+        for el in nums[:0:-1]:
+            xorall ^= el
+            solution.append(xorall)
+        
+        return solution
