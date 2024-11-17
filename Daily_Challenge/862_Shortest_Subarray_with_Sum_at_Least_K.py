990104e24385d1dfd587adc11e02131f8a93e026
mquintus <mqs@stuebs.tech>
2024-11-17 17:26:16 +0100

    Update 862_Shortest_Subarray_with_Sum_at_Least_K.py

--- Daily_Challenge/862_Shortest_Subarray_with_Sum_at_Least_K.py
+++ Daily_Challenge/862_Shortest_Subarray_with_Sum_at_Least_K.py
@@ -1,1 +1,32 @@
 # 862 - Shortest Subarray with Sum at Least K
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        result = 10**9
+
+        cumulsum = [0]
+        for i, el in enumerate(nums):
+            cumulsum.append(cumulsum[-1] + el)
+            if el >= k: return 1
+            if cumulsum[-1] >= k: 
+                result = min(result, i+1)
+                #print("cumulsum",cumulsum)
+        #print("result A",result)
+
+        p0 = 0
+        p1 = -1
+        dp=[0]*len(nums)
+        for i, cumu in enumerate(cumulsum):
+            while p0 <= p1 and cumu - cumulsum[dp[p0]] >= k:
+                #print("Found ",i-dp[p0], "between", p0, p1)
+                result = min(result, i-dp[p0])
+                #print("result",result)
+                p0 += 1
+            while p0 <= p1 and cumu <= cumulsum[dp[p1]]:
+                p1 -= 1
+            p1 += 1 
+            if p1 >= len(nums): break
+            dp[p1] = i
+
+        if result == 10**9: return -1
+        return result
+            
