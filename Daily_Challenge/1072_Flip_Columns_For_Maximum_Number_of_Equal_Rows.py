d9919d2108a67c288d03857c859505c54eb8b9c5
mquintus <mqs@stuebs.tech>
2024-11-22 16:30:14 +0100

    Update 1072_Flip_Columns_For_Maximum_Number_of_Equal_Rows.py

--- Daily_Challenge/1072_Flip_Columns_For_Maximum_Number_of_Equal_Rows.py
+++ Daily_Challenge/1072_Flip_Columns_For_Maximum_Number_of_Equal_Rows.py
@@ -1,1 +1,19 @@
 # 1072 - Flip Columns For Maximum Number of Equal Rows
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        patterns = {}
+        mp = 0
+        for row in matrix:
+            prev = row[0]
+            pattern = []
+            for el in row:
+                pattern.append(el == prev)
+                prev = el
+            pattern = tuple(pattern)
+            if pattern not in patterns:
+                patterns[pattern] = 0
+            patterns[pattern] += 1
+            mp = max(mp,patterns[pattern])
+        return mp
+
+        
