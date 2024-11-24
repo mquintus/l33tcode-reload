f61076d06207c4e42838bb614f45da77eeef12b2
mquintus <mqs@stuebs.tech>
2024-11-24 08:40:45 +0100

    Update 1975_Maximum_Matrix_Sum.py

--- Daily_Challenge/1975_Maximum_Matrix_Sum.py
+++ Daily_Challenge/1975_Maximum_Matrix_Sum.py
@@ -1,1 +1,29 @@
 # 1975 - Maximum Matrix Sum
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        allElements = []
+        for row in matrix:
+            allElements.extend(row)
+        
+        # How many negative elements are there?
+        # If it's even, we can take the abs(), or there is a zero
+        # If it's odd, one on them stays negative: The one with the smallest abs
+        
+        numberOfNegatives = 0
+        smallestAbsNegative = float('inf')
+        hasZero = False
+        for el in allElements:
+            if el == 0:
+                hasZero = True
+            elif el < 0:
+                numberOfNegatives += 1
+
+            if abs(el) < smallestAbsNegative:
+                smallestAbsNegative = abs(el)
+        
+
+        if hasZero or numberOfNegatives % 2 == 0:
+            return sum([abs(el) for el in allElements])
+        else:
+            return sum([abs(el) for el in allElements]) - 2*smallestAbsNegative
+        
