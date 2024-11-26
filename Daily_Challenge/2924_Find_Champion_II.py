bc57aa645f23ba392b806a3f31d2330873706408
mquintus <mqs@stuebs.tech>
2024-11-26 11:02:34 +0100

    Update 2924_Find_Champion_II.py

--- Daily_Challenge/2924_Find_Champion_II.py
+++ Daily_Challenge/2924_Find_Champion_II.py
@@ -1,1 +1,14 @@
 # 2924 - Find Champion II
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        champs = [True for _ in range(n)]
+        champcount = n
+        for win, loose in edges:
+            if champs[loose]:
+                champs[loose] = False
+                champcount -= 1
+        if champcount > 1: return -1
+        for i in range(n):
+            if champs[i]:
+                return i
+        
