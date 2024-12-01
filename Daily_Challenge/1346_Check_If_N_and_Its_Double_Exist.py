d2156f52ab217c2652d27646f5e789581cb8fb48
mquintus <mqs@stuebs.tech>
2024-12-01 15:37:04 +0100

    Update 1346_Check_If_N_and_Its_Double_Exist.py

--- Daily_Challenge/1346_Check_If_N_and_Its_Double_Exist.py
+++ Daily_Challenge/1346_Check_If_N_and_Its_Double_Exist.py
@@ -1,1 +1,21 @@
 # 1346 - Check If N and Its Double Exist
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        pos = [False] * 3002
+        neg = [False] * 3002
+        for el in arr:
+            scope = neg
+            if abs(el) == el:
+                scope = pos
+            el = abs(el)
+            try:
+
+                if scope[el*2] == True:
+                    return True
+                if el % 2 == 0 and scope[el//2] == True:
+                    return True
+            except:
+                pass
+            scope[el] = True
+
+        return False
