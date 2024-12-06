cbdf95ddcf3b50e8a08863141bc70404753aa1b8
mquintus <mqs@stuebs.tech>
2024-12-06 09:46:04 +0100

    Update 2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I.py

--- Daily_Challenge/2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I.py
+++ Daily_Challenge/2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I.py
@@ -1,1 +1,28 @@
 # 2554 - Maximum Number of Integers to Choose From a Range I
+class Solution:
+    def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        ban = [False for _ in range(n+2)]
+        for b in banned:
+            if b <= n:
+                ban[b] = True
+        # take or skip
+        allowed = [i for i in range(1,n+1) if not ban[i]]
+        m = len(allowed)
+
+        @cache
+        def dp(i, prevsum):
+            #print(i, prevsum)
+            if prevsum == maxSum: return 0
+            if i == m: return 0
+            nextsum = allowed[i] + prevsum
+            if nextsum == maxSum: return 1
+            if nextsum > maxSum: return 0
+            # take
+            take = 1 + dp(i+1, nextsum)
+            # skip
+            #skip = dp(i+1, prevsum)
+            #print("take",take,"skip",skip)
+            return take
+        return dp(0,0) 
+
+
