e4860e32525f376d921a5ceb37687f123d3c0477
mquintus <mqs@stuebs.tech>
2024-12-07 08:36:11 +0100

    Update 1760_Minimum_Limit_of_Balls_in_a_Bag.py

--- Daily_Challenge/1760_Minimum_Limit_of_Balls_in_a_Bag.py
+++ Daily_Challenge/1760_Minimum_Limit_of_Balls_in_a_Bag.py
@@ -1,1 +1,28 @@
 # 1760 - Minimum Limit of Balls in a Bag
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        def checkPenalty(p):
+            neededOperations = 0
+            for el in nums:
+                if el <= p: continue
+                #if neededOperations > maxOperations: return False
+                neededOperations -= (-el + p) // p
+            return neededOperations <= maxOperations
+
+        minPenalty = 1
+        maxPenalty = max(nums)
+        assumedPenalty = (maxPenalty + minPenalty)//2
+        while minPenalty < maxPenalty:
+            assumedPenalty = (maxPenalty + minPenalty)//2
+            if checkPenalty(assumedPenalty):
+                if assumedPenalty == 1 or not checkPenalty(assumedPenalty-1):
+                    return assumedPenalty
+                maxPenalty = assumedPenalty
+            else:
+                if checkPenalty(assumedPenalty+1):
+                    return assumedPenalty+1
+                else:
+                    minPenalty = assumedPenalty+1
+        return assumedPenalty
+
+            
