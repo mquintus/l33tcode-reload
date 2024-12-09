a4a029121625ce322d6fe52077419053f9146e92
mquintus <mqs@stuebs.tech>
2024-12-09 17:42:17 +0100

    Update 3152_Special_Array_II.py

--- Daily_Challenge/3152_Special_Array_II.py
+++ Daily_Challenge/3152_Special_Array_II.py
@@ -1,1 +1,19 @@
 # 3152 - Special Array II
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        prefixXor = nums[0]%2
+        special = [0]
+        for i, el in enumerate(nums[1:]):
+            #print(prefixXor, el%2)
+            if el%2 == prefixXor:
+                special.append(i+1)
+            else:
+                special.append(special[-1])
+            prefixXor = el%2
+        
+        #print(special)
+        result = []
+        for fr, to in queries:
+            result.append(special[to] <= fr)
+
+        return result
