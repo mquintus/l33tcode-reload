1dd2f799ddae77213e1ef5d99148a57a1fcd9ea1
mquintus <mqs@stuebs.tech>
2024-12-10 09:01:39 +0100

    Update 2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I.py

--- Daily_Challenge/2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I.py
+++ Daily_Challenge/2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I.py
@@ -1,1 +1,25 @@
 # 2981 - Find Longest Special Substring That Occurs Thrice I
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        maxlengths = {}
+        prev = ""
+        bestval = -1
+        for el in s: 
+            if el == prev:
+                maxlengths[el].append(maxlengths[el][-1] + 1)
+                if maxlengths[el][-1] >= 3:
+                    goodval = maxlengths[el][-1] - 2
+                    bestval = max(bestval, goodval)
+            else:
+                if el not in maxlengths:
+                    maxlengths[el] = []
+                maxlengths[el].append(1)
+            prev = el
+        
+        for key, amounts in maxlengths.items():
+            if len(amounts) < 3: continue
+            amounts.sort()
+            goodval = min(amounts[-3:])
+            bestval = max(bestval, goodval)
+        
+        return bestval
