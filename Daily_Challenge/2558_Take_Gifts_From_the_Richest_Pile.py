dfe6abd806b492ad401bf99dc9c478fdfda9cae5
mquintus <mqs@stuebs.tech>
2024-12-12 09:52:00 +0100

    Update 2558_Take_Gifts_From_the_Richest_Pile.py

--- Daily_Challenge/2558_Take_Gifts_From_the_Richest_Pile.py
+++ Daily_Challenge/2558_Take_Gifts_From_the_Richest_Pile.py
@@ -1,1 +1,9 @@
 # 2558 - Take Gifts From the Richest Pile
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts[:] = [-a for a in gifts]
+        heapq.heapify(gifts)
+        for _ in range(k):
+            best = -1 * gifts[0]
+            heapq.heapreplace(gifts, -1 * (int)(best**.5))
+        return -1 * sum(gifts)
