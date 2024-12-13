5f24f6345f4571432bab7ed4eeb187aba6bc45d1
mquintus <mqs@stuebs.tech>
2024-12-13 12:52:43 +0100

    Update 2593_Find_Score_of_an_Array_After_Marking_All_Elements.py

--- Daily_Challenge/2593_Find_Score_of_an_Array_After_Marking_All_Elements.py
+++ Daily_Challenge/2593_Find_Score_of_an_Array_After_Marking_All_Elements.py
@@ -1,1 +1,27 @@
 # 2593 - Find Score of an Array After Marking All Elements
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        n = len(nums)
+        indexlist = []
+        heap = []
+        for i, el in enumerate(nums):
+            element = [el, i, False]
+            indexlist.append(element)
+            heapq.heappush(heap, element)
+
+        score = 0
+        while heap:
+            element = heapq.heappop(heap)
+            el, i, marked = element
+            if marked: 
+                #print("Index",i,"Value",el,"Already marked") 
+                continue
+            score += el
+            #print("Index",i,"Value",el,"Score",score)
+            indexlist[i][2] = True
+            if i > 0:
+                indexlist[i-1][2] = True
+            if i < n - 1:
+                indexlist[i+1][2] = True
+
+        return score
