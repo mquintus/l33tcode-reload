273a093ec3f55dc023955eba29e6189d8256d2bf
mquintus <mqs@stuebs.tech>
2024-12-15 09:26:21 +0100

    Update 1792_Maximum_Average_Pass_Ratio.py

--- Daily_Challenge/1792_Maximum_Average_Pass_Ratio.py
+++ Daily_Challenge/1792_Maximum_Average_Pass_Ratio.py
@@ -1,1 +1,28 @@
 # 1792 - Maximum Average Pass Ratio
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        for c in classes:
+            p, t = c
+            r1 = p/t
+            r2 = (p+1)/(t+1)
+            c[:] = [r1-r2, t, p]
+        heapq.heapify(classes)
+
+        for i in range(extraStudents):
+            #print(classes)
+            r,t,p = classes[0]
+            
+            t += 1
+            p += 1
+
+            r1 = p/t
+            r2 = (p+1)/(t+1)
+
+            c = [r1-r2, t, p]
+            heapq.heapreplace(classes, c)
+        #print(classes)
+
+        avg = 0
+        for _, t, p in classes:
+            avg += p/t
+        return avg / len(classes)
