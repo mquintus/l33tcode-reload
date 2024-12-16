f48a86a95cbeefd6f9ce86986007dc7a10a3a97c
mquintus <mqs@stuebs.tech>
2024-12-16 13:03:06 +0100

    Update 3264_Final_Array_State_After_K_Multiplication_Operations_I.py

--- Daily_Challenge/3264_Final_Array_State_After_K_Multiplication_Operations_I.py
+++ Daily_Challenge/3264_Final_Array_State_After_K_Multiplication_Operations_I.py
@@ -1,1 +1,13 @@
 # 3264 - Final Array State After K Multiplication Operations I
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        heap = []
+        for i, el in enumerate(nums):
+            heapq.heappush(heap, (el, i))
+        
+        for _ in range(k):
+            el, i = heap[0]
+            nums[i] *= multiplier
+            heapq.heapreplace(heap, (nums[i], i))
+    
+        return nums
