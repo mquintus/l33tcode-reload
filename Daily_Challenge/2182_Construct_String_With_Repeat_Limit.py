c0f30d9137e42eaacc99ed84726de08e26aeb7d9
mquintus <mqs@stuebs.tech>
2024-12-17 09:21:43 +0100

    Update 2182_Construct_String_With_Repeat_Limit.py

--- Daily_Challenge/2182_Construct_String_With_Repeat_Limit.py
+++ Daily_Challenge/2182_Construct_String_With_Repeat_Limit.py
@@ -1,1 +1,23 @@
 # 2182 - Construct String With Repeat Limit
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        c = Counter(s)
+        lexi = sorted([[el, count] for el, count in c.items()])[::-1]
+        prev = ''
+        result = []
+        while True:
+            first = True
+            for i, (el, count) in enumerate(lexi):
+                if count == 0: continue
+                if el == prev: 
+                    first = False
+                    continue
+                if not first: count = 1
+                newcount = min(count, repeatLimit)
+                result.append(el*newcount)
+                lexi[i][1] -= newcount
+                prev = el
+                break
+            else:
+                break
+        return "".join(result)
