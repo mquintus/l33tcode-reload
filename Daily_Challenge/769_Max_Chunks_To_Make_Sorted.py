68309628c935296da02a0e78d1ea693a892c930f
mquintus <mqs@stuebs.tech>
2024-12-19 12:05:43 +0100

    Update 769_Max_Chunks_To_Make_Sorted.py

--- Daily_Challenge/769_Max_Chunks_To_Make_Sorted.py
+++ Daily_Challenge/769_Max_Chunks_To_Make_Sorted.py
@@ -1,1 +1,13 @@
 # 769 - Max Chunks To Make Sorted
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        low = arr[0]
+        up = arr[0]
+        chunks = 0
+        for i, el in enumerate(arr):
+            low = min(low, el)
+            up = max(up, el)
+            if low == 0 and up <= i:
+                chunks += 1
+            
+        return chunks
