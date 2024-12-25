5f663e92cbddf523c3cb8d0e9095e23eb6a7c19e
mquintus <mqs@stuebs.tech>
2024-12-25 05:53:34 +0100

    Update 515_Find_Largest_Value_in_Each_Tree_Row.py

--- Daily_Challenge/515_Find_Largest_Value_in_Each_Tree_Row.py
+++ Daily_Challenge/515_Find_Largest_Value_in_Each_Tree_Row.py
@@ -1,1 +1,24 @@
 # 515 - Find Largest Value in Each Tree Row
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        
+        answer = []
+        def dfs(node, level):
+            if len(answer) <= level:
+                answer.append(float(-inf))
+            answer[level] = max(answer[level], node.val)
+            for n2 in [node.left, node.right]:
+                if n2 is not None:
+                    dfs(n2, level+1)
+
+        dfs(root,0)
+        return answer
+            
