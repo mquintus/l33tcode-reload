2835ff1f74f8246bc2f509f21d4a898d0ea63019
mquintus <mqs@stuebs.tech>
2024-12-30 09:47:17 +0100

    Update 2466_Count_Ways_To_Build_Good_Strings.py

--- Daily_Challenge/2466_Count_Ways_To_Build_Good_Strings.py
+++ Daily_Challenge/2466_Count_Ways_To_Build_Good_Strings.py
@@ -1,1 +1,20 @@
 # 2466 - Count Ways To Build Good Strings
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        MOD = 10**9+7
+        dp = [0 for _ in range(high+1)]
+        dp[0] = 1
+        answer = 0
+        for i in range(1,high+1):
+            dp[i] = 0
+            if i - zero >= 0:
+                dp[i] += dp[i-zero]
+            if i - one >= 0:
+                dp[i] += dp[i-one]
+
+            if i >= low and i <= high:
+                answer += dp[i]
+
+            dp[i] = dp[i] % MOD
+            
+        return answer % MOD
