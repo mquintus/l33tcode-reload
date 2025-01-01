04ab723e2b513d772b3a73b88bcbdc4673fcfe23
mquintus <mqs@stuebs.tech>
2025-01-01 16:59:35 +0100

    Update 1422_Maximum_Score_After_Splitting_a_String.py

--- Daily_Challenge/1422_Maximum_Score_After_Splitting_a_String.py
+++ Daily_Challenge/1422_Maximum_Score_After_Splitting_a_String.py
@@ -1,1 +1,12 @@
 # 1422 - Maximum Score After Splitting a String
+class Solution:
+    def maxScore(self, s: str) -> int:
+        maxscore = 0
+        score = sum([int(el) for el in s])
+        for el in s[:-1]:
+            if el == "1":
+                score -= 1
+            else:
+                score += 1
+            maxscore = max(score, maxscore)
+        return maxscore
