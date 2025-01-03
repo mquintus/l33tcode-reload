a2bc8d6ceacc904405b453a5cbc31ba1a8d90201
mquintus <mqs@stuebs.tech>
2025-01-03 08:16:01 +0100

    Update 2270_Number_of_Ways_to_Split_Array.py

--- Daily_Challenge/2270_Number_of_Ways_to_Split_Array.py
+++ Daily_Challenge/2270_Number_of_Ways_to_Split_Array.py
@@ -1,1 +1,14 @@
 # 2270 - Number of Ways to Split Array
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        totalSum = sum(nums)
+        avg = (totalSum+1) // 2
+        
+        currSum = 0
+        counter = 0
+        for el in nums[:-1]:
+            currSum += el
+            if currSum >= avg:
+                counter += 1
+
+        return counter
