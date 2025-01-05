06217fff6eb64b3dce27b21857c91c01bda47de4
mquintus <mqs@stuebs.tech>
2025-01-05 09:22:18 +0100

    Update 2381_Shifting_Letters_II.py

--- Daily_Challenge/2381_Shifting_Letters_II.py
+++ Daily_Challenge/2381_Shifting_Letters_II.py
@@ -1,1 +1,22 @@
 # 2381 - Shifting Letters II
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        a = ord('a')
+        s = [ord(el) - a for el in s]
+        shifts = [[start,end,direction*2-1] for start,end,direction in shifts]
+
+        starts = [(start, direction) for start,end,direction in shifts]
+        starts.extend([(end+1, -direction) for start,end,direction in shifts])
+        starts.sort()
+
+        curr = 0
+        p1 = 0 
+        for position in range(len(s)):
+            while p1 < len(starts) and starts[p1][0] < position:
+                p1 += 1
+            while p1 < len(starts) and starts[p1][0] == position:
+                curr += starts[p1][1]
+                p1 += 1
+            s[position] += curr
+        
+        return "".join([chr((el%26)+a) for el in s])
