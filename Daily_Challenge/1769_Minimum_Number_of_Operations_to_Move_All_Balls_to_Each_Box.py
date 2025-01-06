e1db0a55505acfd34dba509c7b924db9652947b5
mquintus <mqs@stuebs.tech>
2025-01-06 08:00:42 +0100

    Update 1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.py

--- Daily_Challenge/1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.py
+++ Daily_Challenge/1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.py
@@ -1,1 +1,29 @@
 # 1769 - Minimum Number of Operations to Move All Balls to Each Box
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        prefixSum = 0
+        prefixBox = [0]
+        postfixBox = [0]
+        n=len(boxes)
+        totalSum = 0
+        for i, one in enumerate(boxes):
+            totalSum += int(one)
+            prefixSum += (int(one)*i)
+            prefixBox.append(prefixBox[-1] + int(one))
+            postfixBox.append(postfixBox[-1] + int(boxes[-1-i]))
+        #print(prefixBox)
+        #print(postfixBox)
+        solution = [
+            prefixSum
+        ]
+        for i in range(0,len(boxes)-1):
+            nextSolution = solution[i] 
+            nextSolution -= postfixBox[n-i-1] 
+            nextSolution += prefixBox[i+1]
+            solution.append(nextSolution)
+            
+            
+        return solution
+
+
+        
