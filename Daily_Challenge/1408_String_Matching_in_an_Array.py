bfd96a88b2e07713959503788f6517ce64d02165
mquintus <mqs@stuebs.tech>
2025-01-07 10:06:18 +0100

    Update 1408_String_Matching_in_an_Array.py

--- Daily_Challenge/1408_String_Matching_in_an_Array.py
+++ Daily_Challenge/1408_String_Matching_in_an_Array.py
@@ -1,1 +1,14 @@
 # 1408 - String Matching in an Array
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        answer = []
+        n = len(words)
+        for i in range(n):
+            for j in range(n):
+                if i == j: continue
+                if len(words[i]) >= len(words[j]): continue
+                li = len(words[i])
+                if words[i] in words[j]:
+                    answer.append(words[i])
+                    break
+        return answer
