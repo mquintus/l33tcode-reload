c54c5947f04ac5094df1509fdaf7f80e84db0ffa
mquintus <mqs@stuebs.tech>
2025-01-11 14:23:52 +0100

    Update 1400_Construct_K_Palindrome_Strings.py

--- Daily_Challenge/1400_Construct_K_Palindrome_Strings.py
+++ Daily_Challenge/1400_Construct_K_Palindrome_Strings.py
@@ -1,1 +1,16 @@
 # 1400 - Construct K Palindrome Strings
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k: return False
+        if len(s) == k: return True
+
+        count = [0 for _ in range(26)]
+        for idx in range(26):
+            count[idx] = s.count(chr(idx + ord('a'))) % 2
+
+        number_of_odd = 0
+        for idx in range(26):
+            if count[idx]: 
+                number_of_odd += 1
+
+        return number_of_odd <= k
