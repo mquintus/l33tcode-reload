b89492bcaad41a0c09fa104843cb267ec6cbf698
mquintus <mqs@stuebs.tech>
2025-01-13 09:59:08 +0100

    Update 3223_Minimum_Length_of_String_After_Operations.py

--- Daily_Challenge/3223_Minimum_Length_of_String_After_Operations.py
+++ Daily_Challenge/3223_Minimum_Length_of_String_After_Operations.py
@@ -1,1 +1,13 @@
 # 3223 - Minimum Length of String After Operations
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        l = 0
+        for char in [chr(char) for char in range(ord('a'), ord('z') + 1)]:
+            c = s.count(char) 
+            if c == 0:
+                continue
+            if c % 2 == 0:
+                l += 2
+            else:
+                l += 1
+        return l
