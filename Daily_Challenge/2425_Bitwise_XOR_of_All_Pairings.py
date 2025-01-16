e68f797f70a7a4cf76ef2ad31cc929bff090a0e9
mquintus <mqs@stuebs.tech>
2025-01-16 10:35:41 +0100

    Update 2425_Bitwise_XOR_of_All_Pairings.py

--- Daily_Challenge/2425_Bitwise_XOR_of_All_Pairings.py
+++ Daily_Challenge/2425_Bitwise_XOR_of_All_Pairings.py
@@ -1,1 +1,12 @@
 # 2425 - Bitwise XOR of All Pairings
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        result = 0
+        if len(nums2) % 2 == 1:
+            for el in nums1:
+                result ^= el
+        if len(nums1) % 2 == 1:
+            for el in nums2:
+                result ^= el
+        return result
+
