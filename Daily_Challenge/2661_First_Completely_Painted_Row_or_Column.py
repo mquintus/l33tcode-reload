c2d390416e180a3a69933095073dfd18fb94cd08
mquintus <mqs@stuebs.tech>
2025-01-20 11:19:46 +0100

    Update 2661_First_Completely_Painted_Row_or_Column.py

--- Daily_Challenge/2661_First_Completely_Painted_Row_or_Column.py
+++ Daily_Challenge/2661_First_Completely_Painted_Row_or_Column.py
@@ -1,1 +1,21 @@
 # 2661 - First Completely Painted Row or Column
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        rows = len(mat)
+        cols = len(mat[0])
+        index = [[] for _ in range(rows*cols + 1)]
+        for r in range(rows):
+            for c in range(cols):
+                i = r + c * cols
+                index[mat[r][c]] = (r,c)
+        paintedRows = [0 for _ in range(rows)]
+        paintedCols = [0 for _ in range(cols)]
+        for i in range(len(arr)):
+            el = arr[i]
+            r = index[el][0]
+            c = index[el][1]
+            paintedRows[r] += 1
+            paintedCols[c] += 1
+            if paintedRows[r] == cols or paintedCols[c] == rows: return i
+
+
