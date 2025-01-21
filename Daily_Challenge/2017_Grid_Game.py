b5cc56c195afda26910c18e25af4f2ea742249ee
mquintus <mqs@stuebs.tech>
2025-01-21 11:30:37 +0100

    Update 2017_Grid_Game.py

--- Daily_Challenge/2017_Grid_Game.py
+++ Daily_Challenge/2017_Grid_Game.py
@@ -1,1 +1,16 @@
 # 2017 - Grid Game
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        postfixA = sum(grid[0][1:])
+        prefixB = 0
+
+        bestSolutionB = max(postfixA, prefixB)
+        for i in range(1, len(grid[0])):
+            postfixA -= grid[0][i]
+            prefixB += grid[1][i-1]
+
+            bestSolutionTempB = max(postfixA, prefixB)
+            if bestSolutionB > bestSolutionTempB:
+                bestSolutionB = bestSolutionTempB
+        
+        return bestSolutionB
