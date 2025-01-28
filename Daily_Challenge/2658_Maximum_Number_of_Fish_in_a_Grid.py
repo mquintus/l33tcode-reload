335dded53b442ae59e3de2afd02f9d54e887604d
mquintus <mqs@stuebs.tech>
2025-01-28 10:43:05 +0100

    Update 2658_Maximum_Number_of_Fish_in_a_Grid.py

--- Daily_Challenge/2658_Maximum_Number_of_Fish_in_a_Grid.py
+++ Daily_Challenge/2658_Maximum_Number_of_Fish_in_a_Grid.py
@@ -1,1 +1,27 @@
 # 2658 - Maximum Number of Fish in a Grid
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def bfs(r,c):
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return 0
+            if grid[r][c] == 0:
+                return 0
+            points = grid[r][c]
+            grid[r][c] = 0
+            for dr,dc in [(0,1),(1,0),(0,-1),(-1,0)]:
+                nr = dr + r
+                nc = dc + c
+                points += bfs(nr,nc)
+            return points
+
+        maxpoints = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0: continue
+                points = bfs(r,c)
+                maxpoints = max(points,maxpoints)
+        
+        return maxpoints
