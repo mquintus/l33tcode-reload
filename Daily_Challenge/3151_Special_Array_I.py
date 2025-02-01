bc7838d17d455dd8e3f79e39ba0d9f16e72afdc6
mquintus <mqs@stuebs.tech>
2025-02-01 10:06:05 +0100

    Update 3151_Special_Array_I.py

--- Daily_Challenge/3151_Special_Array_I.py
+++ Daily_Challenge/3151_Special_Array_I.py
@@ -1,1 +1,7 @@
 # 3151 - Special Array I
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(1, len(nums)):
+            if (nums[i]+nums[i-1]) %2 == 0:
+                return False
+        return True
