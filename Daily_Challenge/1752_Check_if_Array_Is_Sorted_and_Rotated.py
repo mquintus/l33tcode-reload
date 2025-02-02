4250776dbf2e8a9f62e72971698c248da86de34b
mquintus <mqs@stuebs.tech>
2025-02-02 11:53:50 +0100

    Update 1752_Check_if_Array_Is_Sorted_and_Rotated.py

--- Daily_Challenge/1752_Check_if_Array_Is_Sorted_and_Rotated.py
+++ Daily_Challenge/1752_Check_if_Array_Is_Sorted_and_Rotated.py
@@ -1,1 +1,14 @@
 # 1752 - Check if Array Is Sorted and Rotated
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        prev = nums[0]
+        smallest = nums[0]
+        rot = False
+        for i in range(1,len(nums)):
+            el = nums[i]
+            if el < prev:
+                if rot: return False
+                rot = True
+            if rot and el > smallest: return False
+            prev = el
+        return True
