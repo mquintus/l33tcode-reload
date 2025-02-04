e1641eb669f34f5ed694b5a3ac11e50cbd4dbbc8
mquintus <mqs@stuebs.tech>
2025-02-04 09:14:54 +0100

    Update 1800_Maximum_Ascending_Subarray_Sum.py

--- Daily_Challenge/1800_Maximum_Ascending_Subarray_Sum.py
+++ Daily_Challenge/1800_Maximum_Ascending_Subarray_Sum.py
@@ -1,1 +1,15 @@
 # 1800 - Maximum Ascending Subarray Sum
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        s = nums[0]
+        prev = nums[0]
+        cs = s
+        for el in nums[1:]:
+            if prev >= el:
+                cs = el
+            else:
+                cs += el
+            s = max(s, cs)
+            prev = el
+            
+        return s
