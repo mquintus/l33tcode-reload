ac33ff11961d78f4125013fb90e57b68c83dc082
mquintus <mqs@stuebs.tech>
2025-02-06 08:58:41 +0100

    Update 1726_Tuple_with_Same_Product.py

--- Daily_Challenge/1726_Tuple_with_Same_Product.py
+++ Daily_Challenge/1726_Tuple_with_Same_Product.py
@@ -1,1 +1,20 @@
 # 1726 - Tuple with Same Product
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        if len(nums) < 4: return 0
+        # if a solution is found, multiply by 8
+        hashmap = {}
+
+        result = 0
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                prod = nums[i] * nums[j] 
+                if prod not in hashmap:
+                    hashmap[prod] = 1
+                else:
+                    result += 8*hashmap[prod]
+                    hashmap[prod] += 1
+                    
+        
+        return result
