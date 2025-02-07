7d76a315dfd633a1b47fe6558a43c55976bb171b
mquintus <mqs@stuebs.tech>
2025-02-07 09:40:49 +0100

    Update 3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls.py

--- Daily_Challenge/3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls.py
+++ Daily_Challenge/3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls.py
@@ -1,1 +1,27 @@
 # 3160 - Find the Number of Distinct Colors Among the Balls
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        result = []
+        curr = 0
+        colorings = {}
+        colors = {}
+        for loc, col in queries:
+            if col not in colors:
+                colors[col] = 0
+            
+            if loc in colorings:
+                oldcolor = colorings[loc]
+                colors[oldcolor] -= 1
+                if colors[oldcolor] == 0:
+                    curr -= 1
+            else:
+                colorings[loc] = 0
+
+            colorings[loc] = col
+                
+            colors[col] += 1
+            if colors[col] == 1:
+                curr += 1
+            
+            result.append(curr)
+        return result
