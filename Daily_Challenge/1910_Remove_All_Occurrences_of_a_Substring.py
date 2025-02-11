73b554544ab29ceac9c38b3fbc099a267f72e0a8
mquintus <mqs@stuebs.tech>
2025-02-11 17:47:43 +0100

    Update 1910_Remove_All_Occurrences_of_a_Substring.py

--- Daily_Challenge/1910_Remove_All_Occurrences_of_a_Substring.py
+++ Daily_Challenge/1910_Remove_All_Occurrences_of_a_Substring.py
@@ -1,1 +1,12 @@
 # 1910 - Remove All Occurrences of a Substring
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        part = list(part)
+        n = len(part)
+        myStack = []
+        for el in s:
+            #print(myStack)
+            myStack.append(el)
+            while myStack[-n:] == part:
+                myStack = myStack[:-n]
+        return "".join(myStack)
