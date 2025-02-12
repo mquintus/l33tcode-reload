6acf0546f5dadd3fdad4588e0484ad1dc73755bf
mquintus <mqs@stuebs.tech>
2025-02-12 18:40:07 +0100

    Update 2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.py

--- Daily_Challenge/2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.py
+++ Daily_Challenge/2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.py
@@ -1,1 +1,20 @@
 # 2342 - Max Sum of a Pair With Equal Sum of Digits
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        def sum_of_digits(i):
+            return sum([int(d) for d in str(i)])
+        sod = [sum_of_digits(i) for i in nums]
+
+        hashmap = {}
+        best = -1
+        for p, el in enumerate(sod):
+            if el in hashmap:
+                otherindex = hashmap[el]
+                best = max(best, nums[otherindex] + nums[p])
+                if nums[p] > nums[otherindex]:
+                    hashmap[el] = p
+            else:
+                hashmap[el] = p
+            
+
+        return best
