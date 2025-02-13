6a60ebc8444aec9b1f1e9cbc76a333e4197022d3
mquintus <mqs@stuebs.tech>
2025-02-13 07:24:06 +0100

    Update 3066_Minimum_Operations_to_Exceed_Threshold_Value_II.py

--- Daily_Challenge/3066_Minimum_Operations_to_Exceed_Threshold_Value_II.py
+++ Daily_Challenge/3066_Minimum_Operations_to_Exceed_Threshold_Value_II.py
@@ -1,1 +1,11 @@
 # 3066 - Minimum Operations to Exceed Threshold Value II
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        op = 0
+        heapq.heapify(nums)
+        while nums[0] < k:
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            heapq.heappush(nums, x * 2 + y)
+            op += 1
+        return op
