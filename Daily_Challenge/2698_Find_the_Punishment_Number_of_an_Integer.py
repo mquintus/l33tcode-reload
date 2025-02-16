45687dd93701c742ca7b0072066d5f27f92d9897
mquintus <mqs@stuebs.tech>
2025-02-16 09:20:24 +0100

    Update 2698_Find_the_Punishment_Number_of_an_Integer.py

--- Daily_Challenge/2698_Find_the_Punishment_Number_of_an_Integer.py
+++ Daily_Challenge/2698_Find_the_Punishment_Number_of_an_Integer.py
@@ -1,1 +1,6 @@
 # 2698 - Find the Punishment Number of an Integer
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        sol = [1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000]
+        p = bisect.bisect_right(sol, n)
+        return sum([i**2 for i in sol[:p]])
