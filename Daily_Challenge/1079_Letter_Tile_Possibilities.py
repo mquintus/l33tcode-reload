9d3ee4c8a1568c60ce0ec1b4ac0f181994a874a4
mquintus <mqs@stuebs.tech>
2025-02-17 08:37:04 +0100

    Update 1079_Letter_Tile_Possibilities.py

--- Daily_Challenge/1079_Letter_Tile_Possibilities.py
+++ Daily_Challenge/1079_Letter_Tile_Possibilities.py
@@ -1,1 +1,16 @@
 # 1079 - Letter Tile Possibilities
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        inventory = Counter(tiles)
+        
+        def dfs():
+            c = 0
+            for letter, count in inventory.items():
+                if count == 0: continue
+                inventory[letter] -= 1
+                c += 1
+                c += dfs()
+                inventory[letter] += 1
+            return c
+
+        return dfs()
