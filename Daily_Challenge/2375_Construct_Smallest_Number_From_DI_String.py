5074f57d033822e212f63c4fd6ad1bd06d70e277
mquintus <mqs@stuebs.tech>
2025-02-18 10:03:29 +0100

    Update 2375_Construct_Smallest_Number_From_DI_String.py

--- Daily_Challenge/2375_Construct_Smallest_Number_From_DI_String.py
+++ Daily_Challenge/2375_Construct_Smallest_Number_From_DI_String.py
@@ -1,1 +1,18 @@
 # 2375 - Construct Smallest Number From DI String
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        result = ''
+        p = 0
+        c = 1
+        while p < len(pattern):
+            for i, el in enumerate(pattern[p:]):
+                if el == 'I':
+                    break
+            else:
+                i += 1
+            result += ''.join([str(ch) for ch in range(c, c+i+1)[::-1]])
+            p += (i+1)
+            c = c + (i+1)
+        if p == len(pattern):
+            result += str(c)
+        return result
