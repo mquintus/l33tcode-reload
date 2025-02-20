54800527810fad9b6e0d2450f513ea616dda335e
mquintus <mqs@stuebs.tech>
2025-02-20 16:40:37 +0100

    Update 1980_Find_Unique_Binary_String.py

--- Daily_Challenge/1980_Find_Unique_Binary_String.py
+++ Daily_Challenge/1980_Find_Unique_Binary_String.py
@@ -1,1 +1,7 @@
 # 1980 - Find Unique Binary String
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        unequal = []
+        for i, num in enumerate(nums):
+            unequal.append(str(1-int(num[i])))
+        return "".join(unequal)
