b99ceb15648d86b557453141f5fef39f4d5c0c0e
mquintus <mqs@stuebs.tech>
2025-02-20 16:41:24 +0100

    Update 1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.py

--- Daily_Challenge/1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.py
+++ Daily_Challenge/1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.py
@@ -1,1 +1,29 @@
 # 1415 - The k-th Lexicographical String of All Happy Strings of Length n
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        counter = 0
+        def appends(i,prev):
+            nonlocal counter,k
+            #print(i,prev)
+            if i == n:
+                counter += 1
+                if counter == k:
+                    return prev
+                else:
+                    return None
+            for seq in 'abc':
+                if seq != prev[-1]:
+                    prev.append(seq)
+                    res = appends(i+1, prev)
+                    if res is not None:
+                        return res
+                    prev.pop()
+                if counter == k:
+                    return prev
+            if counter == k:
+                    returnprev
+        result = appends(0,[""])
+        if result == None:
+            return ""
+        return "".join(result[1:])
+
