17b02dd37b5e76ecd370510251170f1d20b60f39
Marius Stuebs <mqs@stuebs.tech>
2025-02-20 02:01:05 +0100

    'New day, new challenge: #1980. Find Unique Binary String'

--- Daily_Challenge/1980_Find_Unique_Binary_String.py
+++ Daily_Challenge/1980_Find_Unique_Binary_String.py
@@ -1,12 +1,1 @@
 # 1980 - Find Unique Binary String
-class Solution:
-    def findDifferentBinaryString(self, nums: List[str]) -> str:
-        n = 2**len(nums)
-        resultformat = "{:0"+str(len(nums))+"b}"
-        matches = {}
-        for num in nums:
-            matches[int(num, 2)] = 1
-        
-        for i in range(n):
-            if i not in matches:
-                return resultformat.format(i)
