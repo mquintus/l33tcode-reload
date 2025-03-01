cf3457413e0f15a0876ca4956478062cca56770a
mquintus <mqs@stuebs.tech>
2025-03-01 16:14:53 +0100

    Update 2460_Apply_Operations_to_an_Array.py

--- Daily_Challenge/2460_Apply_Operations_to_an_Array.py
+++ Daily_Challenge/2460_Apply_Operations_to_an_Array.py
@@ -1,1 +1,16 @@
 # 2460 - Apply Operations to an Array
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        newNums = []
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        c = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                c += 1
+                continue
+            newNums.append(nums[i])
+        newNums.extend([0]*c)
+        return newNums
