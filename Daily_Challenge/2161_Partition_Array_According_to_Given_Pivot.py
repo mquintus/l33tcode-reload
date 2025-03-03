42dfd809a683265ec2a0633c7262f93211b61612
mquintus <mqs@stuebs.tech>
2025-03-03 10:39:57 +0100

    Update 2161_Partition_Array_According_to_Given_Pivot.py

--- Daily_Challenge/2161_Partition_Array_According_to_Given_Pivot.py
+++ Daily_Challenge/2161_Partition_Array_According_to_Given_Pivot.py
@@ -1,1 +1,14 @@
 # 2161 - Partition Array According to Given Pivot
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        a = []
+        b = 0
+        c = []
+        for el in nums:
+            if el < pivot:
+                a.append(el)
+            elif el > pivot:
+                c.append(el)
+            else:
+                b += 1
+        return [*a, *([pivot]*b), *c]
