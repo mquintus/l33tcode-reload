6810e008ae2da38e41dedb517f05b26689356b74
mquintus <mqs@stuebs.tech>
2025-03-04 20:39:25 +0100

    Update 1780_Check_if_Number_is_a_Sum_of_Powers_of_Three.py

--- Daily_Challenge/1780_Check_if_Number_is_a_Sum_of_Powers_of_Three.py
+++ Daily_Challenge/1780_Check_if_Number_is_a_Sum_of_Powers_of_Three.py
@@ -1,1 +1,23 @@
 # 1780 - Check if Number is a Sum of Powers of Three
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        po = 1
+        pot = []
+        for y in range(100):
+            pot.append(po)
+            po = po * 3
+            if po > 10**7:
+                break
+        
+        def skiptake(curr, i):
+            if curr == n:
+                return True
+            if i >= len(pot) or curr > n or pot[i] > n:
+                return False
+            if pot[i] == n:
+                return True
+            skip = skiptake(curr, i+1)
+            take = skiptake(curr + pot[i], i+1)
+            return take or skip
+        
+        return skiptake(0,0)
