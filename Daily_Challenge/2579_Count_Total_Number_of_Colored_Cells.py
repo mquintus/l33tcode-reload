beb437d7d88d40c5c30de41d245a9f689a877394
mquintus <mqs@stuebs.tech>
2025-03-05 13:51:13 +0100

    Update 2579_Count_Total_Number_of_Colored_Cells.py

--- Daily_Challenge/2579_Count_Total_Number_of_Colored_Cells.py
+++ Daily_Challenge/2579_Count_Total_Number_of_Colored_Cells.py
@@ -1,1 +1,6 @@
 # 2579 - Count Total Number of Colored Cells
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        if n == 1:
+            return 1
+        return 1 + 4 * (n-1) * (n) // 2
