5f8077116de4fc3b70cba925f74797083b4d07ba
mquintus <mqs@stuebs.tech>
2025-03-09 15:39:26 +0100

    Update 3208_Alternating_Groups_II.py

--- Daily_Challenge/3208_Alternating_Groups_II.py
+++ Daily_Challenge/3208_Alternating_Groups_II.py
@@ -1,1 +1,21 @@
 # 3208 - Alternating Groups II
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        colors.extend(colors[:k-1])
+        prev = colors[0]
+        length = 1
+        count = 0
+        #print(colors)
+        for i in range(1,len(colors)):
+            if colors[i] != prev:
+                length += 1
+            else:
+                length = 1
+            
+            if length >= k:
+                count += 1
+
+            prev = colors[i]
+
+        return count
+
