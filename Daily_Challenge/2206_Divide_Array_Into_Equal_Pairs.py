5da870425d423bed9a48d876f535294a3e373639
mquintus <mqs@stuebs.tech>
2025-03-18 01:18:20 +0100

    Update 2206_Divide_Array_Into_Equal_Pairs.py

--- Daily_Challenge/2206_Divide_Array_Into_Equal_Pairs.py
+++ Daily_Challenge/2206_Divide_Array_Into_Equal_Pairs.py
@@ -1,1 +1,13 @@
 # 2206 - Divide Array Into Equal Pairs
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        counts = [0] * 501
+        uneven = 0
+        for el in nums:
+            counts[el] = 1 - (counts[el] % 2)
+            if counts[el] != 0:
+                uneven += 1
+            else:
+                uneven -= 1
+        
+        return uneven == 0
