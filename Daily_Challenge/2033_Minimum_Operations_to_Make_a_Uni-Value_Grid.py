b2c619260258659cc2fcbc6ba0ff586589631634
mquintus <mqs@stuebs.tech>
2025-03-26 09:08:48 +0100

    Update 2033_Minimum_Operations_to_Make_a_Uni-Value_Grid.py

--- Daily_Challenge/2033_Minimum_Operations_to_Make_a_Uni-Value_Grid.py
+++ Daily_Challenge/2033_Minimum_Operations_to_Make_a_Uni-Value_Grid.py
@@ -1,1 +1,22 @@
 # 2033 - Minimum Operations to Make a Uni-Value Grid
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        allNumbers = []
+        for row in grid:
+            allNumbers.extend(row)
+        allNumbers.sort()
+        
+        n = len(allNumbers)
+        pivot = n // 2
+        pivotEl = allNumbers[pivot]
+
+
+        operations = 0
+        for el in allNumbers:
+            distance = abs(pivotEl - el)
+            if distance % x != 0:
+                return -1
+            else:
+                operations += (distance // x)
+
+        return operations
