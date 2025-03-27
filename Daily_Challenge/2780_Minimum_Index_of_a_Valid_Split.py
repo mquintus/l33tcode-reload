3b393b62c399b1e496f3a843b8b4f40e9ca4d528
mquintus <mqs@stuebs.tech>
2025-03-27 12:05:34 +0100

    Update 2780_Minimum_Index_of_a_Valid_Split.py

--- Daily_Challenge/2780_Minimum_Index_of_a_Valid_Split.py
+++ Daily_Challenge/2780_Minimum_Index_of_a_Valid_Split.py
@@ -1,1 +1,15 @@
 # 2780 - Minimum Index of a Valid Split
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        d = sorted(nums)[n//2]
+
+        dc = sum([1 for el in nums if el == d])
+
+        c = 0
+        for i, el in enumerate(nums):
+            if el == d:
+                c += 1
+            if c > (i+1) // 2 and dc - c > (n - i - 1)//2:
+                return i 
+        return -1
