2d1cad2cde6ec6bf76e05499d08a306eda42006c
mquintus <mqs@stuebs.tech>
2025-03-30 13:33:36 +0200

    Update 763_Partition_Labels.py

--- Daily_Challenge/763_Partition_Labels.py
+++ Daily_Challenge/763_Partition_Labels.py
@@ -1,1 +1,24 @@
 # 763 - Partition Labels
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        c = Counter(s)
+
+        cuts = []
+
+        incomplete_count = 0
+        lc = {}
+        i = 0
+        for el in s:
+            i += 1
+            if el not in lc:
+                lc[el] = 0
+                incomplete_count += 1
+            lc[el] += 1
+            if lc[el] == c[el]:
+                incomplete_count -= 1
+                if incomplete_count == 0:
+                    cuts.append(i)
+                    i = 0
+        
+
+        return cuts
