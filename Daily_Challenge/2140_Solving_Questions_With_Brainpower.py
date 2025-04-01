006a6f43f90aa27783a5fc9b693ed15e097aeb80
mquintus <mqs@stuebs.tech>
2025-04-01 12:38:57 +0200

    Update 2140_Solving_Questions_With_Brainpower.py

--- Daily_Challenge/2140_Solving_Questions_With_Brainpower.py
+++ Daily_Challenge/2140_Solving_Questions_With_Brainpower.py
@@ -1,1 +1,17 @@
 # 2140 - Solving Questions With Brainpower
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        df = [-1] * n
+        
+        df[n-1] = questions[n-1][0]
+        for i in range(n-2,-1,-1):
+            # Return the number of points you can get
+            take = questions[i][0]
+            seq = i + questions[i][1] + 1
+            if seq < n:
+                take += df[seq]
+            skip = df[i + 1]
+            df[i] = max(take, skip)
+
+        return df[0]
