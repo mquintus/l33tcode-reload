2cf73dcd31e03ebd7eafec35ba5956807ddab505
mquintus <mqs@stuebs.tech>
2025-04-03 18:07:11 +0200

    Update 2874_Maximum_Value_of_an_Ordered_Triplet_II.py

--- Daily_Challenge/2874_Maximum_Value_of_an_Ordered_Triplet_II.py
+++ Daily_Challenge/2874_Maximum_Value_of_an_Ordered_Triplet_II.py
@@ -1,1 +1,13 @@
 # 2874 - Maximum Value of an Ordered Triplet II
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        maxDiff = nums[0] - nums[1]
+        maxElement = max(nums[0], nums[1])
+        maxTriplet = 0
+        for i in range(2, len(nums)):
+            #print(maxTriplet, maxDiff, nums[i], maxDiff * nums[i])
+            maxTriplet = max([maxTriplet, maxDiff * nums[i]])
+            maxElement = max([maxElement, nums[i]])
+            maxDiff = max(maxDiff, maxElement - nums[i])
+        return maxTriplet
+            
