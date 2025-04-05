6b47ce3a542ac5177941c52208806013b7ff9307
mquintus <mqs@stuebs.tech>
2025-04-05 16:56:46 +0200

    Update 1863_Sum_of_All_Subset_XOR_Totals.py

--- Daily_Challenge/1863_Sum_of_All_Subset_XOR_Totals.py
+++ Daily_Challenge/1863_Sum_of_All_Subset_XOR_Totals.py
@@ -1,1 +1,13 @@
 # 1863 - Sum of All Subset XOR Totals
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        globalSum = 0
+        def rec(i, val):
+            nonlocal globalSum 
+            if i == len(nums):
+                globalSum += val
+                return
+            rec(i+1, val^nums[i])
+            rec(i+1, val)
+        rec(0,0)
+        return globalSum
