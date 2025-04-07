81e2d35e649970f7666361f5289e8ad51b01af26
mquintus <mqs@stuebs.tech>
2025-04-07 09:28:40 +0200

    Update 416_Partition_Equal_Subset_Sum.py

--- Daily_Challenge/416_Partition_Equal_Subset_Sum.py
+++ Daily_Challenge/416_Partition_Equal_Subset_Sum.py
@@ -1,1 +1,19 @@
 # 416 - Partition Equal Subset Sum
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total %2 == 1: return False
+
+        goal = total // 2
+        dp = [False for i in range(goal+1)]
+        dp[0] = True
+
+        for val in nums:
+            if val > goal:
+                continue
+            for i in range(goal, val, -1):
+                dp[i] |= dp[i - val]
+            dp[val] = True
+
+
+        return dp[goal]
