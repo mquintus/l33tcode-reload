a372725256322c68546212129f127759aaf9d7fc
mquintus <mqs@stuebs.tech>
2025-04-08 11:39:29 +0200

    Update 3396_Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct.py

--- Daily_Challenge/3396_Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct.py
+++ Daily_Challenge/3396_Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct.py
@@ -1,1 +1,13 @@
 # 3396 - Minimum Number of Operations to Make Elements in Array Distinct
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        mySet = set()
+        for i, el in enumerate(nums[::-1]):
+            if el in mySet:
+                break
+            mySet.add(el)
+        else:
+            i += 1
+
+        return (n - i + 2) // 3
