d6cc1aa99b7572712318153ad4c53c81cad045d2
mquintus <mqs@stuebs.tech>
2025-04-09 09:08:10 +0200

    Update 3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K.py

--- Daily_Challenge/3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K.py
+++ Daily_Challenge/3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K.py
@@ -1,1 +1,7 @@
 # 3375 - Minimum Operations to Make Array Values Equal to K
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        if min(nums) < k: return -1
+        nums = set(nums)
+        diff = 1 if k in nums else 0
+        return len(nums) - diff
