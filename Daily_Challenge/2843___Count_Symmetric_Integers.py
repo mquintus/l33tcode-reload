bb0c7a8fde7e4e65273ae6f86019e78424a9cb07
mquintus <mqs@stuebs.tech>
2025-04-11 09:19:21 +0200

    Update 2843___Count_Symmetric_Integers.py

--- Daily_Challenge/2843___Count_Symmetric_Integers.py
+++ Daily_Challenge/2843___Count_Symmetric_Integers.py
@@ -1,1 +1,14 @@
 # 2843 -   Count Symmetric Integers
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        final = 0
+        for i in range(low, high+1):
+            num = str(i)
+            strle = len(num)
+            if strle % 2 == 1:
+                continue
+            half1 = [int(j) for j in num[:strle//2]]
+            half2 = [int(j) for j in num[strle//2:]]
+            if sum(half1) == sum(half2):
+                final += 1
+        return final
