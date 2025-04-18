7b8e48f74aa52665ab47df3172c3d191c1309ae3
mquintus <mqs@stuebs.tech>
2025-04-18 12:01:55 +0200

    Update 38_Count_and_Say.py

--- Daily_Challenge/38_Count_and_Say.py
+++ Daily_Challenge/38_Count_and_Say.py
@@ -1,1 +1,20 @@
 # 38 - Count and Say
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def rle(s):
+            enc = ""
+            prev = s[0]
+            c = 1
+            for el in s[1:]:
+                if el != prev :
+                    enc += str(c) + prev
+                    c = 0
+                c += 1
+                prev = el
+            enc += str(c) + prev
+            return enc
+                    
+        enc = "1"
+        for i in range(1,n):
+            enc = rle(enc)
+        return enc
