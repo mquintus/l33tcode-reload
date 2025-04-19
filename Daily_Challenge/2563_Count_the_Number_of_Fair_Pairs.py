c232d10bfaf2361235aef1ddfbf03d75161fedaf
mquintus <mqs@stuebs.tech>
2025-04-19 23:16:36 +0200

    Update 2563_Count_the_Number_of_Fair_Pairs.py

--- Daily_Challenge/2563_Count_the_Number_of_Fair_Pairs.py
+++ Daily_Challenge/2563_Count_the_Number_of_Fair_Pairs.py
@@ -1,1 +1,11 @@
 # 2563 - Count the Number of Fair Pairs
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        count = 0
+        for i, el in enumerate(nums):
+            p0 = bisect_left(nums, lower-el, i+1)
+            p1 = bisect_right(nums, upper-el, i+1)
+            count += p1 - p0
+        return count
+
