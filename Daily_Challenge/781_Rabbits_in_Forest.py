46b81a3c9be8d2fa861829ce874f62911c49145c
mquintus <mqs@stuebs.tech>
2025-04-20 20:05:40 +0200

    Update 781_Rabbits_in_Forest.py

--- Daily_Challenge/781_Rabbits_in_Forest.py
+++ Daily_Challenge/781_Rabbits_in_Forest.py
@@ -1,1 +1,8 @@
 # 781 - Rabbits in Forest
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        res = 0
+        c = Counter(answers)
+        for k, v in c.items():
+            res += math.ceil(v / (k+1)) * (k+1)
+        return res
