fe53c74d6cc5cad99c00743e80875d2c3d60a95f
mquintus <mqs@stuebs.tech>
2025-04-21 10:10:20 +0200

    Update 2145_Count_the_Hidden_Sequences.py

--- Daily_Challenge/2145_Count_the_Hidden_Sequences.py
+++ Daily_Challenge/2145_Count_the_Hidden_Sequences.py
@@ -1,1 +1,14 @@
 # 2145 - Count the Hidden Sequences
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        start = 0
+        down = 0
+        up = 0
+        for el in differences:
+            start += el
+            up = max(up, start)
+            down = min(down, start)
+        width = up - down
+        maxwidth = upper - lower
+        if maxwidth < width: return 0
+        return maxwidth - width + 1
