d3e484dade819a08aeb3f750d72b61da746592e4
mquintus <mqs@stuebs.tech>
2025-04-23 09:37:41 +0200

    Update 1399_Count_Largest_Group.py

--- Daily_Challenge/1399_Count_Largest_Group.py
+++ Daily_Challenge/1399_Count_Largest_Group.py
@@ -1,1 +1,22 @@
 # 1399 - Count Largest Group
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        groups = {}
+
+        for i in range(1,n+1):
+            curr = sum([int(c) for c in str(i)])
+            #print(i, curr)
+            if curr not in groups:
+                groups[curr] = 0
+            groups[curr] += 1
+        
+        biggest = max(groups.values())
+        
+        #print(biggest)
+        #print(groups)
+        res = 0
+        for k,v in groups.items():
+            if v == biggest:
+                res += 1
+
+        return res
