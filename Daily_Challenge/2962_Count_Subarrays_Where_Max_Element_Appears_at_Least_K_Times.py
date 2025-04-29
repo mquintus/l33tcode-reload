5f92f69f3e1dd47910814f6b92a49d188e46155d
mquintus <mqs@stuebs.tech>
2025-04-29 11:37:04 +0200

    Update 2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.py

--- Daily_Challenge/2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.py
+++ Daily_Challenge/2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.py
@@ -1,1 +1,39 @@
 # 2962 - Count Subarrays Where Max Element Appears at Least K Times
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        maxel = max(nums)
+        n = len(nums)
+        
+        maxelcount = 0
+        for el in nums:
+            if el == maxel:
+                maxelcount += 1
+
+        if maxelcount < k:
+            return 0
+
+        result = 0
+        elcount = 0
+        p1 = 0
+        p2 = -1
+        while p2 <= n:
+
+            while elcount < k:
+                p2 += 1
+                if p2 >= n: break
+                if nums[p2] == maxel:
+                    elcount += 1
+            if p2 >= n: break
+            #print("Solution:",p1,"to",p2)
+                
+            while elcount >= k:
+                if nums[p1] == maxel:
+                    elcount -= 1
+                p1 += 1
+
+                new = n - p2
+                result += new
+                #print(p1,p2,new,result)
+                if p1 == n: break
+
+        return result
