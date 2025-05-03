82b7b4b5b270932690acb931a7a94799838f0e13
mquintus <mqs@stuebs.tech>
2025-05-03 22:39:50 +0200

    Update 1007_Minimum_Domino_Rotations_For_Equal_Row.py

--- Daily_Challenge/1007_Minimum_Domino_Rotations_For_Equal_Row.py
+++ Daily_Challenge/1007_Minimum_Domino_Rotations_For_Equal_Row.py
@@ -1,1 +1,22 @@
 # 1007 - Minimum Domino Rotations For Equal Row
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        n = len(tops)
+        solution = float('inf')
+        for a,b in [(tops, bottoms), (bottoms, tops)]:
+            for target in [a[0], b[0]]:
+                switches_a = 0
+                switches_b = 0
+                for i in range(n):
+                    if a[i] == target:
+                        switches_b += 1
+                        continue
+                    if b[i] == target:
+                        switches_a += 1
+                        continue
+                    break
+                else:
+                    solution = min(solution, min(switches_a, switches_b))
+        if solution == float('inf'):
+            return -1
+        return solution
