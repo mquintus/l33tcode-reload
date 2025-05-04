67ba099f39a191f1cef336b33b0d760a78dd2d89
mquintus <mqs@stuebs.tech>
2025-05-04 11:02:36 +0200

    Update 1128_Number_of_Equivalent_Domino_Pairs.py

--- Daily_Challenge/1128_Number_of_Equivalent_Domino_Pairs.py
+++ Daily_Challenge/1128_Number_of_Equivalent_Domino_Pairs.py
@@ -1,1 +1,13 @@
 # 1128 - Number of Equivalent Domino Pairs
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        someSet = dict()
+        pairCount = 0
+        for a,b in dominoes:
+            key = (min(a,b),max(a,b))
+            if key in someSet:
+                someSet[key] += 1
+                pairCount += someSet[key]
+            else:
+                someSet[key] = 0
+        return pairCount
