14c2394c6c42edeaf8985f6a81fac45e18dcc880
mquintus <mqs@stuebs.tech>
2025-05-05 11:31:51 +0200

    Update 790_Domino_and_Tromino_Tiling.py

--- Daily_Challenge/790_Domino_and_Tromino_Tiling.py
+++ Daily_Challenge/790_Domino_and_Tromino_Tiling.py
@@ -1,1 +1,8 @@
 # 790 - Domino and Tromino Tiling
+class Solution:
+    def numTilings(self, n: int) -> int:
+        MOD = 10**9+7
+        seq = [1,1,2,5,11,24,53,117]
+        for i in range(len(seq), n+1):
+            seq.append((seq[-1]*2 + seq[-3])%MOD)
+        return seq[n]
