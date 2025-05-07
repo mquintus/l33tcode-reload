e164977e007efc50cc320beadcb2714e6970248c
mquintus <mqs@stuebs.tech>
2025-05-07 15:09:53 +0200

    Update 3341_Find_Minimum_Time_to_Reach_Last_Room_I.py

--- Daily_Challenge/3341_Find_Minimum_Time_to_Reach_Last_Room_I.py
+++ Daily_Challenge/3341_Find_Minimum_Time_to_Reach_Last_Room_I.py
@@ -1,1 +1,23 @@
 # 3341 - Find Minimum Time to Reach Last Room I
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        rows = len(moveTime)
+        cols = len(moveTime[0])
+        states =[ (0, 0,0)]
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+        while states:
+            time,r,c = heapq.heappop(states)
+            for dr,dc in directions:
+                nc=c+dc
+                nr=r+dr
+                if nc < 0 or nr < 0 or nc >= cols or nr >= rows:
+                    continue
+                if moveTime[nr][nc] == -1:
+                    continue
+                newtime = max(time, moveTime[nr][nc]) + 1
+                heapq.heappush(states, (newtime, nr,nc))
+                moveTime[nr][nc] = -1
+                if nr == rows-1 and nc == cols-1:
+                    return newtime
+
+        
