390d29ee5882c539bbefcbd423106263279c05db
mquintus <mqs@stuebs.tech>
2025-05-19 09:03:37 +0200

    Update 3024_Type_of_Triangle.py

--- Daily_Challenge/3024_Type_of_Triangle.py
+++ Daily_Challenge/3024_Type_of_Triangle.py
@@ -1,1 +1,12 @@
 # 3024 - Type of Triangle
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        if nums[0] + nums[1] <= nums[2] or nums[0] + nums[2] <= nums[1] or  nums[1] + nums[2] <= nums[0]:
+            t = "none"
+        elif nums[0] == nums[1] and nums[0] == nums[2]:
+            t = "equilateral"
+        elif nums[0] == nums[1] or nums[0] == nums[2] or nums[1] == nums[2]:
+            t = "isosceles"
+        else:
+            t = "scalene"
+        return t
