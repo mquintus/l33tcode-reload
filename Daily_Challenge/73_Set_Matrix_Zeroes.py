f67d08dfaecf79ad715c2edbf5b5ffaa5f42ad5f
mquintus <mqs@stuebs.tech>
2025-05-22 11:27:39 +0200

    Update 73_Set_Matrix_Zeroes.py

--- Daily_Challenge/73_Set_Matrix_Zeroes.py
+++ Daily_Challenge/73_Set_Matrix_Zeroes.py
@@ -1,1 +1,22 @@
 # 73 - Set Matrix Zeroes
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = len(matrix)
+        cols = len(matrix[0])
+        row_active = [True for r in range(rows)]
+        col_active = [True for c in range(cols)]
+
+        for r, row in enumerate(matrix):
+            for c, cell in enumerate(row):
+                if cell == 0:
+                    row_active[r] = False
+                    col_active[c] = False
+
+        for r, row in enumerate(matrix):
+            for c, cell in enumerate(row):
+                if row_active[r] == False or col_active[c] == False:
+                    matrix[r][c] = 0
+
