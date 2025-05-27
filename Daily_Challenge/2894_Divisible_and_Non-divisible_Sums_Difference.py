3aae63702822a469bebdc9a925366cf52e4cd8d3
mquintus <mqs@stuebs.tech>
2025-05-27 12:46:29 +0200

    Update 2894_Divisible_and_Non-divisible_Sums_Difference.py

--- Daily_Challenge/2894_Divisible_and_Non-divisible_Sums_Difference.py
+++ Daily_Challenge/2894_Divisible_and_Non-divisible_Sums_Difference.py
@@ -1,1 +1,11 @@
 # 2894 - Divisible and Non-divisible Sums Difference
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        rangesum = (n*(n+1))//2
+        if m == 1:
+            return -rangesum
+        divisible = 0
+        for i in range(m, n+1, m):
+            divisible += i
+        nondivisible = rangesum - divisible
+        return nondivisible - divisible
