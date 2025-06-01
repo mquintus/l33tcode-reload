b557c315d33ffb18d56e2d54812b76577faa87bd
mquintus <mqs@stuebs.tech>
2025-06-01 09:26:07 +0200

    Update 2929_Distribute_Candies_Among_Children_II.py

--- Daily_Challenge/2929_Distribute_Candies_Among_Children_II.py
+++ Daily_Challenge/2929_Distribute_Candies_Among_Children_II.py
@@ -1,1 +1,7 @@
 # 2929 - Distribute Candies Among Children II
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        comb = 0
+        for i in range(0, min(n, limit)+1):
+            comb += max(min(limit, n - i) - max(0, n - i - limit) + 1, 0)
+        return comb
