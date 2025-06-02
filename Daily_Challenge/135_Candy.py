a0bdefc2ea8f45eb90cbd5652f2966937aaa9b52
mquintus <mqs@stuebs.tech>
2025-06-02 07:45:43 +0200

    Update 135_Candy.py

--- Daily_Challenge/135_Candy.py
+++ Daily_Challenge/135_Candy.py
@@ -1,1 +1,22 @@
 # 135 - Candy
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        if n == 0:
+            return 0
+        result = [1] * n
+
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                minimal = result[i-1] + 1
+                result[i] = minimal
+        
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                minimal = result[i+1] + 1
+                result[i] = max(minimal, result[i])
+        
+        #print(result)
+        return sum(result)
+        
+        
