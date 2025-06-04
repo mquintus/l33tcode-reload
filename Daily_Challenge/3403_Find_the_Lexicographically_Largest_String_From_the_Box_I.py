8cf7f94152d2950e47ce58d1140d007f12fb222c
mquintus <mqs@stuebs.tech>
2025-06-04 08:22:10 +0200

    Update 3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.py

--- Daily_Challenge/3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.py
+++ Daily_Challenge/3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.py
@@ -1,1 +1,12 @@
 # 3403 - Find the Lexicographically Largest String From the Box I
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        n = len(word)
+        if numFriends == 1: return word
+        longest_length = n - numFriends + 1
+        highest_letter = max(set(list(word)))
+        start = word.index(highest_letter)
+        final = word[start]
+        for i in range(start,n):
+            final = max(final,word[i:i+longest_length])
+        return final
