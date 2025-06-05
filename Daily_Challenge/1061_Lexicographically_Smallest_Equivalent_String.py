81e4d69cb0a56043194e6e1e4f3745a184286442
mquintus <mqs@stuebs.tech>
2025-06-05 12:47:28 +0200

    Update 1061_Lexicographically_Smallest_Equivalent_String.py

--- Daily_Challenge/1061_Lexicographically_Smallest_Equivalent_String.py
+++ Daily_Challenge/1061_Lexicographically_Smallest_Equivalent_String.py
@@ -1,1 +1,30 @@
 # 1061 - Lexicographically Smallest Equivalent String
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        equivalency = [set([i]) for i in range(26)]
+        for i in range(len(s1)):
+            a = ord(s1[i]) - ord('a')
+            b = ord(s2[i]) - ord('a')
+            if a == b:
+                continue
+            
+            for el in equivalency[a]:
+                if el == a: continue
+                equivalency[el] |= equivalency[b]
+            equivalency[a] |= equivalency[b]
+            for el in equivalency[b]:
+                if el == b: continue
+                equivalency[el] |= equivalency[a]
+            equivalency[b] |= equivalency[a]
+
+
+        equivalency = [sorted(list(i)) for i in equivalency]
+        #print(equivalency)
+        result = []
+        for letter in baseStr:
+            result.append(chr(equivalency[ord(letter)-ord('a')][0]+ord('a')))
+        
+        return "".join(result)
+
+
+
