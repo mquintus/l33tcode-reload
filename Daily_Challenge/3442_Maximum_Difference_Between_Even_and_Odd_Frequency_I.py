c6a976e01ff4c6684b12ceacc16556dea211222b
mquintus <mqs@stuebs.tech>
2025-06-10 08:23:00 +0200

    Update 3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.py

--- Daily_Challenge/3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.py
+++ Daily_Challenge/3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.py
@@ -1,1 +1,17 @@
 # 3442 - Maximum Difference Between Even and Odd Frequency I
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        evenmin = float('inf')
+        oddmax = -1
+
+        for i in range(26):
+            l = chr(ord('a')+i)
+            f = s.count(l)
+            if f == 0:
+                continue
+            if f%2 == 0:
+                evenmin = min(evenmin, f)
+            else:
+                oddmax = max(oddmax, f)
+
+        return oddmax-evenmin
