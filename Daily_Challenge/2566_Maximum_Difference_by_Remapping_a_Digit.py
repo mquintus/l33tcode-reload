60afeddeeeecb71e73e81a920781459dc7c61001
mquintus <mqs@stuebs.tech>
2025-06-14 11:37:43 +0200

    Update 2566_Maximum_Difference_by_Remapping_a_Digit.py

--- Daily_Challenge/2566_Maximum_Difference_by_Remapping_a_Digit.py
+++ Daily_Challenge/2566_Maximum_Difference_by_Remapping_a_Digit.py
@@ -1,1 +1,24 @@
 # 2566 - Maximum Difference by Remapping a Digit
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        n = str(num)
+        highest = []
+        lowest = []
+        reph = -1
+        repm = -1
+        for el in n:
+            if int(el) < 9 and reph == -1:
+                reph = el
+            if int(el) > 0 and repm == -1:
+                repm = el
+            h = el
+            if el == reph:
+                h = "9"
+            highest.append(h)
+            m = el
+            if el == repm:
+                m = "0"
+            lowest.append(m)
+        highest = int("".join(highest))
+        lowest = int("".join(lowest))
+        return highest - lowest
