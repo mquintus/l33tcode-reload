88094c0ed12c4a28b3683d07956debd833568509
mquintus <mqs@stuebs.tech>
2025-06-19 09:47:25 +0200

    Update 2294_Partition_Array_Such_That_Maximum_Difference_Is_K.py

--- Daily_Challenge/2294_Partition_Array_Such_That_Maximum_Difference_Is_K.py
+++ Daily_Challenge/2294_Partition_Array_Such_That_Maximum_Difference_Is_K.py
@@ -1,1 +1,12 @@
 # 2294 - Partition Array Such That Maximum Difference Is K
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        nums.sort()
+        partitions = 0
+        pointer = 0
+        while pointer < n and partitions < n:
+            #print("Starting at", pointer, "with value", nums[pointer], "to maximize the subarray including numbers up until", nums[pointer]+k, "the next subarray starts at" , bisect.bisect_left(nums, nums[pointer]+k+1), "length",n)
+            pointer = bisect.bisect_left(nums, nums[pointer]+k+1)
+            partitions += 1
+        return partitions
