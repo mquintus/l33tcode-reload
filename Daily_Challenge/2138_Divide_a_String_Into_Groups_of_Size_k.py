dd95cda23466ed8591f9b3ee12d892dd27da726c
mquintus <mqs@stuebs.tech>
2025-06-22 13:33:22 +0200

    Update 2138_Divide_a_String_Into_Groups_of_Size_k.py

--- Daily_Challenge/2138_Divide_a_String_Into_Groups_of_Size_k.py
+++ Daily_Challenge/2138_Divide_a_String_Into_Groups_of_Size_k.py
@@ -1,1 +1,9 @@
 # 2138 - Divide a String Into Groups of Size k
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        result = []
+        for i in range(((k-1)+(len(s)))//k):
+            result.append(s[i*k:(i+1)*k])
+        if len(result[-1]) < k:
+            result[-1] += fill*(k-len(result[-1]))
+        return result
