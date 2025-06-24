5ae6f69b5282cc8f5de5718b7d03da55684e7002
mquintus <mqs@stuebs.tech>
2025-06-24 10:48:26 +0200

    Update 2200_Find_All_K-Distant_Indices_in_an_Array.py

--- Daily_Challenge/2200_Find_All_K-Distant_Indices_in_an_Array.py
+++ Daily_Challenge/2200_Find_All_K-Distant_Indices_in_an_Array.py
@@ -1,1 +1,29 @@
 # 2200 - Find All K-Distant Indices in an Array
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        n = len(nums)
+        distances = [n] * n
+        last_match = -1
+        for i, el in enumerate(nums):
+            if el == key: 
+                last_match = i
+            if last_match != -1:
+                distances[i] = min(distances[i], i - last_match)
+        if last_match == -1:
+            return []
+        
+        last_match = -1
+        for i in range(n-1,-1,-1):
+            el = nums[i]
+            if el == key: 
+                last_match = i
+            if last_match != -1:
+                distances[i] = min(distances[i], last_match - i)
+        
+        result = []
+        for i,dist in enumerate(distances):
+            if dist <= k:
+                result.append(i)
+        return result
+        
+
