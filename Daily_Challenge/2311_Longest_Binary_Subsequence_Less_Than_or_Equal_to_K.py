d6ed34cb05a660c113cef4a34878a34e8891aee5
mquintus <mqs@stuebs.tech>
2025-06-26 09:59:07 +0200

    Update 2311_Longest_Binary_Subsequence_Less_Than_or_Equal_to_K.py

--- Daily_Challenge/2311_Longest_Binary_Subsequence_Less_Than_or_Equal_to_K.py
+++ Daily_Challenge/2311_Longest_Binary_Subsequence_Less_Than_or_Equal_to_K.py
@@ -1,1 +1,7 @@
 # 2311 - Longest Binary Subsequence Less Than or Equal to K
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        while int(s, 2) > k:
+            pos = s.index("1")
+            s = s[:pos] + s[(pos+1):]
+        return len(s)
