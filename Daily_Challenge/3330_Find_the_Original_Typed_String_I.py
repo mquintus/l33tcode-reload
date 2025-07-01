23377138e70473d2e55dc5f5e465d8a6094f4076
mquintus <mqs@stuebs.tech>
2025-07-01 08:45:01 +0200

    Update 3330_Find_the_Original_Typed_String_I.py

--- Daily_Challenge/3330_Find_the_Original_Typed_String_I.py
+++ Daily_Challenge/3330_Find_the_Original_Typed_String_I.py
@@ -1,1 +1,10 @@
 # 3330 - Find the Original Typed String I
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        answer = 1
+        prev = word[0]
+        for el in word[1:]:
+            if el == prev:
+                answer += 1
+            prev = el
+        return answer
