2118e4d0a3e211687c4c0b405f3f5e43087bba7f
mquintus <mqs@stuebs.tech>
2025-07-26 22:16:22 +0200

    Update 3136_Valid_Word.py

--- Daily_Challenge/3136_Valid_Word.py
+++ Daily_Challenge/3136_Valid_Word.py
@@ -1,1 +1,20 @@
 # 3136 - Valid Word
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3: return False
+        vowels = set("aeiouAEIOU")
+        upper = 'bcdfghjklmnpqrstvwxyz'.upper()
+        consonant = set('bcdfghjklmnpqrstvwxyz' + upper)
+        numbers = set("0123456789")
+        hasV = False
+        hasC = False
+        for letter in word:
+            if letter not in vowels and letter not in consonant  and letter not in numbers:
+                return False
+            if letter in vowels:
+                hasV = True
+            if letter in consonant:
+                hasC = True
+        if not hasV or not hasC:
+            return False
+        return True
