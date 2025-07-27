56e60dc02f022fd06a4bb574f25810d6202de482
mquintus <mqs@stuebs.tech>
2025-07-27 16:17:17 +0200

    Update 2210_Count_Hills_and_Valleys_in_an_Array.py

--- Daily_Challenge/2210_Count_Hills_and_Valleys_in_an_Array.py
+++ Daily_Challenge/2210_Count_Hills_and_Valleys_in_an_Array.py
@@ -1,1 +1,15 @@
 # 2210 - Count Hills and Valleys in an Array
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        count = 0
+        prev = nums[0]
+        mid = nums[1]
+        for curr in nums[2:]:
+            if curr == mid:
+                continue
+            if mid > prev and mid > curr or (mid < prev and mid < curr):
+                count += 1
+            prev = mid
+            mid = curr
+        return count
+
