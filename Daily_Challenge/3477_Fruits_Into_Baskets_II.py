1225eb8f6c4e61588046757b43ddb81732692a15
mquintus <mqs@stuebs.tech>
2025-08-05 10:57:03 +0200

    Update 3477_Fruits_Into_Baskets_II.py

--- Daily_Challenge/3477_Fruits_Into_Baskets_II.py
+++ Daily_Challenge/3477_Fruits_Into_Baskets_II.py
@@ -1,1 +1,13 @@
 # 3477 - Fruits Into Baskets II
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        not_placed = 0
+        for i, f in enumerate(fruits):
+            for b in range(len(baskets)):
+                if baskets[b] >= f:
+                    baskets[b] = 0
+                    #print("Fruit",i,"in basket",b)
+                    break
+            else:
+                not_placed += 1
+        return not_placed
