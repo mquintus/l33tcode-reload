61aa80cbebc1ddaaf2fce19715cbe91029d8069a
mquintus <mqs@stuebs.tech>
2025-08-09 10:59:10 +0200

    Update 231_Power_of_Two.py

--- Daily_Challenge/231_Power_of_Two.py
+++ Daily_Challenge/231_Power_of_Two.py
@@ -1,1 +1,9 @@
 # 231 - Power of Two
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False 
+        while n > 0:
+            if n > 1 and n&1: return False
+            n //= 2
+        return True
