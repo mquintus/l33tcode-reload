89c8e895db4175085dcabc45d299f59009e299f7
mquintus <mqs@stuebs.tech>
2025-08-10 20:18:11 +0200

    Update 869_Reordered_Power_of_2.py

--- Daily_Challenge/869_Reordered_Power_of_2.py
+++ Daily_Challenge/869_Reordered_Power_of_2.py
@@ -1,12 +1,20 @@
 # 869 - Reordered Power of 2
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        availableDigits = Counter(str(n))
+        strn = str(n)
+        availableDigits = Counter(strn)
         b = 1
         for i in range(32):
-            necDigits = Counter(str(b))
+            strb = str(b)
             b *= 2
+            if len(strb) > len(strn):
+                return False
+            if len(strb) < len(strn):
+                continue
+            necDigits = Counter(strb)
             if availableDigits == necDigits:
                 return True
+            
         return False
+        
         
