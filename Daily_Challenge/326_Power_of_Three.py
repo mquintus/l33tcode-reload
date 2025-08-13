6304ee4472f4fa0e0a97335464a6a90abaefb343
mquintus <mqs@stuebs.tech>
2025-08-13 10:13:31 +0200

    Update 326_Power_of_Three.py

--- Daily_Challenge/326_Power_of_Three.py
+++ Daily_Challenge/326_Power_of_Three.py
@@ -1,1 +1,9 @@
 # 326 - Power of Three
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        b = 1
+        for i in range(n):
+            if b > n: break
+            if b == n: return True
+            b *= 3
+        return False
