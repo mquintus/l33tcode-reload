9dc28597fc245e9ab5db52a76421695308f0a30f
mquintus <mqs@stuebs.tech>
2025-08-19 14:52:26 +0200

    Update 2348_Number_of_Zero-Filled_Subarrays.py

--- Daily_Challenge/2348_Number_of_Zero-Filled_Subarrays.py
+++ Daily_Challenge/2348_Number_of_Zero-Filled_Subarrays.py
@@ -1,1 +1,14 @@
 # 2348 - Number of Zero-Filled Subarrays
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        p1 = 0
+        count = 0
+        while p1 < len(nums):
+            p2 = p1
+            if nums[p1] == 0:
+                while p2 < len(nums) and nums[p2] == 0:
+                    p2 += 1
+                    count += p2 - p1
+                p1 = p2
+            p1 += 1
+        return count
