7cea60db584c81e4590eab73d6071aeb2fb63e7e
mquintus <mqs@stuebs.tech>
2025-08-22 09:13:42 +0200

    Update 3195_Find_the_Minimum_Area_to_Cover_All_Ones_I.py

--- Daily_Challenge/3195_Find_the_Minimum_Area_to_Cover_All_Ones_I.py
+++ Daily_Challenge/3195_Find_the_Minimum_Area_to_Cover_All_Ones_I.py
@@ -1,1 +1,20 @@
 # 3195 - Find the Minimum Area to Cover All Ones I
+class Solution:
+    def minimumArea(self, grid: List[List[int]]) -> int:
+        left = len(grid[0])
+        right = -1
+        up = len(grid)
+        down = -1
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell == 1:
+                    left = min(left, j)
+                    right = max(right, j)
+
+                    up = min(up, i)
+                    down = max(down, i)
+
+                    #print(i, j)
+        if up == -1: return 0
+        #print(right, left, down, up)
+        return (right-left+1)*(down-up+1)
