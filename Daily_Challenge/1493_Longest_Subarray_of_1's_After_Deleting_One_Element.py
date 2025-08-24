b1bf035f6665658d1c2790174ee3b47f1912dd7b
mquintus <mqs@stuebs.tech>
2025-08-24 17:53:45 +0200

    Update 1493_Longest_Subarray_of_1's_After_Deleting_One_Element.py

--- Daily_Challenge/1493_Longest_Subarray_of_1's_After_Deleting_One_Element.py
+++ Daily_Challenge/1493_Longest_Subarray_of_1's_After_Deleting_One_Element.py
@@ -1,1 +1,32 @@
 # 1493 - Longest Subarray of 1's After Deleting One Element
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        zero = 0
+        n = len(nums)
+        c = nums.count(0)
+        if c == 0 or c == 1: return n - 1
+
+        left = 0
+        right = -1
+        solution = 0
+        while left < n:
+            right += 1
+            if right < n and nums[right] == 0:
+                zero += 1
+
+            if right >= n: break
+            while left < n and zero >= 2 and left < right:
+                if nums[left] == 0:
+                    zero -= 1
+                left += 1
+     
+            localsolution = right - left - zero + 1
+            #print(zero, (right, left, ), localsolution)
+            solution = max(solution, localsolution)
+        localsolution = right - left - zero 
+        #print(zero, (right, left, ), localsolution)
+        solution = max(solution, localsolution)
+        
+        return solution
+            
+
