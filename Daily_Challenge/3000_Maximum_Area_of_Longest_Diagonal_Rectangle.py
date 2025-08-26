184753b0a1908661e4447232a59dd7b9bad16a5d
mquintus <mqs@stuebs.tech>
2025-08-26 19:15:41 +0200

    Update 3000_Maximum_Area_of_Longest_Diagonal_Rectangle.py

--- Daily_Challenge/3000_Maximum_Area_of_Longest_Diagonal_Rectangle.py
+++ Daily_Challenge/3000_Maximum_Area_of_Longest_Diagonal_Rectangle.py
@@ -1,1 +1,8 @@
 # 3000 - Maximum Area of Longest Diagonal Rectangle
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
+        for i in range(len(dimensions)):
+            x,y = dimensions[i]
+            dimensions[i] = (x**2+y**2, x*y)
+        dimensions.sort()
+        return dimensions[-1][1]
