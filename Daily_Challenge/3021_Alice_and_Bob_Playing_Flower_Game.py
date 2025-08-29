892a9ff6015e498df51a48a5354eb83966b1c225
mquintus <mqs@stuebs.tech>
2025-08-29 07:32:41 +0200

    Update 3021_Alice_and_Bob_Playing_Flower_Game.py

--- Daily_Challenge/3021_Alice_and_Bob_Playing_Flower_Game.py
+++ Daily_Challenge/3021_Alice_and_Bob_Playing_Flower_Game.py
@@ -1,1 +1,11 @@
 # 3021 - Alice and Bob Playing Flower Game
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        # Alice wins if (x + y) is odd
+        evens_n = n // 2
+        odds_n = (n+1) // 2
+
+        evens_m = m // 2
+        odds_m = (m+1) // 2
+        
+        return evens_n*odds_m + odds_n*evens_m
