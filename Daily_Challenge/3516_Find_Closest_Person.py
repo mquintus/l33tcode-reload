f6a43b8a2d723f41446ce47411e992695ab1adf7
mquintus <mqs@stuebs.tech>
2025-09-04 08:39:29 +0200

    Update 3516_Find_Closest_Person.py

--- Daily_Challenge/3516_Find_Closest_Person.py
+++ Daily_Challenge/3516_Find_Closest_Person.py
@@ -1,1 +1,8 @@
 # 3516 - Find Closest Person
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        dist1 = abs(z-x)
+        dist2 = abs(z-y)
+        if dist1 == dist2 : return 0
+        if dist1 < dist2 : return 1
+        if dist1 > dist2 : return 2
