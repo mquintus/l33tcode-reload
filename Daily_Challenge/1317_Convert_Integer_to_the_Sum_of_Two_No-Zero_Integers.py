0398b3e327d41cfce60645a24ec78bfca17ea29a
mquintus <mqs@stuebs.tech>
2025-09-08 16:29:38 +0200

    Update 1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers.py

--- Daily_Challenge/1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers.py
+++ Daily_Challenge/1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers.py
@@ -1,1 +1,8 @@
 # 1317 - Convert Integer to the Sum of Two No-Zero Integers
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for a in range(1,n):
+            if -1 == str(a).find('0'):
+                b = n - a
+                if -1 == str(b).find('0'):
+                    return [a,b]
