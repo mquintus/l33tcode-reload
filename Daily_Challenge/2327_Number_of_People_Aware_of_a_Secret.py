bdc5cb6b75772f5237003f4d450d287cab06c1f5
mquintus <mqs@stuebs.tech>
2025-09-09 14:43:47 +0200

    Update 2327_Number_of_People_Aware_of_a_Secret.py

--- Daily_Challenge/2327_Number_of_People_Aware_of_a_Secret.py
+++ Daily_Challenge/2327_Number_of_People_Aware_of_a_Secret.py
@@ -2,17 +2,19 @@
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
         MOD =  1_000_000_007
-        hasKnownForDays = [0] * 1001
+        hasKnownForDays = deque([0] * (forget))
         hasKnownForDays[0] = 1
-        
-
+        count = 1
+        telling_people = 0
         for i in range(n-1):
-            #print(hasKnownForDays[:forget])
-            hasKnownForDays2 = [0] * 1001
-            for d, count in enumerate(hasKnownForDays[:forget-1]):
-                hasKnownForDays2[d+1] = count % MOD
-                if d >= delay-1:
-                    hasKnownForDays2[0] += count
-            hasKnownForDays = hasKnownForDays2
+            #print(hasKnownForDays, count, telling_people)
+            telling_people -= hasKnownForDays[forget-1]
+            telling_people += hasKnownForDays[delay-1]
+            count += telling_people
+            count -= hasKnownForDays[forget-1]
 
-        return sum(hasKnownForDays) % MOD
+            hasKnownForDays.appendleft(telling_people % MOD)
+            hasKnownForDays.pop()
+
+        #print(hasKnownForDays, count, telling_people)
+        return count % MOD
