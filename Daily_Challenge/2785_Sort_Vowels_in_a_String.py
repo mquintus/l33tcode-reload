d18a4404bb8a475d3656c6e9c611fb48859b26ce
mquintus <mqs@stuebs.tech>
2025-09-11 11:13:10 +0200

    Update 2785_Sort_Vowels_in_a_String.py

--- Daily_Challenge/2785_Sort_Vowels_in_a_String.py
+++ Daily_Challenge/2785_Sort_Vowels_in_a_String.py
@@ -1,1 +1,24 @@
 # 2785 - Sort Vowels in a String
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowOrder = 'AEIOUaeiou'
+        vowSet = set('AEIOUaeiou')
+        vowels = {char: 0 for char in vowSet}
+        
+        for char in s:
+            if char in vowSet:
+                vowels[char] += 1
+
+        t = list(s)
+        for i, el in enumerate(t):
+            if el in vowSet:
+                for v in vowOrder:
+                    c = vowels[v]
+                    if c > 0:
+                        t[i] = v
+                        vowels[v] -= 1
+                        break
+        
+        return ''.join(t)
+
+
