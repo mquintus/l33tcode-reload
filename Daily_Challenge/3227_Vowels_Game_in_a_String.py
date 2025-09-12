d54d12561ab0afa71f5cabc73eedf67d6343e04d
mquintus <mqs@stuebs.tech>
2025-09-12 13:28:54 +0200

    Update 3227_Vowels_Game_in_a_String.py

--- Daily_Challenge/3227_Vowels_Game_in_a_String.py
+++ Daily_Challenge/3227_Vowels_Game_in_a_String.py
@@ -1,1 +1,7 @@
 # 3227 - Vowels Game in a String
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        c = s.count('a') + s.count('e') + s.count('i') + s.count('o') + s.count('u')
+        if c == 0:
+            return False
+        return True
