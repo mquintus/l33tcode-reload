4395e30ff4aef884d4e4a3c46dce6121de53f082
mquintus <mqs@stuebs.tech>
2025-09-13 11:41:09 +0200

    Update 3541_Find_Most_Frequent_Vowel_and_Consonant.py

--- Daily_Challenge/3541_Find_Most_Frequent_Vowel_and_Consonant.py
+++ Daily_Challenge/3541_Find_Most_Frequent_Vowel_and_Consonant.py
@@ -1,1 +1,9 @@
 # 3541 - Find Most Frequent Vowel and Consonant
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        mv = max([s.count('a'), s.count('e'), s.count('i'), s.count('o'), s.count('u')])
+        c = Counter(s)
+        t = sorted([(f, el) for el, f in c.items() if el not in 'aeiou'])
+        if len(t) == 0: return mv
+        #print(mv, t)
+        return mv + t[-1][0]
