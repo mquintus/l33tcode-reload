2edacbb90bb1dd7acd729eee663017f2e054b664
mquintus <mqs@stuebs.tech>
2025-09-14 20:38:42 +0200

    Update 966_Vowel_Spellchecker.py

--- Daily_Challenge/966_Vowel_Spellchecker.py
+++ Daily_Challenge/966_Vowel_Spellchecker.py
@@ -1,1 +1,43 @@
 # 966 - Vowel Spellchecker
+class Solution:
+    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        
+        lowercase = {}
+        for s in wordlist[::-1]:
+            lowercase[s.lower()] = s
+
+        def vowelReplace(word):
+            word = word.lower()
+            word = word.replace("a", "*")
+            word = word.replace("e", "*")
+            word = word.replace("i", "*")
+            word = word.replace("o", "*")
+            word = word.replace("u", "*")
+            return word
+
+        lowercase = {}
+        for s in wordlist[::-1]:
+            lowercase[s.lower()] = s
+
+        rep = {}
+        for s in wordlist[::-1]:
+            rep[vowelReplace(s)] = s
+
+        wordlist = set(wordlist)
+
+        #print(rep)
+        res = []
+        for q in queries:
+            if q in wordlist: 
+                res.append(q)
+            elif q.lower() in lowercase:
+                res.append(lowercase[q.lower()])
+            elif vowelReplace(q) in rep:
+                res.append(rep[vowelReplace(q)])
+            else:
+                #print("Not found", vowelReplace(q))
+                res.append("")
+
+        return res
+
+        
