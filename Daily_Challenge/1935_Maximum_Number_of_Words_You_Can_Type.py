7116a7c2eba738dd7d4358eae94c499981c90bb7
mquintus <mqs@stuebs.tech>
2025-09-15 15:22:16 +0200

    Update 1935_Maximum_Number_of_Words_You_Can_Type.py

--- Daily_Challenge/1935_Maximum_Number_of_Words_You_Can_Type.py
+++ Daily_Challenge/1935_Maximum_Number_of_Words_You_Can_Type.py
@@ -1,1 +1,11 @@
 # 1935 - Maximum Number of Words You Can Type
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        counter = 0
+        for word in text.split(' '):
+            for bl in brokenLetters:
+                if bl in word:
+                    break
+            else:
+                counter += 1
+        return counter
