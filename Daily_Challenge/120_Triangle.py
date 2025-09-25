12150476872ac65ef6f5a1e446708a71bf5a079c
mquintus <mqs@stuebs.tech>
2025-09-25 09:03:17 +0200

    Update 120_Triangle.py

--- Daily_Challenge/120_Triangle.py
+++ Daily_Challenge/120_Triangle.py
@@ -1,1 +1,7 @@
 # 120 - Triangle
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(1, len(triangle)):
+            for j in range(i+1):
+                triangle[i][j] += min(triangle[i-1][max(0,j-1):j+1])
+        return min(triangle[-1])
