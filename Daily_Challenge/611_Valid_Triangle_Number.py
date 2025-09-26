9133313bb57072ada059e78adffa24988d5f67c1
mquintus <mqs@stuebs.tech>
2025-09-26 12:36:22 +0200

    Update 611_Valid_Triangle_Number.py

--- Daily_Challenge/611_Valid_Triangle_Number.py
+++ Daily_Challenge/611_Valid_Triangle_Number.py
@@ -1,1 +1,20 @@
 # 611 - Valid Triangle Number
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return 0
+        valids = 0
+        nums.sort()
+        n = len(nums)
+        for i in range(n-2):
+            first = nums[i]
+            for j in range(i+1, n-1):
+                second = nums[j]
+                third = first+second
+                k = bisect.bisect_left(nums, third)
+                if k <= j: break
+                valids += k-j-1
+        return valids
+
+
+                    
