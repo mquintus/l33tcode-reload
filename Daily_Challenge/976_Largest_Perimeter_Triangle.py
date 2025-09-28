601db3822f974b7296c3353283844f9da7a16541
mquintus <mqs@stuebs.tech>
2025-09-28 13:42:10 +0200

    Update 976_Largest_Perimeter_Triangle.py

--- Daily_Challenge/976_Largest_Perimeter_Triangle.py
+++ Daily_Challenge/976_Largest_Perimeter_Triangle.py
@@ -1,1 +1,32 @@
 # 976 - Largest Perimeter Triangle
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return 0
+        nums.sort()
+        n = len(nums)
+        for i in range(n-1,1,-1):
+            first = nums[i]
+            # print("////////////",  first)
+            for j in range(i-1, 0,-1):
+                second = nums[j]
+                # print(second)
+                minimal = first - second + 1
+                maximal = first + second - 1
+                
+                # print(first, second, maximal, bisect.bisect_right(nums, maximal) )
+                k = bisect.bisect_left(nums, maximal) 
+                if k == -1: break
+                if k >= j: 
+                    k = j - 1
+                newthird = nums[k]
+                if newthird > maximal:
+                    break
+                if newthird < minimal:
+                    break
+                # print("At position",k,"we find satisfying value",newthird)
+                return first+second+newthird
+        return 0
+
+
+                    
