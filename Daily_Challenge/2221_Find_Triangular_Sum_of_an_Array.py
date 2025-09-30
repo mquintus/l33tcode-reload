acad27d9cf49548a7d00910a70b54cd2d0f603a1
mquintus <mqs@stuebs.tech>
2025-09-30 11:24:54 +0200

    Update 2221_Find_Triangular_Sum_of_an_Array.py

--- Daily_Challenge/2221_Find_Triangular_Sum_of_an_Array.py
+++ Daily_Challenge/2221_Find_Triangular_Sum_of_an_Array.py
@@ -1,1 +1,7 @@
 # 2221 - Find Triangular Sum of an Array
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        for l in range(len(nums)-1, 0, -1):
+            for i in range(l):
+                nums[i] = (nums[i] + nums[i+1]) % 10
+        return nums[0] % 10
