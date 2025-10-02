96a41096e20b36ea4bee096dad10820f0fa322cd
mquintus <mqs@stuebs.tech>
2025-10-02 12:46:21 +0200

    Update 3100_Water_Bottles_II.py

--- Daily_Challenge/3100_Water_Bottles_II.py
+++ Daily_Challenge/3100_Water_Bottles_II.py
@@ -1,1 +1,16 @@
 # 3100 - Water Bottles II
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        # after each action, numExchange is increased by one
+        # therefore the number of bottles that I initially can swap in is
+        # numExchange or 2*numExchange+1 or 3*numExchange+2 or ...
+        # I could implement binarySearch but the maxvalue is 100, so lets go for the simulation approach
+
+        drunk = 0 
+        while numBottles >= numExchange:
+            drunk += numExchange
+            numBottles -= numExchange
+            numExchange += 1
+            numBottles += 1
+        drunk += numBottles
+        return drunk
