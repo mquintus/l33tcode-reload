9ecb9e1cb5bf266d42c366d7d4353fe35bfe13de
mquintus <mqs@stuebs.tech>
2025-10-20 13:01:25 +0200

    Update 2011_Final_Value_of_Variable_After_Performing_Operations.py

--- Daily_Challenge/2011_Final_Value_of_Variable_After_Performing_Operations.py
+++ Daily_Challenge/2011_Final_Value_of_Variable_After_Performing_Operations.py
@@ -1,1 +1,10 @@
 # 2011 - Final Value of Variable After Performing Operations
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for op in operations:
+            if op[1] == '+':
+                x += 1
+            else:
+                x -= 1
+        return x
