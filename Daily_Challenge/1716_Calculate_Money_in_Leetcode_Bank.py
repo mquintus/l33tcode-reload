8a8ae2a1d28b5d7507df12b75d81165f07246e73
mquintus <mqs@stuebs.tech>
2025-10-26 01:16:42 +0200

    Update 1716_Calculate_Money_in_Leetcode_Bank.py

--- Daily_Challenge/1716_Calculate_Money_in_Leetcode_Bank.py
+++ Daily_Challenge/1716_Calculate_Money_in_Leetcode_Bank.py
@@ -1,1 +1,13 @@
 # 1716 - Calculate Money in Leetcode Bank
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        fullweeks = n // 7
+        dayofweek = n % 7
+        def gauss(x):
+            return (x*(x+1))//2
+        part1 = gauss(7) * fullweeks
+        part2 = 7 * gauss(fullweeks-1) 
+        part3 = gauss(dayofweek + fullweeks) 
+        part4 = -gauss(fullweeks)
+        #print(part1, part2, part3, part4)
+        return part1 +  part2 + part3 + part4
