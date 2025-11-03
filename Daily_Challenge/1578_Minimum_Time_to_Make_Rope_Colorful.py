e728e47698951a2ffea96b9d0d9dd8ddfbf75007
mquintus <mqs@stuebs.tech>
2025-11-03 16:40:33 +0100

    Update 1578_Minimum_Time_to_Make_Rope_Colorful.py

--- Daily_Challenge/1578_Minimum_Time_to_Make_Rope_Colorful.py
+++ Daily_Challenge/1578_Minimum_Time_to_Make_Rope_Colorful.py
@@ -1,1 +1,22 @@
 # 1578 - Minimum Time to Make Rope Colorful
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        prev = -1
+        maximumRemove = []
+        timeToRemove = 0
+        for i, el in enumerate(colors):
+            #print(el)
+            if el != prev:
+                maximumRemove = [neededTime[i]]
+            else:
+                if neededTime[i] > maximumRemove[-1]:
+                    #print("Add",maximumRemove)
+                    timeToRemove += maximumRemove.pop()
+                    maximumRemove.append(neededTime[i])
+                else:
+                    timeToRemove += neededTime[i]
+                    #print("Add",neededTime[i])
+                
+            prev = el
+
+        return timeToRemove
