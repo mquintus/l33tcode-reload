9ad2cb877f21f13adf957dd364790eeafd3c3207
mquintus <mqs@stuebs.tech>
2025-11-04 09:52:33 +0100

    Update 3318_Find_X-Sum_of_All_K-Long_Subarrays_I.py

--- Daily_Challenge/3318_Find_X-Sum_of_All_K-Long_Subarrays_I.py
+++ Daily_Challenge/3318_Find_X-Sum_of_All_K-Long_Subarrays_I.py
@@ -1,1 +1,12 @@
 # 3318 - Find X-Sum of All K-Long Subarrays I
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        res = []
+        for start in range(len(nums) - k+1):
+            total = 0
+            freq = Counter(nums[start:start+k])
+            freqitems = sorted([(f,v) for v,f in freq.items()])[-x:]
+            for f,v in freqitems:
+                total += f*v
+            res.append(total)
+        return res
