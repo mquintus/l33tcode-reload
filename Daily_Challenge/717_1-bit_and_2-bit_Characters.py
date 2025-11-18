41fa8772b38b16fda962d8091a9a0943ad077717
mquintus <mqs@stuebs.tech>
2025-11-18 11:25:02 +0100

    Update 717_1-bit_and_2-bit_Characters.py

--- Daily_Challenge/717_1-bit_and_2-bit_Characters.py
+++ Daily_Challenge/717_1-bit_and_2-bit_Characters.py
@@ -1,1 +1,13 @@
 # 717 - 1-bit and 2-bit Characters
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n = len(bits)
+        i = 0 
+        while i < n:
+            if i == n - 1:
+                return True
+            if bits[i] == 0:
+                i += 1
+            if bits[i] == 1:
+                i += 2
+        return False
