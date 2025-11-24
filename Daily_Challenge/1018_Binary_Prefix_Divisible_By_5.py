a7ef07b4dae0853688d6d0d4a8af3432a96edf6d
mquintus <mqs@stuebs.tech>
2025-11-24 11:59:21 +0100

    Update 1018_Binary_Prefix_Divisible_By_5.py

--- Daily_Challenge/1018_Binary_Prefix_Divisible_By_5.py
+++ Daily_Challenge/1018_Binary_Prefix_Divisible_By_5.py
@@ -1,1 +1,12 @@
 # 1018 - Binary Prefix Divisible By 5
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        n = len(nums)
+        number = 0
+        result = []
+        for i in range(n):
+            number += nums[i]
+            #print(number)
+            result.append(number % 5 == 0)
+            number *= 2
+        return result
