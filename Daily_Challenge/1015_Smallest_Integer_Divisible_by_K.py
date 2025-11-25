4382c711bba7764f2d5a0eb237b1edc7f49d619c
mquintus <mqs@stuebs.tech>
2025-11-25 18:49:33 +0100

    Update 1015_Smallest_Integer_Divisible_by_K.py

--- Daily_Challenge/1015_Smallest_Integer_Divisible_by_K.py
+++ Daily_Challenge/1015_Smallest_Integer_Divisible_by_K.py
@@ -1,1 +1,22 @@
 # 1015 - Smallest Integer Divisible by K
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        n = 1
+
+        while n < k:
+            n *= 10
+            n += 1
+
+        size = len(str(n))
+        counter = k + 1
+        while counter > 0:
+            if n % k == 0:
+                return size
+            else:
+                counter -= 1
+                size += 1
+            n *= 10
+            n += 1
+            n %= k
+        return -1 
+            
