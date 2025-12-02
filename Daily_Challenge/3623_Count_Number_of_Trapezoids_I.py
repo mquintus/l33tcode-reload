23c2f8d143f9cb598f66565568f35f7d549b3f2d
mquintus <mqs@stuebs.tech>
2025-12-02 20:32:46 +0100

    Update 3623_Count_Number_of_Trapezoids_I.py

--- Daily_Challenge/3623_Count_Number_of_Trapezoids_I.py
+++ Daily_Challenge/3623_Count_Number_of_Trapezoids_I.py
@@ -1,1 +1,36 @@
 # 3623 - Count Number of Trapezoids I
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        MOD = 1_000_000_007
+
+        points_by_y = dict()
+        for x,y in points:
+            if y not in points_by_y:
+                points_by_y[y] = 0
+            points_by_y[y] += 1
+
+        total = 0
+
+        to_remove = list()
+        for y, count in points_by_y.items():
+            if count < 2:
+                to_remove.append(y)
+
+        while to_remove:
+            del points_by_y[to_remove.pop()]
+
+
+        pairwise_sorted = sorted(list(points_by_y.items()))
+
+        prev = 0
+        for y, n in pairwise_sorted:
+            ways = (n * (n-1))//2
+            if ways == 0: continue
+            total += prev*ways
+            prev += ways
+            
+                
+        
+        return total % MOD
+                
+
