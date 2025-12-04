88e8f3c3e71e020ae0803a5d083701a76997857e
mquintus <mqs@stuebs.tech>
2025-12-04 17:32:49 +0100

    Update 2211_Count_Collisions_on_a_Road.py

--- Daily_Challenge/2211_Count_Collisions_on_a_Road.py
+++ Daily_Challenge/2211_Count_Collisions_on_a_Road.py
@@ -1,1 +1,25 @@
 # 2211 - Count Collisions on a Road
+class Solution:
+    def countCollisions(self, directions: str) -> int:
+        collisions = 0
+        right = 0
+        still = False
+        for d in directions:
+            if d == "R": 
+                right += 1
+
+            elif d == "S":
+                collisions += right
+                right = 0
+                still = True
+
+            elif d in "L":
+                if right > 0:
+                    collisions += right
+                    right = 0
+                    still = True
+
+                if still:
+                    collisions += 1
+
+        return collisions
