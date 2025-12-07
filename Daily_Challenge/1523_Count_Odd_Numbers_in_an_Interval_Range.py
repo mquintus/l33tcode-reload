9cb82225810b26fc593739450565e8e8f15fe2ff
mquintus <mqs@stuebs.tech>
2025-12-07 15:33:13 +0100

    Update 1523_Count_Odd_Numbers_in_an_Interval_Range.py

--- Daily_Challenge/1523_Count_Odd_Numbers_in_an_Interval_Range.py
+++ Daily_Challenge/1523_Count_Odd_Numbers_in_an_Interval_Range.py
@@ -1,1 +1,6 @@
 # 1523 - Count Odd Numbers in an Interval Range
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        total = (high-low+1)//2
+        if (low % 2) == 1 and (high % 2) == 1: total += 1
+        return total
