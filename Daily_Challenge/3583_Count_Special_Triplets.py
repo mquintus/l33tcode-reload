f51c1e2398390cccf96ad0ba956280b56ffd69fc
mquintus <mqs@stuebs.tech>
2025-12-09 19:04:50 +0100

    Update 3583_Count_Special_Triplets.py

--- Daily_Challenge/3583_Count_Special_Triplets.py
+++ Daily_Challenge/3583_Count_Special_Triplets.py
@@ -1,1 +1,29 @@
 # 3583 - Count Special Triplets
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        MOD = 1_000_000_007
+        freqPrev = dict()
+        freqNext = Counter(nums)
+
+        total = 0
+        n = len(nums)
+        for i in range(0, n):
+            el = nums[i]
+            freqNext[el] -= 1
+
+            doubl = 2 * el
+
+            #print(freqPrev, freqNext, "Looking for", doubl)
+
+            #print("looking for ", doubl)
+            if doubl in freqPrev and doubl in freqNext:
+                total += freqNext[doubl] *  freqPrev[doubl]
+                total %= MOD
+
+            if el not in freqPrev:
+                freqPrev[el] = 0
+            freqPrev[el] += 1
+
+        return total % MOD
+
+
