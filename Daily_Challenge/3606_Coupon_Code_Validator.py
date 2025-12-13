e3c98b160cd09f2cce8ab8aace91360726129bfb
mquintus <mqs@stuebs.tech>
2025-12-13 13:02:24 +0100

    Update 3606_Coupon_Code_Validator.py

--- Daily_Challenge/3606_Coupon_Code_Validator.py
+++ Daily_Challenge/3606_Coupon_Code_Validator.py
@@ -1,1 +1,29 @@
 # 3606 - Coupon Code Validator
+class Solution:
+    def countMentions(self, numberOfUsers: int, events: List[List[str]]) -> List[int]:
+        mentions = [0] * numberOfUsers
+        sleeping = [-1] * numberOfUsers
+        events.sort(key=lambda x: (int(x[1]), -1*ord(x[0][0])))
+        for kind, timestamp, text in events:
+            if kind == "OFFLINE":
+                sleeping[int(text)] = int(timestamp) + 59
+                continue
+            
+            for mention in text.split(" "):
+                if mention == "ALL":
+                    mentions = [i + 1 for i in mentions]
+                    continue
+                
+                if mention == "HERE":
+                    #print("HERE is mentioned while", sleeping)
+                    for userid in range(numberOfUsers):
+                        if sleeping[userid] < int(timestamp):
+                            mentions[userid] += 1
+                    continue
+
+                userid = int(mention[2:])
+                mentions[userid] += 1
+                continue
+
+        return mentions
+
