08f569675a990692903f39e2b1da2046f78d981a
Marius Stuebs <mqs@stuebs.tech>
2025-12-25 02:01:05 +0100

    'New day, new challenge: #3075. Maximize Happiness of Selected Children'

--- Daily_Challenge/3075_Maximize_Happiness_of_Selected_Children.py
+++ Daily_Challenge/3075_Maximize_Happiness_of_Selected_Children.py
@@ -1,16 +1,1 @@
 # 3075 - Maximize Happiness of Selected Children
-class Solution:
-    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        happiness.sort()
-        value = 0
-        value += happiness[-1]
-        k -= 1
-        reduction = 1
-        while k > 0:
-            curr_value = happiness[-1 - reduction] - reduction
-            if curr_value <= 0:
-                return value
-            value += curr_value
-            reduction += 1
-            k -= 1
-        return value
