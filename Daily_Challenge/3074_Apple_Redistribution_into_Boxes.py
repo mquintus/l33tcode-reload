726f0a9958a6aa3bfb4d1dbfea015b21e0cbcf2f
mquintus <mqs@stuebs.tech>
2025-12-25 01:59:20 +0100

    Update 3074_Apple_Redistribution_into_Boxes.py

--- Daily_Challenge/3074_Apple_Redistribution_into_Boxes.py
+++ Daily_Challenge/3074_Apple_Redistribution_into_Boxes.py
@@ -1,1 +1,14 @@
 # 3074 - Apple Redistribution into Boxes
+class Solution:
+    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+        capacity.sort()
+        total = 0
+        apple = sum(apple)
+        while apple > 0:
+            total += 1
+            if capacity[-1] >= apple:
+                break
+            apple -= capacity.pop()
+
+            
+        return total
