0420019ccba2f71af8425dd1499626ac3d6874cb
mquintus <mqs@stuebs.tech>
2026-01-01 16:16:06 +0100

    Update 66_Plus_One.py

--- Daily_Challenge/66_Plus_One.py
+++ Daily_Challenge/66_Plus_One.py
@@ -1,1 +1,18 @@
 # 66 - Plus One
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = True
+        i = len(digits)-1
+        while carry and i >= 0:
+            if digits[i] == 9:
+                digits[i] = 0
+                i -= 1
+                continue
+
+            digits[i] += 1
+            carry = False
+            i -= 1
+            
+        if carry:
+            digits.insert(0,1)
+        return digits
