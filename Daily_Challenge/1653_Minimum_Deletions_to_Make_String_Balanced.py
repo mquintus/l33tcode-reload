9c81abb6431e5de7a84040212b8c71b4a0300ea3
Marius Stuebs <mqs@stuebs.tech>
2026-02-07 02:01:04 +0100

    'New day, new challenge: #1653. Minimum Deletions to Make String Balanced'

--- Daily_Challenge/1653_Minimum_Deletions_to_Make_String_Balanced.py
+++ Daily_Challenge/1653_Minimum_Deletions_to_Make_String_Balanced.py
@@ -1,15 +1,1 @@
 # 1653 - Minimum Deletions to Make String Balanced
-class Solution:
-    def minimumDeletions(self, s: str) -> int:
-        b_counter = 0
-        min_removals = 0
-
-        for char in s:
-            if char == 'a':
-                if b_counter > 0:
-                    b_counter -= 1
-                    min_removals += 1
-            else:
-                b_counter += 1
-
-        return min_removals
